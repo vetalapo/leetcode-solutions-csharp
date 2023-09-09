f7e1b4c19080464b832c0de029302449d37dd99d
Vitaly Dushkin <vetalapo@gmail.com>
2023-09-10 01:52:00 +0200

    Solved problem 876: Middle of the linked list

---
+++ src/Problems/linked-list/MiddleOfTheLinkedList.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * 876
+ * Middle of the Linked List
+ **
+ * Given the head of a singly linked list, return the middle node of the linked list.
+ * If there are two middle nodes, return the second middle node.
+ * 
+ * https://leetcode.com/problems/middle-of-the-linked-list/
+ */
+
+namespace Problems;
+
+public class MiddleOfTheLinkedList
+{
+    public ListNode MiddleNode( ListNode head )
+    {
+        ListNode middle = head;
+        ListNode end = head;
+
+        while ( end is not null && end.next is not null )
+        {
+            middle = middle.next;
+            end = end.next.next;
+        }
+
+        return middle;
+    }
+}
---
+++ src/Tests/linked-list/MiddleOfTheLinkedListTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+
+using NUnit.Framework;
+
+using Problems;
+
+[TestFixture]
+public class MiddleOfTheLinkedListTests
+{
+    [Test]
+    public void MiddleNodeOneToFiveTest()
+    {
+        // Arrange
+        ListNode input = new( 1, new( 2, new( 3, new( 4, new( 5 ) ) ) ) );
+        ListNode expectedResult = new( 3, new( 4, new( 5 ) ) );
+
+        // Act
+        ListNode testObject = new MiddleOfTheLinkedList().MiddleNode( input );
+
+        // Assert
+        testObject.Should().BeEquivalentTo( expectedResult );
+    }
+
+    [Test]
+    public void MiddleNodeOneToSixTest()
+    {
+        // Arrange
+        ListNode input = new( 1, new( 2, new( 3, new( 4, new( 5, new( 6 ) ) ) ) ) );
+        ListNode expectedResult = new( 4, new( 5, new( 6 ) ) );
+
+        // Act
+        ListNode testObject = new MiddleOfTheLinkedList().MiddleNode( input );
+
+        // Assert
+        testObject.Should().BeEquivalentTo( expectedResult );
+    }
+}
