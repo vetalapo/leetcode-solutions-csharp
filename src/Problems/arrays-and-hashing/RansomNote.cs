ac23c328971c2d002dc5307147be4db345c868d1
Vitaly Dushkin <vetalapo@gmail.com>
2023-09-10 23:51:30 +0200

    Solved 383: ransom note

---
+++ src/Problems/arrays-and-hashing/RansomNote.cs
@@ -0,0 +1,42 @@
+﻿/**
+ * 383
+ * Ransom Note
+ **
+ * Given two strings ransomNote and magazine,
+ * return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+ * Each letter in magazine can only be used once in ransomNote.
+ * 
+ * https://leetcode.com/problems/ransom-note/
+ */
+
+namespace Problems;
+
+public class RansomNote
+{
+    public bool CanConstruct( string ransomNote, string magazine )
+    {
+        Dictionary<char, ulong> magazineMap = new();
+
+        foreach( char c in magazine )
+        {
+            if ( !magazineMap.ContainsKey( c ))
+            {
+                magazineMap[c] = 0;
+            }
+
+            magazineMap[c]++;
+        }
+
+        foreach ( char c in ransomNote )
+        {
+            if ( !magazineMap.ContainsKey( c ) || magazineMap[c] <= 0 )
+            {
+                return false;
+            }
+
+            magazineMap[c]--;
+        }
+
+        return true;
+    }
+}
---
+++ src/Tests/arrays-and-hashing/RansomNoteTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+[TestFixture]
+public class RansomNoteTests
+{
+    [TestCase( "a", "b", ExpectedResult = false )]
+    [TestCase( "aa", "ab", ExpectedResult = false )]
+    [TestCase( "aa", "aab", ExpectedResult = true )]
+    [TestCase( "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+               "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+               ExpectedResult = true )]
+    public bool CanConstructTest( string ransomNote, string magazine ) => new RansomNote().CanConstruct( ransomNote, magazine );
+}
