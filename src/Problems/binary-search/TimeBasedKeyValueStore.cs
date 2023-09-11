8cd905b2e9969bdf7f51ce9bd16256cfd0285e9d
Vitaly Dushkin <vetalapo@gmail.com>
2023-09-11 23:53:15 +0200

    Solved problem 981: Time Based Key-value store

---
+++ src/Problems/binary-search/TimeBasedKeyValueStore.cs
@@ -0,0 +1,86 @@
+﻿/**
+ * 981
+ * Time Based Key-Value Store
+ **
+ * Design a time-based key-value data structure that
+ * can store multiple values for the same key at different time stamps
+ * and retrieve the key's value at a certain timestamp.
+ * 
+ * Implement the TimeMap class:
+ *   TimeMap() Initializes the object of the data structure.
+ *   
+ *   void set(String key, String value, int timestamp) Stores the key key with the value value at the given time timestamp.
+ *   
+ *   String get(String key, int timestamp) Returns a value such that set was called previously, with timestamp_prev <= timestamp.
+ *     If there are multiple such values, it returns the value associated with the largest timestamp_prev.
+ *     If there are no values, it returns "".
+ * 
+ * https://leetcode.com/problems/time-based-key-value-store/
+ */
+
+namespace Problems;
+
+public class TimeMap
+{
+    private readonly Dictionary<string, List<(int timestamp, string value)>> _store;
+
+    public TimeMap()
+    {
+        this._store = new Dictionary<string, List<(int timestamp, string value)>>();
+    }
+
+    public void Set( string key, string value, int timestamp )
+    {
+        if ( !this._store.ContainsKey( key ) )
+        {
+            this._store[key] = new List<(int timestamp, string value)>();
+        }
+
+        this._store[key].Add( (timestamp, value) );
+    }
+
+    public string Get( string key, int timestamp )
+    {
+        if ( this._store.TryGetValue( key, out List<(int timestamp, string value)>? values ) )
+        {
+            return values.LastOrDefault( x => x.timestamp <= timestamp ).value ?? string.Empty;
+        }
+
+        return string.Empty;
+    }
+
+    public string GetBinarySearch( string key, int timestamp )
+    {
+        if ( !this._store.TryGetValue( key, out List<(int timestamp, string value)>? values ) )
+        {
+            return string.Empty;
+        }
+
+        int left = 0;
+        int right = values.Count;
+        string result = string.Empty;
+
+        while ( left < right )
+        {
+            int middle = ( left + right ) / 2;
+
+            if ( values[middle].timestamp == timestamp )
+            {
+                result = values[middle].value;
+
+                return result;
+            }
+            else if ( values[middle].timestamp < timestamp )
+            {
+                left = middle + 1;
+                result = values[middle].value;
+            }
+            else
+            {
+                right = middle;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/Tests/binary-search/TimeBasedKeyValueStoreTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+[TestFixture]
+public class TimeBasedKeyValueStoreTests
+{
+    [Test]
+    public void TimeBasedKeyValueStoreTest()
+    {
+        TimeMap timeMap = new();
+
+        timeMap.Set( key: "foo", value: "bar", timestamp: 1 );
+
+        Assert.AreEqual( expected: "bar", actual: timeMap.Get( "foo", 1 ) );
+        Assert.AreEqual( expected: "bar", actual: timeMap.Get( "foo", 3 ) );
+
+        timeMap.Set( "foo", "bar2", 4 );
+
+        Assert.AreEqual( expected: "bar2", actual: timeMap.Get( "foo", 4 ) );
+        Assert.AreEqual( expected: "bar2", actual: timeMap.Get( "foo", 5 ) );
+    }
+
+    [Test]
+    public void TimeBasedKeyValueStoreBorderChecksTest()
+    {
+        TimeMap timeMap = new();
+
+        timeMap.Set( key: "love", value: "high", timestamp: 10 );
+        timeMap.Set( key: "love", value: "low", timestamp: 20 );
+
+        Assert.AreEqual( expected: string.Empty, actual: timeMap.Get( "love", 5 ) );
+        Assert.AreEqual( expected: "high", actual: timeMap.Get( "love", 10 ) );
+        Assert.AreEqual( expected: "high", actual: timeMap.Get( "love", 15 ) );
+        Assert.AreEqual( expected: "low", actual: timeMap.Get( "love", 20 ) );
+        Assert.AreEqual( expected: "low", actual: timeMap.Get( "love", 25 ) );
+    }
+
+    [Test]
+    public void TimeBasedKeyValueStoreBinarySearchTest()
+    {
+        TimeMap timeMap = new();
+
+        timeMap.Set( key: "foo", value: "bar", timestamp: 1 );
+
+        Assert.AreEqual( expected: "bar", actual: timeMap.GetBinarySearch( "foo", 1 ) );
+        Assert.AreEqual( expected: "bar", actual: timeMap.GetBinarySearch( "foo", 3 ) );
+
+        timeMap.Set( "foo", "bar2", 4 );
+
+        Assert.AreEqual( expected: "bar2", actual: timeMap.GetBinarySearch( "foo", 4 ) );
+        Assert.AreEqual( expected: "bar2", actual: timeMap.GetBinarySearch( "foo", 5 ) );
+    }
+
+    [Test]
+    public void TimeBasedKeyValueStoreBinarySearchBorderChecksTest()
+    {
+        TimeMap timeMap = new();
+
+        timeMap.Set( key: "love", value: "high", timestamp: 10 );
+        timeMap.Set( key: "love", value: "low", timestamp: 20 );
+
+        Assert.AreEqual( expected: string.Empty, actual: timeMap.GetBinarySearch( "love", 5 ) );
+        Assert.AreEqual( expected: "high", actual: timeMap.GetBinarySearch( "love", 10 ) );
+        Assert.AreEqual( expected: "high", actual: timeMap.GetBinarySearch( "love", 15 ) );
+        Assert.AreEqual( expected: "low", actual: timeMap.GetBinarySearch( "love", 20 ) );
+        Assert.AreEqual( expected: "low", actual: timeMap.GetBinarySearch( "love", 25 ) );
+    }
+}
