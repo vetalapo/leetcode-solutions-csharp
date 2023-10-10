c3a1ba43150dbf0775ec1fa9d21a88d44deec794
Vitaly Dushkin <vetalapo@gmail.com>
2023-10-10 20:32:20 +0200

    Added solution for the problem #121: Best Time to Buy and Sell Stock

---
+++ src/Problems/sliding-window/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,42 @@
+﻿/**
+ * 121
+ * Best Time to Buy and Sell Stock
+ **
+ * You are given an array prices where prices[i] is the price of a given stock on the ith day.
+ * 
+ * You want to maximize your profit by choosing a single day to buy one stock
+ * and choosing a different day in the future to sell that stock.
+ * 
+ * Return the maximum profit you can achieve from this transaction.
+ * If you cannot achieve any profit, return 0.
+ * 
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ */
+
+namespace Problems;
+
+public class BestTimeToBuyAndSellStock
+{
+    public int MaxProfit( int[] prices )
+    {
+        int maxProfit = 0;
+        int left = 0;
+        int right = 1;
+
+        while ( right < prices.Length )
+        {
+            if ( prices[left] >= prices[right] )
+            {
+                left = right;
+            }
+            else
+            {
+                maxProfit = Math.Max( maxProfit, prices[right] - prices[left] );
+            }
+
+            right++;
+        }
+
+        return maxProfit;
+    }
+}
---
+++ src/Tests/sliding-window/BestTimeToBuyAndSellStockTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+[TestFixture]
+public class BestTimeToBuyAndSellStockTests
+{
+    [TestCase( new int[] { 7, 1, 5, 3, 6, 4 }, ExpectedResult = 5 )]
+    [TestCase( new int[] { 7, 6, 4, 3, 1 }, ExpectedResult = 0 )]
+    public int MaxProfitTest( int[] prices ) => new BestTimeToBuyAndSellStock().MaxProfit( prices );
+}
