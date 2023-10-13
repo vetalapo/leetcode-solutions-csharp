6612973e8778d84d2f92e8bd386edb3cf419bae1
Vitaly Dushkin <vetalapo@gmail.com>
2023-10-13 23:53:34 +0200

    Added tests for problem #424: Longest Repeating Character Replacement

---
+++ src/Problems/sliding-window/LongestRepeatingCharacterReplacement.cs
@@ -0,0 +1,24 @@
+﻿/**
+ * 424
+ * Longest Repeating Character Replacement
+ ** 
+ * You are given a string s and an integer k.
+ * You can choose any character of the string and change it to any other uppercase English character.
+ * You can perform this operation at most k times.
+ * 
+ * Return the length of the longest substring containing the same letter you can get after performing the above operations.
+ * 
+ * s consists of only uppercase English letters.
+ * 
+ * https://leetcode.com/problems/longest-repeating-character-replacement/description/
+ */
+
+namespace Problems;
+
+public class LongestRepeatingCharacterReplacement
+{
+    public int CharacterReplacement( string s, int k )
+    {
+        return 0;
+    }
+}
---
+++ src/Tests/sliding-window/LongestRepeatingCharacterReplacementTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+[TestFixture]
+public class LongestRepeatingCharacterReplacementTests
+{
+    [TestCase( "ABAB", 2, ExpectedResult = 4 )]
+    [TestCase( "AABABBA", 1, ExpectedResult = 4 )]
+    public int CharacterReplacementTest( string s, int k ) => new LongestRepeatingCharacterReplacement().CharacterReplacement( s, k );
+}
