8820fb0d34eeef2b1ced6098a0a70eab573029e0
Vitaly Dushkin <vetalapo@gmail.com>
2023-10-14 22:33:43 +0200

    Solved problem #567: Permutation in String

---
+++ src/Problems/sliding-window/PermutationInString.cs
@@ -0,0 +1,89 @@
+﻿/**
+ * 567
+ * Permutation in String
+ ** 
+ * Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+ * In other words, return true if one of s1's permutations is the substring of s2.
+ * 
+ * s1 and s2 consist of lowercase English letters.
+ * 
+ * https://leetcode.com/problems/permutation-in-string/
+ */
+
+namespace Problems;
+
+public class PermutationInString
+{
+    public bool CheckInclusion( string s1, string s2 )
+    {
+        // s1 must be less or equal s2, otherwise no inclusion
+        if ( s1.Length > s2.Length )
+        {
+            return false;
+        }
+
+        // Initial count
+        int[] s1Count = new int[26];
+        int[] s2PortionCount = new int[26];
+
+        for ( int i = 0; i < s1.Length; i++ )
+        {
+            s1Count[s1[i] - 'a']++;
+            s2PortionCount[s2[i] - 'a']++;
+        }
+
+        // Counting mathes on initial values
+        int matches = 0;
+
+        for ( int i = 0; i < 26; i++ )
+        {
+            if ( s1Count[i] == s2PortionCount[i] )
+            {
+                matches++;
+            }
+        }
+
+        // Matching the rest of s2
+        int left = 0;
+
+        for ( int right = s1.Length; right < s2.Length; right++ )
+        {
+            if ( matches == 26 )
+            {
+                return true;
+            }
+
+            // Extending window right
+            int index = s2[right] - 'a';
+
+            s2PortionCount[index]++;
+
+            if ( s1Count[index] == s2PortionCount[index] )
+            {
+                matches++;
+            }
+            else if ( s1Count[index] + 1 == s2PortionCount[index] )
+            {
+                matches--;
+            }
+
+            // Deleting left portion
+            index = s2[left] - 'a';
+
+            s2PortionCount[index]--;
+
+            if ( s1Count[index] == s2PortionCount[index] )
+            {
+                matches++;
+            }
+            else if ( s1Count[index] - 1 == s2PortionCount[index] )
+            {
+                matches--;
+            }
+
+            left++;
+        }
+
+        return matches == 26;
+    }
+}
---
+++ src/Tests/sliding-window/PermutationInStringTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+[TestFixture]
+public class PermutationInStringTests
+{
+    [TestCase( "ab", "eidbaooo", ExpectedResult = true )]
+    [TestCase( "ab", "eidboaoo", ExpectedResult = false )]
+    [TestCase( "abc", "bbbca", ExpectedResult = true )]
+    [TestCase( "hello", "ooolleoooleh", ExpectedResult = false )]
+    public bool CheckInclusionTest( string s1, string s2 ) => new PermutationInString().CheckInclusion( s1, s2 );
+}
