afa87f9754d16d8e1d3046ff1152c0f91d388659
Vitaly Dushkin <vetalapo@gmail.com>
2023-10-24 23:07:17 +0200

    # 239: Sliding Window Maximum. O(n) Solution and tests

---
+++ src/Problems/sliding-window/SlidingWindowMaximum.cs
@@ -0,0 +1,54 @@
+﻿/**
+ * 239
+ * Sliding Window Maximum
+ ** 
+ * You are given an array of integers nums,
+ * there is a sliding window of size k which is moving from the very left of the array to the very right.
+ * You can only see the k numbers in the window.
+ * Each time the sliding window moves right by one position.
+ * 
+ * Return the max sliding window.
+ * 
+ * https://leetcode.com/problems/sliding-window-maximum/
+ */
+
+namespace Problems;
+
+public class SlidingWindowMaximum
+{
+    public int[] MaxSlidingWindow( int[] nums, int k )
+    {
+        List<int> result = new();
+        LinkedList<int> queue = new();
+
+        int left = 0;
+        int right = 0;
+
+        while ( right < nums.Length )
+        {
+            // Removing smaller values from right, making sure we have max on the right
+            while ( queue.Count > 0 && nums[queue.Last.Value] < nums[right] )
+            {
+                queue.RemoveLast();
+            }
+
+            queue.AddLast( right );
+
+            // Removing left value from the window
+            if ( left > queue.First.Value )
+            {
+                queue.RemoveFirst();
+            }
+
+            if ( right + 1 >= k )
+            {
+                result.Add( nums[queue.First.Value] );
+                left++;
+            }
+
+            right++;
+        }
+
+        return result.ToArray();
+    }
+}
