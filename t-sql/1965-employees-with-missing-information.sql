79c613ce0324df2071094b25146a3e43bf550cc7
Vitaly Dushkin <vetalapo@gmail.com>
2023-12-05 22:42:58 +0100

    Add T-SQL Solution #1965: Employees with missing information

---
+++ t-sql/1965-employees-with-missing-information.sql
@@ -0,0 +1,75 @@
+﻿/**
+ * 1965
+ * Employees With Missing Information
+ **
+ * Table: Employees
+ * +-------------+---------+
+ * | Column Name | Type    |
+ * +-------------+---------+
+ * | employee_id | int     |
+ * | name        | varchar |
+ * +-------------+---------+
+ * employee_id is the column with unique values for this table.
+ * Each row of this table indicates the name of the employee whose ID is employee_id.
+ *
+ * Table: Salaries
+ *
+ * +-------------+---------+
+ * | Column Name | Type    |
+ * +-------------+---------+
+ * | employee_id | int     |
+ * | salary      | int     |
+ * +-------------+---------+
+ * employee_id is the column with unique values for this table.
+ * Each row of this table indicates the salary of the employee whose ID is employee_id.
+ * 
+ * Write a solution to report the IDs of all the employees with missing information. The information of an employee is missing if:
+ * 
+ * The employee's name is missing, or
+ * The employee's salary is missing.
+ * Return the result table ordered by employee_id in ascending order.
+ * 
+ * The result format is in the following example.
+ *
+ * Example 1:
+ * Input:
+ * Employees table:
+ * +-------------+----------+
+ * | employee_id | name     |
+ * +-------------+----------+
+ * | 2           | Crew     |
+ * | 4           | Haven    |
+ * | 5           | Kristian |
+ * +-------------+----------+
+ * Salaries table:
+ * +-------------+--------+
+ * | employee_id | salary |
+ * +-------------+--------+
+ * | 5           | 76071  |
+ * | 1           | 22517  |
+ * | 4           | 63539  |
+ * +-------------+--------+
+ * Output:
+ * +-------------+
+ * | employee_id |
+ * +-------------+
+ * | 1           |
+ * | 2           |
+ * +-------------+
+ * Explanation:
+ * Employees 1, 2, 4, and 5 are working at this company.
+ * The name of employee 1 is missing.
+ * The salary of employee 2 is missing.
+ **
+ * https://leetcode.com/problems/employees-with-missing-information/
+ */
+ 
+SELECT s.employee_id
+FROM Salaries AS s
+LEFT JOIN Employees AS e ON e.employee_id = s.employee_id
+WHERE name IS NULL
+UNION
+SELECT e.employee_id
+FROM Employees AS e
+LEFT JOIN Salaries AS s ON s.employee_id = e.employee_id
+WHERE salary IS NULL;
