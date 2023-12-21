0e74e8c1926fc49b092cecf0185417e66ee1d787
Vitaly Dushkin <vetalapo@gmail.com>
2023-12-21 12:48:01 +0100

    T-SQL solution #196: Delete duplicate emails

---
+++ t-sql/0196-delete-duplicate-emails.sql
@@ -0,0 +1,57 @@
+﻿/**
+ * 196
+ * Delete Duplicate Emails
+ **
+ * Table: Person
+ * +-------------+---------+
+ * | Column Name | Type    |
+ * +-------------+---------+
+ * | id          | int     |
+ * | email       | varchar |
+ * +-------------+---------+
+ * id is the primary key (column with unique values) for this table.
+ * Each row of this table contains an email. The emails will not contain uppercase letters.
+ *
+ * Write a solution to delete all duplicate emails, keeping only one unique email with the smallest id.
+ * For SQL users, please note that you are supposed to write a DELETE statement and not a SELECT one.
+ * For Pandas users, please note that you are supposed to modify Person in place.
+ *
+ * After running your script, the answer shown is the Person table.
+ * The driver will first compile and run your piece of code and then show the Person table.
+ * The final order of the Person table does not matter.
+ * The result format is in the following example.
+ *
+ * Example 1:
+ * Input:
+ * Person table:
+ * +----+------------------+
+ * | id | email            |
+ * +----+------------------+
+ * | 1  | john@example.com |
+ * | 2  | bob@example.com  |
+ * | 3  | john@example.com |
+ * +----+------------------+
+ * Output:
+ * +----+------------------+
+ * | id | email            |
+ * +----+------------------+
+ * | 1  | john@example.com |
+ * | 2  | bob@example.com  |
+ * +----+------------------+
+ * Explanation: john@example.com is repeated two times. We keep the row with the smallest Id = 1.
+ **
+ * https://leetcode.com/problems/delete-duplicate-emails/
+ */
+ 
+DELETE FROM Person
+WHERE id IN (
+  SELECT id
+  FROM (
+    SELECT
+      email,
+      id,
+      ROW_NUMBER() OVER(PARTITION BY email ORDER BY id) AS row_num
+    FROM Person
+  ) AS Person_Num
+  WHERE row_num > 1
+);
