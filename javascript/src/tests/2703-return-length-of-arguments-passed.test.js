47edf0ed4a1018d17e99b20f21365a4128f1f6e2
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-03 20:56:46 +0100

    JS #2703: Return length of arguments passed

---
+++ javascript/src/tests/2703-return-length-of-arguments-passed.test.js
@@ -0,0 +1,21 @@
+const argumentsLength = require("../2703-return-length-of-arguments-passed");
+
+test("Empty array", () => {
+  expect(argumentsLength([])).toBe(1);
+});
+
+test("List a few arguments", () => {
+  expect(argumentsLength(1, 2, 3)).toBe(3);
+});
+
+test("Single argument, array with one item", () => {
+  expect(argumentsLength([5])).toBe(1);
+});
+
+test("Single argument, array with a few items", () => {
+  expect(argumentsLength([5])).toBe(1);
+});
+
+test("No arguments", () => {
+  expect(argumentsLength()).toBe(0);
+});
