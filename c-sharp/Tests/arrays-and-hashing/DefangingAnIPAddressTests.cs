2e1dd5765e3c814c8ef1ef6ef2441afa4e11caed
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-09 17:39:07 +0100

    C-Sharp: #1108: defanging an IP Address

---
+++ c-sharp/Tests/arrays-and-hashing/DefangingAnIPAddressTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+[TestFixture]
+public class DefangingAnIPAddressTests
+{
+    [TestCase( "1.1.1.1", ExpectedResult = "1[.]1[.]1[.]1" )]
+    [TestCase( "255.100.50.0", ExpectedResult = "255[.]100[.]50[.]0" )]
+    public string DefangIPaddrTest( string address ) => new DefangingAnIPAddress().DefangIPaddr( address );
+}
