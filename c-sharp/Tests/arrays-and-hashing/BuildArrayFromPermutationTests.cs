0b441f2de817d217d57f7d8b3c00c4cd3c6db90a
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-09 18:01:31 +0100

    C-Sharp #1920: Build Array from Permutation

---
+++ c-sharp/Tests/arrays-and-hashing/BuildArrayFromPermutationTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+[TestFixture]
+public class BuildArrayFromPermutationTests
+{
+    [TestCase( new int[] { 0, 2, 1, 5, 3, 4 }, ExpectedResult = new int[] { 0, 1, 2, 4, 5, 3 } )]
+    [TestCase( new int[] { 5, 0, 1, 2, 3, 4 }, ExpectedResult = new int[] { 4, 5, 0, 1, 2, 3 } )]
+    public int[] BuildArrayTest( int[] nums ) => new BuildArrayFromPermutation().BuildArray( nums );
+}
