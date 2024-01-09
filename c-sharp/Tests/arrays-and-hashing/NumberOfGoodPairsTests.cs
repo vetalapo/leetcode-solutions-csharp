4fcf8f8677111bef5de5688a1b20159204153c81
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-09 17:49:37 +0100

    C-Sharp #1512: Number of Good Pairs

---
+++ c-sharp/Tests/arrays-and-hashing/NumberOfGoodPairsTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+[TestFixture]
+public class NumberOfGoodPairsTests
+{
+    [TestCase( new int[] { 1, 2, 3, 1, 1, 3 }, ExpectedResult = 4 )]
+    [TestCase( new int[] { 1, 1, 1, 1 }, ExpectedResult = 6 )]
+    [TestCase( new int[] { 1, 2, 3 }, ExpectedResult = 0 )]
+    public int NumIdenticalPairsTest( int[] nums ) => new NumberOfGoodPairs().NumIdenticalPairs( nums );
+}
