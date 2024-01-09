d2bee8dfd919c2abaeaf5c62f727f9f152fd0436
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-09 05:10:26 +0100

    T-SQL #608 description update

--- t-sql/0608-tree-node.sql
+++ t-sql/0608-tree-node.sql
@@ -24,51 +24,58 @@
  * The result format is in the following example.
  *
  * Example 1:
- * 
- * Input:
- * Tree table:
- * +----+------+
- * | id | p_id |
- * +----+------+
- * | 1  | null |
- * | 2  | 1    |
- * | 3  | 1    |
- * | 4  | 2    |
- * | 5  | 2    |
- * +----+------+
- * Output: 
- * +----+-------+
- * | id | type  |
- * +----+-------+
- * | 1  | Root  |
- * | 2  | Inner |
- * | 3  | Leaf  |
- * | 4  | Leaf  |
- * | 5  | Leaf  |
- * +----+-------+
- * Explanation: 
- * Node 1 is the root node because its parent node is null and it has child nodes 2 and 3.
- * Node 2 is an inner node because it has parent node 1 and child node 4 and 5.
- * Nodes 3, 4, and 5 are leaf nodes because they have parent nodes and they do not have child nodes.
+ *
+ *       1
+ *      / \
+ *     2   3
+ *    / \
+ *   4   5
+ *
+ *   Input:
+ *   Tree table:
+ *   +----+------+
+ *   | id | p_id |
+ *   +----+------+
+ *   | 1  | null |
+ *   | 2  | 1    |
+ *   | 3  | 1    |
+ *   | 4  | 2    |
+ *   | 5  | 2    |
+ *   +----+------+
+ *   
+ *   Output: 
+ *   +----+-------+
+ *   | id | type  |
+ *   +----+-------+
+ *   | 1  | Root  |
+ *   | 2  | Inner |
+ *   | 3  | Leaf  |
+ *   | 4  | Leaf  |
+ *   | 5  | Leaf  |
+ *   +----+-------+
+ *
+ *   Explanation: 
+ *   Node 1 is the root node because its parent node is null and it has child nodes 2 and 3.
+ *   Node 2 is an inner node because it has parent node 1 and child node 4 and 5.
+ *   Nodes 3, 4, and 5 are leaf nodes because they have parent nodes and they do not have child nodes.
+ *
  * Example 2:
- * 
- * 
- * Input: 
- * Tree table:
- * +----+------+
- * | id | p_id |
- * +----+------+
- * | 1  | null |
- * +----+------+
- *
- * Output: 
- * +----+-------+
- * | id | type  |
- * +----+-------+
- * | 1  | Root  |
- * +----+-------+
- *
- * Explanation: If there is only one node on the tree, you only need to output its root attributes.
+ *   Input: 
+ *   Tree table:
+ *   +----+------+
+ *   | id | p_id |
+ *   +----+------+
+ *   | 1  | null |
+ *   +----+------+
+ *   
+ *   Output: 
+ *   +----+-------+
+ *   | id | type  |
+ *   +----+-------+
+ *   | 1  | Root  |
+ *   +----+-------+
+ *   
+ *   Explanation: If there is only one node on the tree, you only need to output its root attributes.
  **
  * https://leetcode.com/problems/tree-node/description/
 **/
