b0f469acbd72e32fc03d7bed6dd2d5e5cde71eb8
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-10 23:50:43 +0100

    C-Sharp #2769: Find the Maximum Achievable Number

---
+++ c-sharp/Tests/math/FindTheMaximumAchievableNumberTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+[TestFixture]
+public class FindTheMaximumAchievableNumberTests
+{
+    [TestCase( 4, 1, ExpectedResult = 6 )]
+    [TestCase( 3, 2, ExpectedResult = 7 )]
+    public int TheMaximumAchievableXTest( int num, int t ) => new FindTheMaximumAchievableNumber().TheMaximumAchievableX( num, t );
+}
