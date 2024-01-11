92eb9d0dccddd84376f6153e76921f1ca9538a15
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-11 23:24:19 +0100

    C-Sharp #2469: Convert the Temperature

---
+++ c-sharp/Tests/math/ConvertTheTemperatureTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+[TestFixture]
+public class ConvertTheTemperatureTests
+{
+    [TestCase( 36.50, ExpectedResult = new double[] { 309.65000, 97.70000 } )]
+    [TestCase( 122.11, ExpectedResult = new double[] { 395.26000, 251.79800 } )]
+    public double[] ConvertTemperatureTest( double celsius ) => new ConvertTheTemperature().ConvertTemperature( celsius );
+}
