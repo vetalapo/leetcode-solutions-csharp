93772fbd2afd49f8f62bf2e4fc9db022b81580f5
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-13 16:24:43 +0100

    pandas #2881: Create a New Column

---
+++ pandas/src/2881-create-a-new-column.py
@@ -0,0 +1,61 @@
+﻿##
+# 2881
+# Create a New Column
+##
+# DataFrame employees
+#   +-------------+--------+
+#   | Column Name | Type.  |
+#   +-------------+--------+
+#   | name        | object |
+#   | salary      | int.   |
+#   +-------------+--------+
+#
+# A company plans to provide its employees with a bonus.
+# Write a solution to create a new column name bonus that contains the doubled values of the salary column.
+#
+# The result format is in the following example.
+#
+# Example 1:
+#   Input:
+#   DataFrame employees
+#   +---------+--------+
+#   | name    | salary |
+#   +---------+--------+
+#   | Piper   | 4548   |
+#   | Grace   | 28150  |
+#   | Georgia | 1103   |
+#   | Willow  | 6593   |
+#   | Finn    | 74576  |
+#   | Thomas  | 24433  |
+#   +---------+--------+
+#
+#   Output:
+#   +---------+--------+--------+
+#   | name    | salary | bonus  |
+#   +---------+--------+--------+
+#   | Piper   | 4548   | 9096   |
+#   | Grace   | 28150  | 56300  |
+#   | Georgia | 1103   | 2206   |
+#   | Willow  | 6593   | 13186  |
+#   | Finn    | 74576  | 149152 |
+#   | Thomas  | 24433  | 48866  |
+#   +---------+--------+--------+
+#
+#   Explanation: A new column bonus is created by doubling the value in the column salary.
+#
+# Hint: Consider using the `[]` brackets with the new column name at the left side of the assignment.
+# The calculation of the value is done element-wise.
+##
+# https://leetcode.com/problems/create-a-new-column/
+##
+
+import sys
+
+# Preventing creation of __pycache__
+sys.dont_write_bytecode = True
+
+import pandas as pd
+
+def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees["bonus"] = employees.salary * 2
+    return employees
---
+++ pandas/src/tests/test_2881_create_a_new_column.py
@@ -0,0 +1,33 @@
+﻿import sys
+
+# Preventing creation of __pycache__
+sys.dont_write_bytecode = True
+
+import importlib
+import unittest
+import pandas as pd
+
+# Python really doesn't like paths with "-" and "/"
+# I don't like Python not liking it so
+# have to use importlib to import modules
+unit = importlib.import_module("src.2881-create-a-new-column")
+
+sample_df = pd.DataFrame({
+    "name": ["Piper", "Grace", "Georgia", "Willow", "Finn", "Thomas"],
+    "salary": [4548, 28150, 1103, 6593, 74576, 24433]
+})
+
+expected_df = pd.DataFrame({
+    "name": ["Piper", "Grace", "Georgia", "Willow", "Finn", "Thomas"],
+    "salary": [4548, 28150, 1103, 6593, 74576, 24433],
+    "bonus": [9096, 56300, 2206, 13186, 149152, 48866]
+})
+
+class TestCreateBonusColumn(unittest.TestCase):
+    def test_createBonusColumn(self):
+        actual_df = unit.createBonusColumn(sample_df)
+        
+        pd.testing.assert_frame_equal(actual_df, expected_df)
+
+if __name__ == "__main__":
+    unittest.main()
