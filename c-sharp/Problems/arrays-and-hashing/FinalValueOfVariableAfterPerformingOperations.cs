995b3d33a543961263cba9b7fed8a1c09fd1ac0e
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-14 17:56:48 +0100

    C-Sharp #2011: Final Value of Variable After Performing Operations

---
+++ c-sharp/Problems/arrays-and-hashing/FinalValueOfVariableAfterPerformingOperations.cs
@@ -0,0 +1,74 @@
+﻿/**
+ * 2011
+ * Final Value of Variable After Performing Operations
+ **
+ * There is a programming language with only four operations and one variable X:
+ *   ++X and X++ increments the value of the variable X by 1.
+ *   --X and X-- decrements the value of the variable X by 1.
+ *
+ * Initially, the value of X is 0.
+ * Given an array of strings operations containing a list of operations,
+ * return the final value of X after performing all the operations.
+ * 
+ * Example 1:
+ *   Input: operations = ["--X","X++","X++"]
+ *   Output: 1
+ *   Explanation: The operations are performed as follows:
+ *     Initially, X = 0.
+ *     --X: X is decremented by 1, X =  0 - 1 = -1.
+ *     X++: X is incremented by 1, X = -1 + 1 =  0.
+ *     X++: X is incremented by 1, X =  0 + 1 =  1.
+ *
+ *
+ * Example 2:
+ *   Input: operations = ["++X","++X","X++"]
+ *   Output: 3
+ *   Explanation: The operations are performed as follows:
+ *     Initially, X = 0.
+ *     ++X: X is incremented by 1, X = 0 + 1 = 1.
+ *     ++X: X is incremented by 1, X = 1 + 1 = 2.
+ *     X++: X is incremented by 1, X = 2 + 1 = 3.
+ *
+ *
+ * Example 3:
+ *   Input: operations = ["X++","++X","--X","X--"]
+ *   Output: 0
+ *   Explanation: The operations are performed as follows:
+ *     Initially, X = 0.
+ *     X++: X is incremented by 1, X = 0 + 1 = 1.
+ *     ++X: X is incremented by 1, X = 1 + 1 = 2.
+ *     --X: X is decremented by 1, X = 2 - 1 = 1.
+ *     X--: X is decremented by 1, X = 1 - 1 = 0.
+ * 
+ * Hint 1: There are only two operations to keep track of.
+ * Hint 2: Use a variable to store the value after each operation.
+ **
+ * https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+***/
+
+namespace Problems;
+
+public class FinalValueOfVariableAfterPerformingOperations
+{
+    public int FinalValueAfterOperations( string[] operations )
+    {
+        int x = 0;
+
+        foreach ( string operation in operations )
+        {
+            switch ( operation )
+            {
+                case "++X":
+                case "X++":
+                    x++;
+                    break;
+                case "--X":
+                case "X--":
+                    x--;
+                    break;
+            }
+        }
+
+        return x;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/FinalValueOfVariableAfterPerformingOperationsTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+[TestFixture]
+public class FinalValueOfVariableAfterPerformingOperationsTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int FinalValueAfterOperationsTest( string[] operations ) =>
+        new FinalValueOfVariableAfterPerformingOperations().FinalValueAfterOperations( operations );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( new object[] { new string[] { "--X", "X++", "X++" } } ).Returns( 1 );
+            yield return new TestCaseData( new object[] { new string[] { "++X", "++X", "X++" } } ).Returns( 3 );
+            yield return new TestCaseData( new object[] { new string[] { "X++", "++X", "--X", "X--" } } ).Returns( 0 );
+        }
+    }
+}
