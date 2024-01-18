d371dab0e1e062d80a8522434356a37313f627cf
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-18 18:43:48 +0100

    C-Sharp #1603: Design parking system

---
+++ c-sharp/Tests/arrays-and-hashing/ParkingSystemTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+[TestFixture]
+public class ParkingSystemTests
+{
+    [Test]
+    public void AddCarTest()
+    {
+        ParkingSystem parkingSystem = new( 1, 1, 0 );
+
+        Assert.IsTrue( parkingSystem.AddCar( 1 ) );
+        Assert.IsTrue( parkingSystem.AddCar( 2 ) );
+        Assert.IsFalse( parkingSystem.AddCar( 3 ) );
+        Assert.IsFalse( parkingSystem.AddCar( 1 ) );
+    }
+}
