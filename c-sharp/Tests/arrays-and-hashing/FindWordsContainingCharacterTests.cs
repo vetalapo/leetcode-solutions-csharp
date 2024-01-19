a73435043fa0b453d316fd9be74718b0ddbd5a1b
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-19 15:53:27 +0100

    C-Sharp #2942: Find Words Containing Character

---
+++ c-sharp/Tests/arrays-and-hashing/FindWordsContainingCharacterTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+[TestFixture]
+public class FindWordsContainingCharacterTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public IList<int> FindWordsContainingTest( string[] words, char x ) =>
+        new FindWordsContainingCharacter().FindWordsContaining( words, x );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( new string[] { "leet", "code" }, 'e' ).Returns( new List<int> { 0, 1 } );
+            yield return new TestCaseData( new string[] { "abc", "bcd", "aaaa", "cbc" }, 'a' ).Returns( new List<int> { 0, 2 } );
+            yield return new TestCaseData( new string[] { "abc", "bcd", "aaaa", "cbc" }, 'z' ).Returns( new List<int> { } );
+        }
+    }
+}
