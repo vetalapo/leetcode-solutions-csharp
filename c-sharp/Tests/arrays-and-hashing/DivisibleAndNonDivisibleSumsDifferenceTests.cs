152d2028fb8af6268050b82c5d26ac477535c966
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-20 23:32:47 +0100

    C-Sharp #2894: Divisible and non-divisible sums difference

---
+++ c-sharp/Tests/arrays-and-hashing/DivisibleAndNonDivisibleSumsDifferenceTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+[TestFixture]
+public class DivisibleAndNonDivisibleSumsDifferenceTests
+{
+    [TestCase( 10, 3, ExpectedResult = 19 )]
+    [TestCase( 5, 6, ExpectedResult = 15 )]
+    [TestCase( 5, 1, ExpectedResult = -15 )]
+    public int DifferenceOfSumsTest( int n, int m ) => new DivisibleAndNonDivisibleSumsDifference().DifferenceOfSums( n, m );
+}
