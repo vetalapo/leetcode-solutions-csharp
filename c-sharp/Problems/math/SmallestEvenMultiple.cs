b3b04aa19d68ce3f8feebfc380b56656baa1a8c4
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-21 23:29:59 +0100

    C-Sharp #2413: Samllest Even Multiple

---
+++ c-sharp/Problems/math/SmallestEvenMultiple.cs
@@ -0,0 +1,41 @@
+﻿/**
+ * 2413
+ * Smallest Even Multiple
+ **
+ * Given a positive integer n,
+ * return the smallest positive integer that is a multiple of both 2 and n.
+ * 
+ * Example 1:
+ *   Input: n = 5
+ *   Output: 10
+ *   Explanation: The smallest multiple of both 5 and 2 is 10.
+ * 
+ * Example 2:
+ *   Input: n = 6
+ *   Output: 6
+ *   Explanation: The smallest multiple of both 6 and 2 is 6. Note that a number is a multiple of itself.
+ *  
+ * 
+ * Constraints: 1 <= n <= 150
+ * 
+ * Hint 1:
+ *   A guaranteed way to find a multiple of 2 and n is to multiply them together.
+ *   When is this the answer, and when is there a smaller answer?
+ * 
+ * Hint 2:
+ *   There is a smaller answer when n is even.
+ **
+ * https://leetcode.com/problems/smallest-even-multiple/
+***/
+
+namespace Problems;
+
+public class SmallestEvenMultipleSolution
+{
+    public int SmallestEvenMultiple( int n )
+    {
+        return n % 2 == 0
+            ? n
+            : n << 1;
+    }
+}
---
+++ c-sharp/Tests/math/SmallestEvenMultipleSolutionTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+[TestFixture]
+public class SmallestEvenMultipleSolutionTests
+{
+    [TestCase( 5, ExpectedResult = 10 )]
+    [TestCase( 6, ExpectedResult = 6 )]
+    public int SmallestEvenMultipleTest( int n ) => new SmallestEvenMultipleSolution().SmallestEvenMultiple( n );
+}
