ae2bea115f119bf4f6917153376deaa7c6f26bad
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-22 16:41:01 +0100

    C-Sharp #217 new solution added

--- c-sharp/Problems/arrays-and-hashing/ContainsDuplicate.cs
+++ c-sharp/Problems/arrays-and-hashing/ContainsDuplicate.cs
@@ -6,8 +6,24 @@
  * return true if any value appears at least twice in the array,
  * and return false if every element is distinct.
  * 
+ * Example 1:
+ *   Input: nums = [1,2,3,1]
+ *   Output: true
+ *   
+ * Example 2:
+ *   Input: nums = [1,2,3,4]
+ *   Output: false
+ * 
+ * Example 3:
+ *   Input: nums = [1,1,1,3,3,4,3,2,4,2]
+ *   Output: true
+ *
+ * Constraints:
+ *   1 <= nums.length <= 105
+ *   -109 <= nums[i] <= 109
+ **
  * https://leetcode.com/problems/contains-duplicate/
- */
+***/
 
 namespace Problems;
 
@@ -16,8 +32,8 @@ public class ContainsDuplicateSolution
     public bool ContainsDuplicate( int[] nums )
     {
         HashSet<int> set = new();
-        
-        foreach( int i in nums )
+
+        foreach ( int i in nums )
         {
             if ( set.Contains( i ) )
             {
@@ -28,5 +44,10 @@ public class ContainsDuplicateSolution
         }
 
         return false;
+    }
+
+    public bool ContainsDuplicateAlt( int[] nums )
+    {
+        return new HashSet<int>().Count == nums.Length;
     }
 }
--- c-sharp/Tests/arrays-and-hashing/ContainsDuplicateTests.cs
+++ c-sharp/Tests/arrays-and-hashing/ContainsDuplicateTests.cs
@@ -1,12 +1,27 @@
-﻿using NUnit.Framework;
+﻿using System.Collections;
+
+using NUnit.Framework;
 
 using Problems;
 
 [TestFixture]
 public class ContainsDuplicateTests
 {
-    [TestCase( new int[] { 1, 2, 3, 1 }, ExpectedResult = true )]
-    [TestCase( new int[] { 1, 2, 3, 4 }, ExpectedResult = false )]
-    [TestCase( new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }, ExpectedResult = true )]
+    [TestCaseSource( nameof( TestCases ) )]
     public bool ContainsDuplicateTest( int[] nums ) => new ContainsDuplicateSolution().ContainsDuplicate( nums );
+
+
+    [TestCaseSource( nameof( TestCases ) )]
+    public bool ContainsDuplicateAltTest( int[] nums ) => new ContainsDuplicateSolution().ContainsDuplicateAlt( nums );
+
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( new int[] { 1, 2, 3, 1 } ).Returns( true );
+            yield return new TestCaseData( new int[] { 1, 2, 3, 4 } ).Returns( false );
+            yield return new TestCaseData( new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 } ).Returns( true );
+        }
+    }
 }
