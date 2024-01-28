5ed38ae8233d56e15aa218022fda37eb63077845
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-28 16:13:24 +0100

    C-Sharp #344: Reverse string

---
+++ c-sharp/Problems/two-pointers/ReverseString.cs
@@ -0,0 +1,42 @@
+﻿/**
+ * 344
+ * Reverse String
+ **
+ * Write a function that reverses a string. The input string is given as an array of characters s.
+ * You must do this by modifying the input array in-place with O(1) extra memory.
+ * 
+ * Example 1:
+ *   Input: s = ["h","e","l","l","o"]
+ *   Output: ["o","l","l","e","h"]
+ * 
+ * Example 2:
+ *   Input: s = ["H","a","n","n","a","h"]
+ *   Output: ["h","a","n","n","a","H"]
+ * 
+ * Constraints:
+ *   • 1 <= s.length <= 105
+ *   • s[i] is a printable ascii character.
+ *
+ * Hint 1: The entire logic for reversing a string is based on using the opposite directional two-pointer approach!
+ **
+ * https://leetcode.com/problems/reverse-string/
+***/
+
+namespace Problems;
+
+public class ReverseStringSolution
+{
+    public void ReverseString( char[] s )
+    {
+        int left = 0;
+        int right = s.Length - 1;
+
+        while ( left < right )
+        {
+            (s[left], s[right]) = (s[right], s[left]);
+
+            left++;
+            right--;
+        }
+    }
+}
---
+++ c-sharp/Tests/two-pointers/ReverseStringSolutionTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+[TestFixture]
+public class ReverseStringSolutionTest
+{
+    [TestCase( new char[] { 'h', 'e', 'l', 'l', 'o' }, new char[] { 'o', 'l', 'l', 'e', 'h' } )]
+    [TestCase( new char[] { 'H', 'a', 'n', 'n', 'a', 'h' }, new char[] { 'h', 'a', 'n', 'n', 'a', 'H' } )]
+    public void ReverseStringTest( char[] s, char[] expectedResult )
+    {
+        new ReverseStringSolution().ReverseString( s );
+
+        Assert.AreEqual( s, expectedResult );
+    }
+}
