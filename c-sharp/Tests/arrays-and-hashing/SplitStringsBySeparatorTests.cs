1441f3a4c9312007f4cf1573e378c068be1787e8
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-29 13:11:00 +0100

    C-Sharp #2788: Split string by separator

---
+++ c-sharp/Tests/arrays-and-hashing/SplitStringsBySeparatorTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+[TestFixture]
+public class SplitStringsBySeparatorTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public IList<string> SplitWordsBySeparatorTest( IList<string> words, char separator ) =>
+        new SplitStringsBySeparator().SplitWordsBySeparator( words, separator );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( new List<string>() { "one.two.three", "four.five", "six" }, '.' )
+                .Returns( new List<string>() { "one", "two", "three", "four", "five", "six" } );
+
+            yield return new TestCaseData( new List<string>() { "$easy$", "$problem$" }, '$' )
+                .Returns( new List<string>() { "easy", "problem" } );
+
+            yield return new TestCaseData( new List<string>() { "|||" }, '|' )
+                .Returns( new List<string>() { } );
+        }
+    }
+}
