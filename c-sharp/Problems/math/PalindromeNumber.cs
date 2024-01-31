cdf24a50e0e2d1388eb2fa03ee4b62542ce7d11d
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-31 17:18:45 +0100

    C-Sharp #9: Palindrome Number

---
+++ c-sharp/Problems/math/PalindromeNumber.cs
@@ -0,0 +1,61 @@
+﻿/**
+ * 9
+ * Palindrome Number
+ **
+ * Given an integer x, return true if x is a palindrome, and false otherwise.
+ *
+ * Example 1:
+ *   Input: x = 121
+ *   Output: true
+ *   Explanation: 121 reads as 121 from left to right and from right to left.
+ *
+ * Example 2:
+ *   Input: x = -121
+ *   Output: false
+ *   Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+ *
+ * Example 3:
+ *   Input: x = 10
+ *   Output: false
+ *   Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ *
+ *
+ * Constraints:
+ *   -231 <= x <= 231 - 1
+ *
+ *
+ * Follow up: Could you solve it without converting the integer to a string?
+ * 
+ * Hint 1: Beware of overflow when you reverse the integer.
+ **
+ * https://leetcode.com/problems/palindrome-number/
+***/
+
+namespace Problems;
+
+public class PalindromeNumber
+{
+    public bool IsPalindrome( int x )
+    {
+        if ( x < 0 )
+        {
+            return false;
+        }
+
+        if ( x < 10 )
+        {
+            return true;
+        }
+
+        int xCopy = x;
+        int xReversed = 0;
+
+        while ( xCopy > 0 )
+        {
+            xReversed = ( xReversed * 10 ) + ( xCopy % 10 );
+            xCopy /= 10;
+        }
+
+        return x == xReversed;
+    }
+}
---
+++ c-sharp/Tests/math/PalindromeNumberTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class PalindromeNumberTests
+{
+    [TestCase( 121, ExpectedResult = true )]
+    [TestCase( -121, ExpectedResult = false )]
+    [TestCase( 10, ExpectedResult = false )]
+    public bool IsPalindromeTest( int x ) => new PalindromeNumber().IsPalindrome( x );
+}
