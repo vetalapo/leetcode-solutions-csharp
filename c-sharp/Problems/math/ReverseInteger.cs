ae6e810673fb4e294cdfc7f97f1be552ce87bbc4
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-02 14:06:50 +0100

    C-Sharp #7: Reverse Integer

---
+++ c-sharp/Problems/math/ReverseInteger.cs
@@ -0,0 +1,52 @@
+﻿/**
+ * 7
+ * Reverse Integer
+ **
+ * Given a signed 32-bit integer x, return x with its digits reversed.
+ * If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+ * Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+ *
+ * Example 1:
+ *   Input: x = 123
+ *   Output: 321
+ *
+ * Example 2:
+ *   Input: x = -123
+ *   Output: -321
+ *
+ * Example 3:
+ *   Input: x = 120
+ *   Output: 21
+ *
+ * Constraints: -231 <= x <= 231 - 1
+ **
+ * https://leetcode.com/problems/reverse-integer/
+***/
+
+namespace Problems;
+
+public class ReverseInteger
+{
+    public int Reverse( int x )
+    {
+        int result = 0;
+
+        try
+        {
+            checked
+            {
+                while ( Math.Abs( x ) > 0 )
+                {
+                    result = result * 10 + ( x % 10 );
+                    x /= 10;
+                }
+            }
+        }
+        catch
+        {
+            return 0;
+        }
+
+        return result;
+    }
+}
---
+++ c-sharp/Tests/math/ReverseIntegerTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Problems.Tests
+{
+    [TestFixture]
+    public class ReverseIntegerTests
+    {
+        [TestCase( 123, ExpectedResult = 321 )]
+        [TestCase( -123, ExpectedResult = -321 )]
+        [TestCase( 120, ExpectedResult = 21 )]
+        [TestCase( 1534236469, ExpectedResult = 0 )]
+        public int ReverseTest( int x ) => new ReverseInteger().Reverse( x );
+    }
+}
