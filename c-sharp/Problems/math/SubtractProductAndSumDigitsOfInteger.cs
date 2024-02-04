155418161787fd264a70938f0bb0a57751cb896d
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-04 16:48:13 +0100

    C-Sharp #1281: Subtract the Product and Sum of Digits of an Integer

---
+++ c-sharp/Problems/math/SubtractProductAndSumDigitsOfInteger.cs
@@ -0,0 +1,53 @@
+﻿/**
+ * 1281
+ * Subtract the Product and Sum of Digits of an Integer
+ **
+ * Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+ *
+ * Example 1:
+ *   Input: n = 234
+ *   Output: 15 
+ *   Explanation: 
+ *     Product of digits = 2 * 3 * 4 = 24 
+ *     Sum of digits = 2 + 3 + 4 = 9 
+ *     Result = 24 - 9 = 15
+ * 
+ * Example 2:
+ *   Input: n = 4421
+ *   Output: 21
+ *   Explanation: 
+ *     Product of digits = 4 * 4 * 2 * 1 = 32 
+ *     Sum of digits = 4 + 4 + 2 + 1 = 11 
+ *     Result = 32 - 11 = 21
+ *
+ * Constraints: 1 <= n <= 10^5
+ * 
+ * Hint 1: How to compute all digits of the number ?
+ * Hint 2: Use modulus operator (%) to compute the last digit.
+ * Hint 3: Generalise modulus operator idea to compute all digits.
+ **
+ * https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+***/
+
+namespace Problems;
+
+public class SubtractProductAndSumDigitsOfInteger
+{
+    public int SubtractProductAndSum( int n )
+    {
+        int product = 1;
+        int sum = 0;
+
+        while ( n > 0 )
+        {
+            int lastDigit = n % 10;
+
+            product *= lastDigit;
+            sum += lastDigit;
+
+            n /= 10;
+        }
+
+        return product - sum;
+    }
+}
---
+++ c-sharp/Tests/math/SubtractProductAndSumDigitsOfIntegerTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+
+[TestFixture]
+public class SubtractProductAndSumDigitsOfIntegerTests
+{
+    [TestCase( 234, ExpectedResult = 15 )]
+    [TestCase( 4421, ExpectedResult = 21 )]
+    public int SubtractProductAndSumTest( int n ) => new SubtractProductAndSumDigitsOfInteger().SubtractProductAndSum( n );
+}
