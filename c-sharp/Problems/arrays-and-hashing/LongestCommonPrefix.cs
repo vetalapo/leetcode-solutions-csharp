6398a7df72f81fb23eb390be992efcd43e1b394f
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-05 15:51:41 +0100

    C-Sharp #14: Longest Common Prefix

---
+++ c-sharp/Problems/arrays-and-hashing/LongestCommonPrefix.cs
@@ -0,0 +1,53 @@
+﻿/**
+ * 14
+ * Longest Common Prefix
+ **
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ * If there is no common prefix, return an empty string "".
+ *
+ * Example 1:
+ *   Input: strs = ["flower","flow","flight"]
+ *   Output: "fl"
+ * 
+ * Example 2:
+ *   Input: strs = ["dog","racecar","car"]
+ *   Output: ""
+ *   Explanation: There is no common prefix among the input strings.
+ *
+ * Constraints:
+ *   • 1 <= strs.length <= 200
+ *   • 0 <= strs[i].length <= 200
+ *   • strs[i] consists of only lowercase English letters.
+ **
+ * https://leetcode.com/problems/longest-common-prefix/
+***/
+
+namespace Problems;
+
+public class LongestCommonPrefixSolution
+{
+    public string LongestCommonPrefix( string[] strs )
+    {
+        int right = 0;
+        bool keepLooping = true;
+
+        while ( right < strs[0].Length && keepLooping )
+        {
+            for ( int i = 1; i < strs.Length; i++ )
+            {
+                string word = strs[i];
+
+                if ( right >= word.Length || strs[0][right] != word[right] )
+                {
+                    keepLooping = false;
+                    right--;
+                    break;
+                }
+            }
+
+            right++;
+        }
+
+        return strs[0][..right];
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/LongestCommonPrefixSolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class LongestCommonPrefixSolutionTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public string LongestCommonPrefixTest( string[] strs ) =>
+        new LongestCommonPrefixSolution().LongestCommonPrefix( strs );
+
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( new object[] { new string[] { "flower", "flow", "flight" } } ).Returns( "fl" );
+
+            yield return new TestCaseData( new object[] { new string[] { "flower", "flower", "flower" } } ).Returns( "flower" );
+
+            yield return new TestCaseData( new object[] { new string[] { "dog", "racecar", "car" } } ).Returns( string.Empty );
+        }
+    }
+}
