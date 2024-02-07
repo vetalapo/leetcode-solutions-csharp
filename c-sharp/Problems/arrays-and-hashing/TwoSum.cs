30e5fcf9736e56486e5ddfa0746eff9db7feef1d
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-07 18:16:45 +0100

    C-Sharp updated solution for #1: Two sum

--- c-sharp/Problems/arrays-and-hashing/TwoSum.cs
+++ c-sharp/Problems/arrays-and-hashing/TwoSum.cs
@@ -2,13 +2,51 @@
  * 1
  * Two Sum
  **
- * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
- * 
+ * Given an array of integers nums and an integer target,
+ * return indices of the two numbers such that they add up to target.
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
  * You can return the answer in any order.
+ *
+ * Example 1:
+ *   Input: nums = [2,7,11,15], target = 9
+ *   Output: [0,1]
+ *   Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ *
+ * Example 2:
+ *   Input: nums = [3,2,4], target = 6
+ *   Output: [1,2]
+ *
+ * Example 3:
+ *   Input: nums = [3,3], target = 6
+ *   Output: [0,1]
+ *
+ * Constraints:
+ *   • 2 <= nums.length <= 104
+ *   • -109 <= nums[i] <= 109
+ *   • -109 <= target <= 109
+ *   • Only one valid answer exists.
+ *
+ * Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
  * 
+ * Hint 1:
+ *   A really brute force way would be to search for all possible pairs of numbers
+ *   but that would be too slow. Again, it's best to try out brute force solutions for just for completeness.
+ *   It is from these brute force solutions that you can come up with optimizations.
+ * 
+ * Hint 2:
+ *   So, if we fix one of the numbers, say x,
+ *   we have to scan the entire array to find the next number y which is value - x
+ *   where value is the input parameter.
+ *   Can we change our array somehow so that this search becomes faster?
+ * 
+ * Hint 3:
+ *   The second train of thought is, without changing the array,
+ *   can we use additional space somehow?
+ *   Like maybe a hash map to speed up the search?
+ ** 
  * https://leetcode.com/problems/two-sum/
- */
+***/
 
 namespace Problems;
 
@@ -16,20 +54,18 @@ public class TwoSumSolution
 {
     public int[] TwoSum( int[] nums, int target )
     {
-        Dictionary<int, int> indices = new();
+        Dictionary<int, int> indices = new() { { nums[0], 0 } };
 
-        for ( int i = 0; i < nums.Length; i++ )
+        for ( int i = 1; i < nums.Length; i++ )
         {
-            int diff = target - nums[i];
-
-            if ( indices.ContainsKey( diff ) )
+            if ( indices.TryGetValue( target - nums[i], out int prevIndex ) )
             {
-                return new int[] { indices[diff], i };
+                return [prevIndex, i];
             }
 
             indices[nums[i]] = i;
         }
 
-        return Array.Empty<int>();
+        return [];
     }
 }
