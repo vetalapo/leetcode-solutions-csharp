9683e96ba4b30ecfb316326d6033639e64bbc81d
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-09 19:01:55 +0100

    C-Sharp #347, update description, make run faster

--- c-sharp/Problems/arrays-and-hashing/TopKFrequentElements.cs
+++ c-sharp/Problems/arrays-and-hashing/TopKFrequentElements.cs
@@ -2,10 +2,31 @@
  * 347
  * Top K Frequent Elements
  **
- * Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+ * Given an integer array nums and an integer k, return the k most frequent elements.
+ * You may return the answer in any order.
  * 
+ * Example 1:
+ *   Input: nums = [1,1,1,2,2,3], k = 2
+ *   Output: [1,2]
+ * 
+ * Example 2:
+ *   Input: nums = [1], k = 1
+ *   Output: [1]
+ *
+ * Constraints:
+ *   • 1 <= nums.length <= 105
+ *   • -104 <= nums[i] <= 104
+ *   • k is in the range [1, the number of unique elements in the array].
+ *   • It is guaranteed that the answer is unique.
+ *  
+ * 
+ * Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+ ** 
  * https://leetcode.com/problems/top-k-frequent-elements/
- */
+***/
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace Problems;
 
@@ -13,35 +34,34 @@ public class TopKFrequentElements
 {
     public int[] TopKFrequent( int[] nums, int k )
     {
-        Dictionary<int, int> frequencyMap = new();
+        Dictionary<int, int> frequencyMap = [];
 
-        foreach ( int n in nums )
+        foreach ( int num in nums )
         {
-            if ( !frequencyMap.ContainsKey( n ) )
-            {
-                frequencyMap[n] = 0;
-            }
+            ref int valOrNull = ref CollectionsMarshal.GetValueRefOrNullRef( frequencyMap, num );
 
-            frequencyMap[n]++;
+            if ( Unsafe.IsNullRef( ref valOrNull ) )
+            {
+                frequencyMap[num] = 1;
+            }
+            else
+            {
+                valOrNull++;
+            }
         }
 
-        PriorityQueue<int, int> priorityQueue = new();
+        PriorityQueue<int, int> priorityQueue = new( frequencyMap.Count );
 
-        foreach ( int mapKey in frequencyMap.Keys )
+        foreach ( (int key, int value) in frequencyMap )
         {
-            priorityQueue.Enqueue( mapKey, frequencyMap[mapKey] );
-
-            if ( priorityQueue.Count > k )
-            {
-                priorityQueue.Dequeue();
-            }
+            priorityQueue.Enqueue( key, value * -1 );
         }
 
         int[] result = new int[k];
 
-        while ( priorityQueue.Count > 0 )
+        for ( int i = 0; i < k; i++ )
         {
-            result[--k] = priorityQueue.Dequeue();
+            result[i] = priorityQueue.Dequeue();
         }
 
         return result;
