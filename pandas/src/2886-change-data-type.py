02b81a8d4844cf2226968970fe2e96c28f9ed80d
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-11 04:18:36 +0100

    pandas #2886: Change data type

---
+++ pandas/src/2886-change-data-type.py
@@ -0,0 +1,52 @@
+﻿##
+# 2886
+# Change Data Type
+##
+# DataFrame students
+# +-------------+--------+
+# | Column Name | Type   |
+# +-------------+--------+
+# | student_id  | int    |
+# | name        | object |
+# | age         | int    |
+# | grade       | float  |
+# +-------------+--------+
+# Write a solution to correct the errors:
+#   The grade column is stored as floats, convert it to integers.
+#   The result format is in the following example.
+#
+# Example 1:
+#   Input:
+#   DataFrame students:
+#   +------------+------+-----+-------+
+#   | student_id | name | age | grade |
+#   +------------+------+-----+-------+
+#   | 1          | Ava  | 6   | 73.0  |
+#   | 2          | Kate | 15  | 87.0  |
+#   +------------+------+-----+-------+
+#   Output:
+#   +------------+------+-----+-------+
+#   | student_id | name | age | grade |
+#   +------------+------+-----+-------+
+#   | 1          | Ava  | 6   | 73    |
+#   | 2          | Kate | 15  | 87    |
+#   +------------+------+-----+-------+
+#   
+#   Explanation: The data types of the column grade is converted to int.
+#
+# Hint 1
+# Consider using a build-in function in pandas library with a dictionary to convert the datatype of columns as specified.
+##
+# https://leetcode.com/problems/change-data-type/
+##
+
+import sys
+
+# Preventing creation of __pycache__
+sys.dont_write_bytecode = True
+
+import pandas as pd
+
+def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
+    students.grade = students.grade.astype(int)
+    return students
---
+++ pandas/src/tests/test_2886_change_data_type.py
@@ -0,0 +1,37 @@
+﻿import sys
+
+# Preventing creation of __pycache__
+sys.dont_write_bytecode = True
+
+import importlib
+import unittest
+import pandas as pd
+
+# Python really doesn't like paths with "-" and "/"
+# I don't like Python not liking it so
+# have to use importlib to import modules
+unit = importlib.import_module("src.2886-change-data-type")
+
+df = pd.DataFrame({
+    "student_id": [1, 2],
+    "name": ["Ava", "Kate"],
+    "age": [6, 15],
+    "grade": [73.0, 87.5]
+})
+
+expected_df = pd.DataFrame({
+    "student_id": [1, 2],
+    "name": ["Ava", "Kate"],
+    "age": [6, 15],
+    "grade": [73, 87]
+})
+
+class TestChangeDatatype(unittest.TestCase):
+    def test_changeDatatype(self):
+        expected_df.grade = expected_df.grade.astype(int)
+        actual_df = unit.changeDatatype(df)
+        
+        pd.testing.assert_frame_equal(actual_df, expected_df)
+
+if __name__ == "__main__":
+    unittest.main()
