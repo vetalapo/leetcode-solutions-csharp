9dfad4199ee81e14f1b73fe18d04f08fe870ee6b
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-12 18:09:52 +0100

    C-Sharp #258: Add Digits

---
+++ c-sharp/Problems/math/AddDigits.cs
@@ -0,0 +1,49 @@
+﻿/**
+ * 258
+ * Add Digits
+ **
+ * Given an integer num,
+ * repeatedly add all its digits until the result has only one digit,
+ * and return it.
+ *
+ * Example 1:
+ *   Input: num = 38
+ *   Output: 2
+ *   Explanation: The process is
+ *     38 --> 3 + 8 --> 11 --> 1 + 1 --> 2
+ *     Since 2 has only one digit, return it.
+ *
+ * Example 2:
+ *   Input: num = 0
+ *   Output: 0
+ *
+ * Constraints: 0 <= num <= 231 - 1
+ *
+ * Follow up: Could you do it without any loop/recursion in O(1) runtime?
+ *
+ * Hint 1:
+ *   A naive implementation of the above process is trivial.
+ *   Could you come up with other methods?
+ *
+ * Hint 2:
+ *   What are all the possible results?
+ *
+ * Hint 3:
+ *   How do they occur, periodically or randomly?
+ *
+ * Hint 4:
+ *   You may find this Wikipedia article useful.
+ *   https://en.wikipedia.org/wiki/Digital_root
+ **
+ * https://leetcode.com/problems/add-digits/
+***/
+
+namespace Problems;
+
+public class AddDigitsSolution
+{
+    public int AddDigits( int num )
+    {
+        return ( num - 1 ) % 9 + 1;
+    }
+}
---
+++ c-sharp/Tests/math/AddDigitsSolutionTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class AddDigitsSolutionTests
+{
+    [TestCase( 0, ExpectedResult = 0 )]
+    [TestCase( 16, ExpectedResult = 7 )]
+    [TestCase( 38, ExpectedResult = 2 )]
+    [TestCase( 942, ExpectedResult = 6 )]
+    [TestCase( 132189, ExpectedResult = 6 )]
+    [TestCase( 493193, ExpectedResult = 2 )]
+    public int AddDigitsTest( int num ) => new AddDigitsSolution().AddDigits( num );
+}
