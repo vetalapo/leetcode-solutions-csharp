cca0d7030bcfd6ad7e728df2f7e479af35d5a0f2
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-14 16:05:13 +0100

    C-Sharp #128 solution and description update

--- c-sharp/Problems/arrays-and-hashing/LongestConsecutiveSequence.cs
+++ c-sharp/Problems/arrays-and-hashing/LongestConsecutiveSequence.cs
@@ -5,8 +5,21 @@
  * Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
  * You must write an algorithm that runs in O(n) time.
  * 
+ * Example 1:
+ *   Input: nums = [100,4,200,1,3,2]
+ *   Output: 4
+ *   Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+ * 
+ * Example 2:
+ *   Input: nums = [0,3,7,2,5,8,4,6,0,1]
+ *   Output: 9
+ *
+ * Constraints: 
+ *   • 0 <= nums.length <= 105
+ *   • -109 <= nums[i] <= 109
+ **
  * https://leetcode.com/problems/longest-consecutive-sequence/
- */
+***/
 
 namespace Problems;
 
@@ -14,27 +27,24 @@ public class LongestConsecutiveSequence
 {
     public int LongestConsecutive( int[] nums )
     {
-        HashSet<int> numsSet = new( nums );
+        HashSet<int> numSet = new( nums );
+        int longestConsecutive = 0;
 
-        int maxConsecutiveCount = 0;
-
-        foreach ( int n in numsSet )
+        foreach ( int num in numSet )
         {
-            if ( numsSet.Contains( n - 1 ) )
+            if ( !numSet.Contains( num - 1 ) )
             {
-                continue;
+                int currentLength = 1;
+
+                while ( numSet.Contains( num + currentLength ) )
+                {
+                    currentLength++;
+                }
+
+                longestConsecutive = Math.Max( longestConsecutive, currentLength );
             }
-
-            int currentConsecutiveCount = 0;
-
-            while ( numsSet.Contains( n + currentConsecutiveCount ) )
-            {
-                currentConsecutiveCount++;
-            }
-
-            maxConsecutiveCount = Math.Max( maxConsecutiveCount, currentConsecutiveCount );
         }
 
-        return maxConsecutiveCount;
+        return longestConsecutive;
     }
 }
