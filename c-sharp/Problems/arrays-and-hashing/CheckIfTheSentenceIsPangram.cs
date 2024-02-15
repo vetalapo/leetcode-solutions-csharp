22254a51081054c0084d3a0a4f731f0d9ff038b1
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-15 19:38:06 +0100

    C-Sharp #1832: Check if the sentence is pangram

---
+++ c-sharp/Problems/arrays-and-hashing/CheckIfTheSentenceIsPangram.cs
@@ -0,0 +1,56 @@
+﻿/**
+ * 1832
+ * Check if the Sentence Is Pangram
+ **
+ * A pangram is a sentence where every letter of the English alphabet appears at least once.
+ * Given a string sentence containing only lowercase English letters,
+ * return true if sentence is a pangram, or false otherwise.
+ *
+ * Example 1:
+ *   Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+ *   Output: true
+ *   Explanation: sentence contains at least one of every letter of the English alphabet.
+ *
+ * Example 2:
+ *   Input: sentence = "leetcode"
+ *   Output: false
+ *
+ * Constraints:
+ *   • 1 <= sentence.length <= 1000
+ *   • sentence consists of lowercase English letters.
+ *
+ * Hint 1:
+ *   Iterate over the string and mark each character as found
+ *   (using a boolean array, bitmask, or any other similar way).
+ *
+ * Hint 2: Check if the number of found characters equals the alphabet length.
+ **
+ * https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+***/
+
+namespace Problems;
+
+public class CheckIfTheSentenceIsPangram
+{
+    public bool CheckIfPangram( string sentence )
+    {
+        HashSet<char> sentenceSet = new( sentence );
+
+        for ( char c = 'a'; c <= 'z'; c++ )
+        {
+            if ( !sentenceSet.Contains( c ) )
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public bool CheckIfPangramCountCheck( string sentence )
+    {
+        HashSet<char> sentenceSet = new( sentence );
+
+        return sentenceSet.Count == 26;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/CheckIfTheSentenceIsPangramTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+namespace Problems;
+
+public class CheckIfTheSentenceIsPangramTests
+{
+    [TestCaseSource( nameof( TestValues ) )]
+    public bool CheckIfPangramTest( string sentence ) =>
+        new CheckIfTheSentenceIsPangram().CheckIfPangram( sentence );
+
+    [TestCaseSource( nameof( TestValues ) )]
+    public bool CheckIfPangramCountCheckTest( string sentence ) =>
+        new CheckIfTheSentenceIsPangram().CheckIfPangramCountCheck( sentence );
+
+    private static IEnumerable TestValues
+    {
+        get
+        {
+            yield return new TestCaseData( "thequickbrownfoxjumpsoverthelazydog" ).Returns( true );
+            yield return new TestCaseData( "leetcode" ).Returns( false );
+        }
+    }
+}
