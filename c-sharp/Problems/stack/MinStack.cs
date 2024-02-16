19a136e1cbc33e6f19a4b91648c253bea3fa15df
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-16 14:44:49 +0100

    C-Sharp #155 updated solution

--- c-sharp/Problems/stack/MinStack.cs
+++ c-sharp/Problems/stack/MinStack.cs
@@ -2,39 +2,69 @@
  * 155
  * Min Stack
  **
- * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
- * 
+ * Design a stack that supports push, pop, top,
+ * and retrieving the minimum element in constant time.
+ *
  * Implement the MinStack class:
- *   MinStack() initializes the stack object.
- *   void push(int val) pushes the element val onto the stack.
- *   void pop() removes the element on the top of the stack.
- *   int top() gets the top element of the stack.
- *   int getMin() retrieves the minimum element in the stack.
- *   
+ *   • MinStack() initializes the stack object.
+ *   • void push(int val) pushes the element val onto the stack.
+ *   • void pop() removes the element on the top of the stack.
+ *   • int top() gets the top element of the stack.
+ *   • int getMin() retrieves the minimum element in the stack.
+ *
  * You must implement a solution with O(1) time complexity for each function.
- * 
+ *
+ * Example 1:
+ *   Input
+ *     ["MinStack","push","push","push","getMin","pop","top","getMin"]
+ *     [[],[-2],[0],[-3],[],[],[],[]]
+ *
+ *   Output
+ *     [null,null,null,null,-3,null,0,-2]
+ *
+ *   Explanation
+ *       MinStack minStack = new MinStack();
+ *       minStack.push(-2);
+ *       minStack.push(0);
+ *       minStack.push(-3);
+ *       minStack.getMin(); // return -3
+ *       minStack.pop();
+ *       minStack.top();    // return 0
+ *       minStack.getMin(); // return -2
+ *
+ * Constraints:
+ *   • -231 <= val <= 231 - 1
+ *   • Methods pop, top and getMin operations will always be called on non-empty stacks.
+ *   • At most 3 * 104 calls will be made to push, pop, top, and getMin.
+ *
+ * Hint 1: Consider each node in the stack having a minimum value.
+ **
  * https://leetcode.com/problems/min-stack/
- */
+***/
 
 namespace Problems;
 
 public class MinStack
 {
-    private readonly Stack<int> _stack;
-    private readonly Stack<int> _minStack;
+    private readonly Stack<int> _stack = [];
+    private readonly Stack<int> _minStack = [];
 
     public MinStack()
     {
-        _stack = new Stack<int>();
-        _minStack = new Stack<int>();
     }
 
     public void Push( int val )
     {
         _stack.Push( val );
 
-        int min = Math.Min( val, _minStack.Count == 0 ? val : _minStack.Peek() );
-        _minStack.Push( min );
+        if ( _minStack.TryPeek( out int minStackValue ) )
+        {
+            _minStack.Push( Math.Min( minStackValue, val ) );
+        }
+        else
+        {
+            _minStack.Push( val );
+        }
     }
 
     public void Pop()
