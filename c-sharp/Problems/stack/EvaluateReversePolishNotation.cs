6da2bfbf10d5955fafd2c92d2b7d33d068438387
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-17 15:58:02 +0100

    C-Sharp #150 solution update

--- c-sharp/Problems/stack/EvaluateReversePolishNotation.cs
+++ c-sharp/Problems/stack/EvaluateReversePolishNotation.cs
@@ -3,18 +3,47 @@
  * Evaluate Reverse Polish Notation
  **
  * You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
- * Evaluate the expression. Return an integer that represents the value of the expression.
- * 
+ * https://en.wikipedia.org/wiki/Reverse_Polish_notation
+ *
+ * Evaluate the expression.
+ * Return an integer that represents the value of the expression.
+ *
  * Note that:
- *   The valid operators are '+', '-', '*', and '/'.
- *   Each operand may be an integer or another expression.
- *   The division between two integers always truncates toward zero.
- *   There will not be any division by zero.
- *   The input represents a valid arithmetic expression in a reverse polish notation.
- *   The answer and all the intermediate calculations can be represented in a 32-bit integer.
- * 
+ *   • The valid operators are '+', '-', '*', and '/'.
+ *   • Each operand may be an integer or another expression.
+ *   • The division between two integers always truncates toward zero.
+ *   • There will not be any division by zero.
+ *   • The input represents a valid arithmetic expression in a reverse polish notation.
+ *   • The answer and all the intermediate calculations can be represented in a 32-bit integer.
+ *
+ * Example 1: 
+ *   Input: tokens = ["2","1","+","3","*"]
+ *   Output: 9
+ *   Explanation: ((2 + 1) * 3) = 9
+ *
+ * Example 2:
+ *   Input: tokens = ["4","13","5","/","+"]
+ *   Output: 6
+ *   Explanation: (4 + (13 / 5)) = 6
+ *
+ * Example 3:
+ *   Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+ *   Output: 22
+ *   Explanation:
+ *     ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+ *     = ((10 * (6 / (12 * -11))) + 17) + 5
+ *     = ((10 * (6 / -132)) + 17) + 5
+ *     = ((10 * 0) + 17) + 5
+ *     = (0 + 17) + 5
+ *     = 17 + 5
+ *     = 22
+ *
+ * Constraints:
+ *   • 1 <= tokens.length <= 104
+ *   • tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
+ **
  * https://leetcode.com/problems/evaluate-reverse-polish-notation/
- */
+***/
 
 namespace Problems;
 
@@ -22,13 +51,11 @@ public class EvaluateReversePolishNotation
 {
     public int EvalRPN( string[] tokens )
     {
-        Stack<int> stack = new();
+        Stack<int> stack = [];
 
         foreach ( string token in tokens )
         {
-            bool isNumber = int.TryParse( token, out int number );
-
-            if ( isNumber )
+            if ( int.TryParse( token, out int number ) )
             {
                 stack.Push( number );
             }
