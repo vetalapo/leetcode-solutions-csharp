2181b6ec0549c45550df743aba658a8405763a2c
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-19 00:10:10 +0100

    C-Sharp #22 solution update

--- c-sharp/Problems/stack/GenerateParenthesesSolution.cs
+++ c-sharp/Problems/stack/GenerateParenthesesSolution.cs
@@ -2,10 +2,21 @@
  * 22
  * Generate Parentheses
  **
- * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ * Given n pairs of parentheses,
+ * write a function to generate all combinations of well-formed parentheses.
+ *
+ * Example 1:
+ *   Input: n = 3
+ *   Output: ["((()))","(()())","(())()","()(())","()()()"]
  * 
+ * Example 2:
+ *   Input: n = 1
+ *   Output: ["()"]
+ * 
+ * Constraints: 1 <= n <= 8
+ **
  * https://leetcode.com/problems/generate-parentheses/
- */
+***/
 
 namespace Problems;
 
@@ -13,8 +24,8 @@ public class GenerateParenthesesSolution
 {
     public IList<string> GenerateParenthesis( int n )
     {
-        List<string> result = new();
-        Stack<string> stack = new();
+        List<string> result = [];
+        Stack<string> stack = [];
 
         void backtrack( int openP, int closedP )
         {
