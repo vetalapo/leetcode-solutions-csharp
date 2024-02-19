aa58dfc34fe3aed9c6d221c93696e69ca5398396
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-19 18:23:38 +0100

    C-Sharp solution #231: Power of Two

---
+++ c-sharp/Tests/math/PowerOfTwoTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class PowerOfTwoTests
+{
+    [TestCase( 1, ExpectedResult = true )]
+    [TestCase( 3, ExpectedResult = false )]
+    [TestCase( 8, ExpectedResult = true )]
+    [TestCase( 16, ExpectedResult = true )]
+    public bool IsPowerOfTwoTest( int n ) => new PowerOfTwo().IsPowerOfTwo( n );
+}
