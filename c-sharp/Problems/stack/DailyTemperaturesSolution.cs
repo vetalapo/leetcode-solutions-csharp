b25cea1ffef91800ffdf1130dd8e646df3703305
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-20 17:52:44 +0100

    C-Sharp #739 solution update

--- c-sharp/Problems/stack/DailyTemperaturesSolution.cs
+++ c-sharp/Problems/stack/DailyTemperaturesSolution.cs
@@ -3,13 +3,28 @@
  * Daily Temperatures
  **
  * Given an array of integers temperatures represents the daily temperatures,
- * return an array answer such that
- * answer[i] is the number of days you have to wait after the ith day to get a warmer temperature.
- * 
+ * return an array answer such that answer[i]
+ * is the number of days you have to wait after the ith day to get a warmer temperature.
  * If there is no future day for which this is possible, keep answer[i] == 0 instead.
  * 
+ * Example 1:
+ *   Input: temperatures = [73,74,75,71,69,72,76,73]
+ *   Output: [1,1,4,2,1,1,0,0]
+ * 
+ * Example 2:
+ *   Input: temperatures = [30,40,50,60]
+ *   Output: [1,1,1,0]
+ * 
+ * Example 3:
+ *   Input: temperatures = [30,60,90]
+ *   Output: [1,1,0]
+ *
+ * Constraints: 
+ *   • 1 <= temperatures.length <= 105
+ *   • 30 <= temperatures[i] <= 100
+ **
  * https://leetcode.com/problems/daily-temperatures/
- */
+***/
 
 namespace Problems;
 
@@ -18,18 +33,18 @@ public class DailyTemperaturesSolution
     public int[] DailyTemperatures( int[] temperatures )
     {
         int[] result = new int[temperatures.Length];
-        Stack<int> indxStack = new();
+        Stack<(int Index, int Value)> stack = [];
 
         for ( int i = 0; i < temperatures.Length; i++ )
         {
-            while ( indxStack.Count > 0 && temperatures[i] > temperatures[indxStack.Peek()] )
-            {
-                int prevTempIndex = indxStack.Pop();
+            int currTemp = temperatures[i];
 
-                result[prevTempIndex] = i - prevTempIndex;
+            while ( stack.TryPeek( out (int Index, int Value) topStack ) && currTemp > topStack.Value )
+            {
+                result[stack.Pop().Index] = i - topStack.Index;
             }
 
-            indxStack.Push( i );
+            stack.Push( (i, currTemp) );
         }
 
         return result;
