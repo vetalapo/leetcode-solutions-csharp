199ddf915c5e34cada4ddf914250f9afc9e3bbd6
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-21 17:13:12 +0100

    C-Sharp #207: Bitwise AND of Numbers Range

---
+++ c-sharp/Tests/bit-manipulation/BitwiseANDofNumbersRangeTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class BitwiseANDofNumbersRangeTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int RangeBitwiseAndTest( int left, int right ) => new BitwiseANDofNumbersRange().RangeBitwiseAnd( left, right );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( 0, 0 ).Returns( 0 );
+            yield return new TestCaseData( 0, 1 ).Returns( 0 );
+            yield return new TestCaseData( 1, 1 ).Returns( 1 );
+            yield return new TestCaseData( 2, 5 ).Returns( 0 );
+            yield return new TestCaseData( 5, 7 ).Returns( 4 );
+            yield return new TestCaseData( 1, 2147483647 ).Returns( 0 );
+        }
+    }
+}
