5fa6fc97620e20bb50eacf629f51b46d598a71c1
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-22 19:29:01 +0100

    C-Sharp #997: Find the Town Judge

---
+++ c-sharp/Tests/arrays-and-hashing/FindTheTownJudgeTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class FindTheTownJudgeTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int FindJudgeTest( int n, int[][] trust ) => new FindTheTownJudge().FindJudge( n, trust );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( 2, new int[][] { [1, 2] } ).Returns( 2 );
+            yield return new TestCaseData( 3, new int[][] { [1, 3], [2, 3] } ).Returns( 3 );
+            yield return new TestCaseData( 3, new int[][] { [1, 3], [2, 3], [3, 1] } ).Returns( -1 );
+        }
+    }
+}
