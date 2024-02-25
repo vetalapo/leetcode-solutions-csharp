a61731cc4650acf8cac21327b48ad4e9a6dd8408
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-25 23:18:14 +0100

    C-Sharp #15 solution, description, and tests update

--- c-sharp/Problems/two-pointers/ThreeSumSolution.cs
+++ c-sharp/Problems/two-pointers/ThreeSumSolution.cs
@@ -2,14 +2,39 @@
  * 15
  * 3Sum
  **
- * Given an integer array nums, return all the triplets 
- * [nums[i], nums[j], nums[k]] such that i != j,
- * i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
- * 
+ * Given an integer array nums,
+ * return all the triplets [nums[i], nums[j], nums[k]] such that 
+ * i != j, i != k, and j != k,
+ * and nums[i] + nums[j] + nums[k] == 0.
+ *
  * Notice that the solution set must not contain duplicate triplets.
+ *
+ * Example 1:
+ *   Input: nums = [-1,0,1,2,-1,-4]
+ *   Output: [[-1,-1,2],[-1,0,1]]
+ *   Explanation: 
+ *     nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+ *     nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+ *     nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+ *     The distinct triplets are [-1,0,1] and [-1,-1,2].
+ *     Notice that the order of the output and the order of the triplets does not matter.
+ *
+ * Example 2:
+ *   Input: nums = [0,1,1]
+ *   Output: []
+ *   Explanation: The only possible triplet does not sum up to 0.
  * 
+ * Example 3:
+ *   Input: nums = [0,0,0]
+ *   Output: [[0,0,0]]
+ *   Explanation: The only possible triplet sums up to 0.
+ *
+ * Constraints:
+ *   • 3 <= nums.length <= 3000
+ *   • -105 <= nums[i] <= 105
+ **
  * https://leetcode.com/problems/3sum/
- */
+***/
 
 namespace Problems;
 
@@ -17,42 +42,42 @@ public class ThreeSumSolution
 {
     public IList<IList<int>> ThreeSum( int[] nums )
     {
-        List<IList<int>> result = new();
+        List<IList<int>> result = [];
 
         Array.Sort( nums );
 
-        for ( int i = 0; i < nums.Length; i++ )
+        for ( int i = 0; i < nums.Length - 2; i++ )
         {
             if ( i > 0 && nums[i] == nums[i - 1] )
             {
                 continue;
             }
 
-            int start = i + 1;
-            int end = nums.Length - 1;
+            int left = i + 1;
+            int right = nums.Length - 1;
 
-            while ( start < end )
+            while ( left < right )
             {
-                int threeSum = nums[i] + nums[start] + nums[end];
+                int threeSum = nums[i] + nums[left] + nums[right];
 
                 if ( threeSum == 0 )
                 {
-                    result.Add( new List<int> { nums[i], nums[start], nums[end] } );
+                    result.Add( new List<int> { nums[i], nums[left], nums[right] } );
 
-                    start++;
+                    left++;
 
-                    while ( nums[start] == nums[start - 1] && start < end )
+                    while ( nums[left] == nums[left - 1] && left < right )
                     {
-                        start++;
+                        left++;
                     }
                 }
                 else if ( threeSum > 0 )
                 {
-                    end--;
+                    right--;
                 }
                 else
                 {
-                    start++;
+                    left++;
                 }
             }
         }
--- c-sharp/Tests/two-pointers/ThreeSumSolutionTests.cs
+++ c-sharp/Tests/two-pointers/ThreeSumSolutionTests.cs
@@ -14,11 +14,11 @@ public class ThreeSumSolutionTests
         get
         {
             yield return new TestCaseData( new int[] { -1, 0, 1, 2, -1, -4 } )
-                .Returns( new List<List<int>>() { new List<int>() { -1, -1, 2 }, new List<int>() { -1, 0, 1 } } );
+                .Returns( new List<List<int>>() { new() { -1, -1, 2 }, new() { -1, 0, 1 } } );
 
             yield return new TestCaseData( new int[] { 0, 1, 1 } ).Returns( new List<List<int>>() );
-            yield return new TestCaseData( new int[] { 0, 0, 0 } ).Returns( new List<List<int>>() { new List<int>() { 0, 0, 0 } } );
-            yield return new TestCaseData( new int[] { 0, 0, 0, 0 } ).Returns( new List<List<int>>() { new List<int>() { 0, 0, 0 } } );
+            yield return new TestCaseData( new int[] { 0, 0, 0 } ).Returns( new List<List<int>>() { new() { 0, 0, 0 } } );
+            yield return new TestCaseData( new int[] { 0, 0, 0, 0 } ).Returns( new List<List<int>>() { new() { 0, 0, 0 } } );
         }
     }
 }
