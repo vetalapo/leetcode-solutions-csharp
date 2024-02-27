5298ea07b1e7bc57e2a052d2e0b533795da24649
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-27 21:20:29 +0100

    C-Sharp #11 description update, add hints

--- c-sharp/Problems/two-pointers/ContainerWithMostWater.cs
+++ c-sharp/Problems/two-pointers/ContainerWithMostWater.cs
@@ -24,6 +24,17 @@
  *   • n == height.length
  *   • 2 <= n <= 105
  *   • 0 <= height[i] <= 104
+ *   
+ * Hint 1:
+ *   If you simulate the problem, it will be O(n^2) which is not efficient.
+ *   
+ * Hint 2:
+ *   Try to use two-pointers.
+ *   Set one pointer to the left and one to the right of the array.
+ *   Always move the pointer that points to the lower line.
+ *   
+ * Hint 3:
+ *   How can you calculate the amount of water at each step?
  **
  * https://leetcode.com/problems/container-with-most-water/
 ***/
