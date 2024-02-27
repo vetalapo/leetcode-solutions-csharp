49202f2c6761acd4962116727b4e5f711cccbdc1
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-27 21:04:14 +0100

    C-Sharp #11 solution and description update

--- c-sharp/Problems/two-pointers/ContainerWithMostWater.cs
+++ c-sharp/Problems/two-pointers/ContainerWithMostWater.cs
@@ -5,12 +5,28 @@
  * You are given an integer array height of length n.
  * There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
  * Find two lines that together with the x-axis form a container, such that the container contains the most water.
- * 
+ *
  * Return the maximum amount of water a container can store.
  * Notice that you may not slant the container.
  * 
+ * Example 1:
+ *   Input: height = [1,8,6,2,5,4,8,3,7]
+ *   Output: 49
+ *   Explanation:
+ *     The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7].
+ *     In this case, the max area of water (blue section) the container can contain is 49.
+ * 
+ * Example 2:
+ *   Input: height = [1,1]
+ *   Output: 1
+ *
+ * Constraints:
+ *   • n == height.length
+ *   • 2 <= n <= 105
+ *   • 0 <= height[i] <= 104
+ **
  * https://leetcode.com/problems/container-with-most-water/
- */
+***/
 
 namespace Problems;
 
@@ -18,28 +34,28 @@ public class ContainerWithMostWater
 {
     public int MaxArea( int[] height )
     {
-        int start = 0;
-        int end = height.Length - 1;
-
         int maxArea = 0;
 
-        while ( start < end )
-        {
-            int diff = end - start;
-            int currentMinheight;
+        int left = 0;
+        int right = height.Length - 1;
 
-            if ( height[start] > height[end] )
+        while ( left < right )
+        {
+            int diff = right - left;
+            int currentMinHeight;
+
+            if ( height[left] > height[right] )
             {
-                currentMinheight = height[end];
-                end--;
+                currentMinHeight = height[right];
+                right--;
             }
             else
             {
-                currentMinheight = height[start];
-                start++;
+                currentMinHeight = height[left];
+                left++;
             }
 
-            maxArea = Math.Max( maxArea, diff * currentMinheight );
+            maxArea = Math.Max( maxArea, currentMinHeight * diff );
         }
 
         return maxArea;
