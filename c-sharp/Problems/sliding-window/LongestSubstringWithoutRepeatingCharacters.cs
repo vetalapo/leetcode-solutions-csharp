4de95700a3d92ab5f7950c263a634b6fca1ddae3
Vitaly Dushkin <vetalapo@gmail.com>
2024-03-01 15:53:07 +0100

    C-Sharp #3 description update

--- c-sharp/Problems/sliding-window/LongestSubstringWithoutRepeatingCharacters.cs
+++ c-sharp/Problems/sliding-window/LongestSubstringWithoutRepeatingCharacters.cs
@@ -1,13 +1,34 @@
 ﻿/**
-* 3
-* Longest Substring Without Repeating Characters
-**
-* Given a string s, find the length of the longest substring without repeating characters.
-* 
-* s consists of English letters, digits, symbols and spaces.
-* 
-* https://leetcode.com/problems/longest-substring-without-repeating-characters/
-*/
+ * 3
+ * Longest Substring Without Repeating Characters
+ **
+ * Given a string s, find the length of the longest substring
+ * ( A substring is a contiguous non-empty sequence of characters within a string. )
+ * without repeating characters.
+ *
+ * Example 1:
+ *   Input: s = "abcabcbb"
+ *   Output: 3
+ *   Explanation: The answer is "abc", with the length of 3.
+ *
+ * Example 2:
+ *   Input: s = "bbbbb"
+ *   Output: 1
+ *   Explanation: The answer is "b", with the length of 1.
+ *
+ * Example 3:
+ *   Input: s = "pwwkew"
+ *   Output: 3
+ *   Explanation: 
+ *     The answer is "wke", with the length of 3.
+ *     Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ *
+ * Constraints:
+ *   • 0 <= s.length <= 5 * 104
+ *   • s consists of English letters, digits, symbols and spaces.
+ **
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+***/
 
 namespace Problems;
 
@@ -17,7 +38,7 @@ public class LongestSubstringWithoutRepeatingCharacters
     {
         int maxCount = 0;
 
-        Queue<char> chars = new();
+        Queue<char> chars = [];
 
         foreach ( char c in s )
         {
@@ -39,7 +60,7 @@ public class LongestSubstringWithoutRepeatingCharacters
         int maxLength = 0;
 
         int left = 0;
-        HashSet<char> charSet = new();
+        HashSet<char> charSet = [];
 
         for ( int right = 0; right < s.Length; right++ )
         {
