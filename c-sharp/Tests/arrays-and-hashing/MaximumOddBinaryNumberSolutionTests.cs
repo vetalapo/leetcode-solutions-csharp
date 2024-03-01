bb8739e11bf6fecf60874ddbcc62e5f6b18696a6
Vitaly Dushkin <vetalapo@gmail.com>
2024-03-01 21:49:40 +0100

    C-Sharp #2864: Maximum Odd Binary Number

---
+++ c-sharp/Tests/arrays-and-hashing/MaximumOddBinaryNumberSolutionTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+using Problems;
+public class MaximumOddBinaryNumberSolutionTests
+{
+    [TestCase( "010", ExpectedResult = "001" )]
+    [TestCase( "0101", ExpectedResult = "1001" )]
+    public string MaximumOddBinaryNumberTest( string s )
+        => new MaximumOddBinaryNumberSolution().MaximumOddBinaryNumber( s );
+}
