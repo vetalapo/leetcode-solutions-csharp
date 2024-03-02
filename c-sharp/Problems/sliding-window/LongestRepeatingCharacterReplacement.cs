1da9a4a3e9049f1186f3b05e2413897aace09de6
Vitaly Dushkin <vetalapo@gmail.com>
2024-03-02 22:21:26 +0100

    C-Sharp #424 description update

--- c-sharp/Problems/sliding-window/LongestRepeatingCharacterReplacement.cs
+++ c-sharp/Problems/sliding-window/LongestRepeatingCharacterReplacement.cs
@@ -7,11 +7,27 @@
  * You can perform this operation at most k times.
  * 
  * Return the length of the longest substring containing the same letter you can get after performing the above operations.
+ *
+ * Example 1: 
+ *   Input: s = "ABAB", k = 2
+ *   Output: 4
+ *   Explanation: Replace the two 'A's with two 'B's or vice versa.
  * 
- * s consists of only uppercase English letters.
- * 
- * https://leetcode.com/problems/longest-repeating-character-replacement/description/
- */
+ * Example 2:
+ *   Input: s = "AABABBA", k = 1
+ *   Output: 4
+ *   Explanation:
+ *     Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+ *     The substring "BBBB" has the longest repeating letters, which is 4.
+ *     There may exists other ways to achieve this answer too.
+ *
+ * Constraints: 
+ *   • 1 <= s.length <= 105
+ *   • s consists of only uppercase English letters.
+ *   • 0 <= k <= s.length
+ **
+ * https://leetcode.com/problems/longest-repeating-character-replacement/
+***/
 
 namespace Problems;
 
