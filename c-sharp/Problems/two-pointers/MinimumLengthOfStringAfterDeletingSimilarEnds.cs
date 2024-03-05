fb35216236a52ed70c8dbfa845ebcf9e119a2bcf
Vitaly Dushkin <vetalapo@gmail.com>
2024-03-05 15:12:46 +0100

    C-Sharp #1750 Minimum Length of String After Deleting Similar Ends

---
+++ c-sharp/Problems/two-pointers/MinimumLengthOfStringAfterDeletingSimilarEnds.cs
@@ -0,0 +1,78 @@
+﻿/**
+ * 1750
+ * Minimum Length of String After Deleting Similar Ends
+ **
+ * Given a string s consisting only of characters 'a', 'b', and 'c'.
+ *
+ * You are asked to apply the following algorithm on the string any number of times:
+ *   1. Pick a non-empty prefix from the string s where all the characters in the prefix are equal.
+ *   2. Pick a non-empty suffix from the string s where all the characters in this suffix are equal.
+ *   3. The prefix and the suffix should not intersect at any index.
+ *   4. The characters from the prefix and suffix must be the same.
+ *   5. Delete both the prefix and the suffix.
+ * 
+ * Return the minimum length of s after performing the above operation any number of times (possibly zero times).
+ *
+ * Example 1: 
+ *   Input: s = "ca"
+ *   Output: 2
+ *   Explanation: You can't remove any characters, so the string stays as is.
+ *
+ * Example 2:
+ *   Input: s = "cabaabac"
+ *   Output: 0
+ *   Explanation: An optimal sequence of operations is:
+ *     - Take prefix = "c" and suffix = "c" and remove them, s = "abaaba".
+ *     - Take prefix = "a" and suffix = "a" and remove them, s = "baab".
+ *     - Take prefix = "b" and suffix = "b" and remove them, s = "aa".
+ *     - Take prefix = "a" and suffix = "a" and remove them, s = "".
+ *
+ * Example 3:
+ *   Input: s = "aabccabba"
+ *   Output: 3
+ *   Explanation: An optimal sequence of operations is:
+ *     - Take prefix = "aa" and suffix = "a" and remove them, s = "bccabb".
+ *     - Take prefix = "b" and suffix = "bb" and remove them, s = "cca".
+ *
+ * Constraints:
+ *   • 1 <= s.length <= 105
+ *   • s only consists of characters 'a', 'b', and 'c'.
+ *   
+ * Hint 1:
+ *   If both ends have distinct characters, no more operations can be made.
+ *   Otherwise, the only operation is to remove all of the same characters from both ends.
+ *   We will do this as many times as we can.
+ * 
+ * Hint 2:
+ *   Note that if the length is equal 1 the answer is 1
+ **
+ * https://leetcode.com/problems/minimum-length-of-string-after-deleting-similar-ends/
+***/
+
+namespace Problems;
+
+public class MinimumLengthOfStringAfterDeletingSimilarEnds
+{
+    public int MinimumLength( string s )
+    {
+        int left = 0;
+        int right = s.Length - 1;
+
+        while ( left < right && s[left] == s[right] )
+        {
+            char c = s[left];
+
+            while ( left <= right && s[left] == c )
+            {
+                left++;
+            }
+
+            while ( left <= right && s[right] == c )
+            {
+                right--;
+            }
+        }
+
+        return right - left + 1;
+    }
+}
---
+++ c-sharp/Tests/two-pointers/MinimumLengthOfStringAfterDeletingSimilarEndsTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MinimumLengthOfStringAfterDeletingSimilarEndsTests
+{
+    [TestCase( "c", ExpectedResult = 1 )]
+    [TestCase( "ca", ExpectedResult = 2 )]
+    [TestCase( "aba", ExpectedResult = 1 )]
+    [TestCase( "cabaabac", ExpectedResult = 0 )]
+    [TestCase( "aabccabba", ExpectedResult = 3 )]
+    [TestCase( "bbbbbbbbbbbbbbbbbbbbbbbbbbbabbbbbbbbbbbbbbbccbcbcbccbbabbb", ExpectedResult = 1 )]
+    [TestCase( "abbbbbbbbbbbbbbbbbbba", ExpectedResult = 0 )]
+    public int MinimumLengthTest( string s ) =>
+        new MinimumLengthOfStringAfterDeletingSimilarEnds().MinimumLength( s );
+}
