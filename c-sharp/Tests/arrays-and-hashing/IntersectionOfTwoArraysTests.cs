a301f8ca7bcb9e4dd1fca326075dc9074861b750
Vitaly Dushkin <vetalapo@gmail.com>
2024-03-11 00:32:30 +0100

    C-Sharp #349 add tests

---
+++ c-sharp/Tests/arrays-and-hashing/IntersectionOfTwoArraysTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class IntersectionOfTwoArraysTests
+{
+    [TestCase( new int[] { 1, 2, 2, 1 }, new int[] { 2, 2 }, ExpectedResult = new int[] { 2 } )]
+    [TestCase( new int[] { 4, 9, 5 }, new int[] { 9, 4, 9, 8, 4 }, ExpectedResult = new int[] { 9, 4 } )]
+    public int[] IntersectionTest( int[] nums1, int[] nums2 )
+        => new IntersectionOfTwoArrays().Intersection( nums1, nums2 );
+}
