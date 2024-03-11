94da0697527c4806f486547393b2786cfafa294a
Vitaly Dushkin <vetalapo@gmail.com>
2024-03-12 00:48:11 +0100

    C-Sharp #791: Custom Sort String

---
+++ c-sharp/Tests/arrays-and-hashing/CustomSortStringSolutionTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CustomSortStringSolutionTests
+{
+    [TestCase( "cba", "abcd", ExpectedResult = "cbad" )]
+    public string CustomSortStringTest( string order, string s )
+        => new CustomSortStringSolution().CustomSortString( order, s );
+}
