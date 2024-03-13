e64d07bcc74eb47718499ce8d650bd2f1b3f64d8
Vitaly Dushkin <vetalapo@gmail.com>
2024-03-14 00:26:23 +0100

    C-Sharp #2485: Find the Pivot Integer

---
+++ c-sharp/Tests/math/FindThePivotIntegerTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class FindThePivotIntegerTests
+{
+    [TestCase( 8, ExpectedResult = 6 )]
+    [TestCase( 1, ExpectedResult = 1 )]
+    [TestCase( 4, ExpectedResult = -1 )]
+    public int PivotIntegerTest( int n ) => new FindThePivotInteger().PivotInteger( n );
+}
