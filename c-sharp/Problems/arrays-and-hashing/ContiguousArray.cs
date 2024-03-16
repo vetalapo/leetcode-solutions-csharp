a90d302676bded04bf2341304d33fc0b1ebc4753
Vitaly Dushkin <vetalapo@gmail.com>
2024-03-16 23:05:52 +0100

    C-Sharp #525: Contiguous Array solution and tests

---
+++ c-sharp/Problems/arrays-and-hashing/ContiguousArray.cs
@@ -0,0 +1,67 @@
+﻿/**
+ * 525
+ * Contiguous Array
+ **
+ * Given a binary array nums,
+ * return the maximum length of a contiguous subarray with an equal number of 0 and 1.
+ *
+ * Example 1:
+ *   Input: nums = [0,1]
+ *   Output: 2
+ *   Explanation: [0, 1] is the longest contiguous subarray with an equal number of 0 and 1.
+ *
+ * Example 2:
+ *   Input: nums = [0,1,0]
+ *   Output: 2
+ *   Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+ *
+ * Constraints:
+ *   • 1 <= nums.length <= 10^5
+ *   • nums[i] is either 0 or 1.
+ **
+ * https://leetcode.com/problems/contiguous-array/
+***/
+
+namespace Problems;
+
+public class ContiguousArray
+{
+    public int FindMaxLength( int[] nums )
+    {
+        int zeros = 0;
+        int ones = 0;
+        int result = 0;
+
+        Dictionary<int, int> diffIndex = [];
+
+        for ( int i = 0; i < nums.Length; i++ )
+        {
+            int n = nums[i];
+
+            if ( n == 0 )
+            {
+                zeros++;
+            }
+            else
+            {
+                ones++;
+            }
+
+            if ( !diffIndex.ContainsKey( ones - zeros ) )
+            {
+                diffIndex[ones - zeros] = i;
+            }
+
+            if ( ones == zeros )
+            {
+                result = ones + zeros;
+            }
+            else
+            {
+                result = Math.Max( result, i - diffIndex[ones - zeros] );
+            }
+        }
+
+        return result;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/ContiguousArrayTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class ContiguousArrayTests
+{
+    [TestCase( new int[] { 0, 1 }, ExpectedResult = 2 )]
+    [TestCase( new int[] { 0, 1, 0 }, ExpectedResult = 2 )]
+    public int FindMaxLengthTest( int[] nums ) => new ContiguousArray().FindMaxLength( nums );
+}
