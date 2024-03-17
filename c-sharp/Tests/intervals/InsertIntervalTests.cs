8e75353ce5f1e4a319ba28b7f7b71fda540636f2
Vitaly Dushkin <vetalapo@gmail.com>
2024-03-17 21:31:00 +0100

    C-Sharp #57: Insert Interval, solution

---
+++ c-sharp/Tests/intervals/InsertIntervalTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class InsertIntervalTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int[][] InsertTest( int[][] intervals, int[] newInterval )
+        => new InsertInterval().Insert( intervals, newInterval );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( new int[][] { [1, 3], [6, 9] }, new int[] { 2, 5 } )
+                .Returns( new int[][] { [1, 5], [6, 9] } );
+
+            yield return new TestCaseData( new int[][] { [1, 2], [3, 5], [6, 7], [8, 10], [12, 16] }, new int[] { 4, 8 } )
+                .Returns( new int[][] { [1, 2], [3, 10], [12, 16] } );
+        }
+    }
+}
