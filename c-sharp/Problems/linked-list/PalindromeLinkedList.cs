69aa60db43da3920d9a2c68c2c3272bdd2a7fcbf
Vitaly Dushkin <vetalapo@gmail.com>
2024-03-22 23:29:31 +0100

    C-Sharp #234: Palindrome Linked List

---
+++ c-sharp/Problems/linked-list/PalindromeLinkedList.cs
@@ -0,0 +1,80 @@
+﻿/**
+ * 234
+ * Palindrome Linked List
+ **
+ * Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+ *
+ * Example 1:
+ *   Input: head = [1,2,2,1]
+ *   Output: true
+ *
+ * Example 2:
+ *   Input: head = [1,2]
+ *   Output: false
+ *
+ * Constraints:
+ *   • The number of nodes in the list is in the range [1, 105].
+ *   • 0 <= Node.val <= 9
+ *
+ * Follow up: Could you do it in O(n) time and O(1) space?
+ **
+ * https://leetcode.com/problems/palindrome-linked-list/
+***/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+***/
+
+namespace Problems;
+
+public class PalindromeLinkedList
+{
+    public bool IsPalindrome( ListNode head )
+    {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        // Find middle;
+        while ( fast is not null && fast.next is not null )
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        // Reverse the second half
+        ListNode prev = null;
+
+        while ( slow is not null )
+        {
+            ListNode temp = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = temp;
+        }
+
+        // Check if palindrome
+        ListNode left = head;
+        ListNode right = prev;
+
+        while ( right is not null )
+        {
+            if ( left.val != right.val )
+            {
+                return false;
+            }
+
+            left = left.next;
+            right = right.next;
+        }
+
+        return true;
+    }
+}
---
+++ c-sharp/Tests/linked-list/PalindromeLinkedListTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class PalindromeLinkedListTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public bool IsPalindromeTest( ListNode head ) =>
+        new PalindromeLinkedList().IsPalindrome( head );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( ListNode.Create( [1, 2, 2, 1] ) ).Returns( true );
+            yield return new TestCaseData( ListNode.Create( [1, 2] ) ).Returns( false );
+        }
+    }
+}
