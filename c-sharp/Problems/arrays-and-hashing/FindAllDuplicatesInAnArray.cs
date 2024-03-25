215a1535779317d1622f0646b2c454406dddb7ea
Vitaly Dushkin <vetalapo@gmail.com>
2024-03-25 17:56:25 +0100

    C-Sharp #442: Find All Duplicates in an Array solution

---
+++ c-sharp/Problems/arrays-and-hashing/FindAllDuplicatesInAnArray.cs
@@ -0,0 +1,72 @@
+﻿/**
+ * 442
+ * Find All Duplicates in an Array
+ **
+ * Given an integer array nums of length n where all the integers of nums are in the range [1, n]
+ * and each integer appears once or twice, return an array of all the integers that appears twice.
+ *
+ * You must write an algorithm that runs in O(n) time and uses only constant extra space.
+ *
+ * Example 1:
+ * Input: nums = [4,3,2,7,8,2,3,1]
+ * Output: [2,3]
+ *
+ * Example 2:
+ * Input: nums = [1,1,2]
+ * Output: [1]
+ *
+ * Example 3:
+ * Input: nums = [1]
+ * Output: []
+ *
+ * Constraints:
+ *   • n == nums.length
+ *   • 1 <= n <= 105
+ *   • 1 <= nums[i] <= n
+ *   • Each element in nums appears once or twice.
+ **
+ * https://leetcode.com/problems/find-all-duplicates-in-an-array/
+***/
+
+using System;
+using System.Collections.Generic;
+
+namespace Problems;
+
+public class FindAllDuplicatesInAnArray
+{
+    public IList<int> FindDuplicates( int[] nums )
+    {
+        List<int> result = [];
+
+        foreach ( int num in nums )
+        {
+            int absNum = Math.Abs( num );
+
+            if ( nums[absNum - 1] < 0 )
+            {
+                result.Add( absNum );
+            }
+
+            nums[absNum - 1] = -nums[absNum - 1];
+        }
+
+        return result;
+    }
+
+    public IList<int> FindDuplicatesHashSet( int[] nums )
+    {
+        List<int> result = [];
+        HashSet<int> checkSet = [];
+
+        foreach ( int num in nums )
+        {
+            if ( !checkSet.Add( num ) )
+            {
+                result.Add( num );
+            }
+        }
+
+        return result;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/FindAllDuplicatesInAnArrayTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+namespace Problems;
+
+public class FindAllDuplicatesInAnArrayTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public IList<int> FindDuplicatesTest( int[] nums )
+        => new FindAllDuplicatesInAnArray().FindDuplicates( nums );
+
+    [TestCaseSource( nameof( TestCases ) )]
+    public IList<int> FindDuplicatesHashSetTest( int[] nums )
+        => new FindAllDuplicatesInAnArray().FindDuplicatesHashSet( nums );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( [new int[] { 4, 3, 2, 7, 8, 2, 3, 1 }] ).Returns( new List<int> { 2, 3 } );
+            yield return new TestCaseData( [new int[] { 1, 1, 2 }] ).Returns( new List<int> { 1 } );
+            yield return new TestCaseData( [new int[] { 1 }] ).Returns( new List<int> { } );
+        }
+    }
+}
