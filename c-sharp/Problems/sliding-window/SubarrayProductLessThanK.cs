f893e1f1bae86e98e94090d24565f3de18c777c5
Vitaly Dushkin <vetalapo@gmail.com>
2024-03-27 17:52:06 +0100

    C-Sharp #713: Subarray Product Less Than K

---
+++ c-sharp/Problems/sliding-window/SubarrayProductLessThanK.cs
@@ -0,0 +1,66 @@
+﻿/**
+ * 713
+ * Subarray Product Less Than K
+ **
+ * Given an array of integers nums and an integer k,
+ * return the number of contiguous subarrays
+ * where the product of all the elements in the subarray is strictly less than k. 
+ *
+ * Example 1:
+ *   Input: nums = [10,5,2,6], k = 100
+ *   Output: 8
+ *   Explanation:
+ *     The 8 subarrays that have product less than 100 are:
+ *     [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
+ *      Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
+ *
+ * Example 2:
+ *   Input: nums = [1,2,3], k = 0
+ *   Output: 0
+ *
+ * Constraints:
+ *   • 1 <= nums.length <= 3 * 10^4
+ *   • 1 <= nums[i] <= 1000
+ *   • 0 <= k <= 10^6
+ *
+ * Hint 1:
+ *   For each j,
+ *   let opt(j) be the smallest i
+ *   so that nums[i] * nums[i+1] * ... * nums[j]
+ *   is less than k. opt is an increasing function.
+ **
+ * https://leetcode.com/problems/subarray-product-less-than-k/
+***/
+
+namespace Problems;
+
+public class SubarrayProductLessThanK
+{
+    public int NumSubarrayProductLessThanK( int[] nums, int k )
+    {
+        if ( k <= 1 )
+        {
+            return 0;
+        }
+
+        int result = 0;
+
+        int left = 0;
+        int product = 1;
+
+        for ( int right = 0; right < nums.Length; right++ )
+        {
+            product *= nums[right];
+
+            while ( left <= right && product >= k )
+            {
+                product /= nums[left];
+                left++;
+            }
+
+            result += right - left + 1;
+        }
+
+        return result;
+    }
+}
---
+++ c-sharp/Tests/sliding-window/SubarrayProductLessThanKTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class SubarrayProductLessThanKTests
+{
+    [TestCase( new int[] { 10, 5, 2, 6 }, 100, ExpectedResult = 8 )]
+    [TestCase( new int[] { 1, 2, 3 }, 0, ExpectedResult = 0 )]
+    public int NumSubarrayProductLessThanKTest( int[] nums, int k )
+        => new SubarrayProductLessThanK().NumSubarrayProductLessThanK( nums, k );
+}
