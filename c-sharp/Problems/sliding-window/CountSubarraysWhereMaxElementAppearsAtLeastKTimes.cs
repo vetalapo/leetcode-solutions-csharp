b352e9f0e607a21a99c52ed74a62399abb00da37
Vitaly Dushkin <vetalapo@gmail.com>
2024-03-29 03:53:16 +0100

    C-Sharp #2962: Count Subarrays Where Max Element Appears at Least K Times

---
+++ c-sharp/Problems/sliding-window/CountSubarraysWhereMaxElementAppearsAtLeastKTimes.cs
@@ -0,0 +1,63 @@
+﻿/**
+ * 2962
+ * Count Subarrays Where Max Element Appears at Least K Times
+ **
+ * You are given an integer array nums and a positive integer k.
+ * Return the number of subarrays
+ * where the maximum element of nums appears at least k times in that subarray.
+ * A subarray is a contiguous sequence of elements within an array.
+ *
+ * Example 1:
+ *   Input: nums = [1,3,2,3,3], k = 2
+ *   Output: 6
+ *   Explanation: The subarrays that contain the element 3 at least 2 times are: [1,3,2,3], [1,3,2,3,3], [3,2,3], [3,2,3,3], [2,3,3] and [3,3].
+ *
+ * Example 2:
+ *   Input: nums = [1,4,2,1], k = 3
+ *   Output: 0
+ *   Explanation: No subarray contains the element 4 at least 3 times.
+ *
+ * Constraints:
+ *   • 1 <= nums.length <= 10^5
+ *   • 1 <= nums[i] <= 10^6
+ *   • 1 <= k <= 10^5
+ **
+ * https://leetcode.com/problems/count-subarrays-where-max-element-appears-at-least-k-times/
+***/
+
+using System.Linq;
+
+namespace Problems;
+
+public class CountSubarraysWhereMaxElementAppearsAtLeastKTimes
+{
+    public long CountSubarrays( int[] nums, int k )
+    {
+        int maxNumber = nums.Max();
+        int maxCount = 0;
+        long result = 0;
+        int left = 0;
+
+        for ( int right = 0; right < nums.Length; right++ )
+        {
+            if ( nums[right] == maxNumber )
+            {
+                maxCount++;
+            }
+
+            while ( maxCount == k )
+            {
+                if ( nums[left] == maxNumber )
+                {
+                    maxCount--;
+                }
+
+                left++;
+            }
+
+            result += left;
+        }
+
+        return result;
+    }
+}
---
+++ c-sharp/Tests/sliding-window/CountSubarraysWhereMaxElementAppearsAtLeastKTimesTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CountSubarraysWhereMaxElementAppearsAtLeastKTimesTests
+{
+    [TestCase( new int[] { 1, 3, 2, 3, 3 }, 2, ExpectedResult = 6 )]
+    [TestCase( new int[] { 1, 4, 2, 1 }, 3, ExpectedResult = 0 )]
+    public long CountSubarraysTest( int[] nums, int k )
+        => new CountSubarraysWhereMaxElementAppearsAtLeastKTimes().CountSubarrays( nums, k );
+}
