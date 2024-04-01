45b4728f9502a0750384de2a9cfc1d7039ef1b22
Vitaly Dushkin <vetalapo@gmail.com>
2024-04-01 04:23:34 +0200

    C-Sharp #58: Length of Last Word

---
+++ c-sharp/Tests/arrays-and-hashing/LengthOfLastWordSolutionTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class LengthOfLastWordSolutionTests
+{
+    [TestCase( "Hello World", ExpectedResult = 5 )]
+    [TestCase( "   fly me   to   the moon  ", ExpectedResult = 4 )]
+    [TestCase( "luffy is still joyboy", ExpectedResult = 6 )]
+    public int LengthOfLastWordTest( string s )
+        => new LengthOfLastWordSolution().LengthOfLastWord( s );
+}
