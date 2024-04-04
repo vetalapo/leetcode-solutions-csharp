d82c5363e7d54bc0506fd8b270f42af12fcdb1e7
Vitaly Dushkin <vetalapo@gmail.com>
2024-04-04 17:16:58 +0200

    C-Sharp #1614: Maximum Nesting Depth of the Parentheses solution

---
+++ c-sharp/Tests/stack/MaximumNestingDepthOfTheParenthesesTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MaximumNestingDepthOfTheParenthesesTests
+{
+    [TestCase( "(1+(2*3)+((8)/4))+1", ExpectedResult = 3 )]
+    [TestCase( "(1)+((2))+(((3)))", ExpectedResult = 3 )]
+    public int MaxDepthTest( string s )
+        => new MaximumNestingDepthOfTheParentheses().MaxDepth( s );
+}
