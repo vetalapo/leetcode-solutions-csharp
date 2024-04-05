a14582413dec42e1126da371849f97208aa61c8f
Vitaly Dushkin <vetalapo@gmail.com>
2024-04-05 23:20:31 +0200

    C-Sharp #1544: Make The String Great

---
+++ c-sharp/Problems/stack/MakeTheStringGreat.cs
@@ -0,0 +1,86 @@
+﻿/**
+ * 1544
+ * Make The String Great
+ **
+ * Given a string s of lower and upper case English letters.
+ *
+ * A good string is a string which doesn't have two adjacent characters s[i] and s[i + 1] where:
+ *   • 0 <= i <= s.length - 2
+ *   • s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.
+ *
+ * To make the string good,
+ * you can choose two adjacent characters
+ * that make the string bad and remove them.
+ * You can keep doing this until the string becomes good.
+ *
+ * Return the string after making it good.
+ * The answer is guaranteed to be unique under the given constraints.
+ *
+ * Notice that an empty string is also good.
+ *
+ * Example 1:
+ *   Input: s = "leEeetcode"
+ *   Output: "leetcode"
+ *   Explanation:
+ *     In the first step,
+ *     either you choose i = 1 or i = 2,
+ *     both will result "leEeetcode" to be reduced to "leetcode".
+ *
+ * Example 2:
+ *   Input: s = "abBAcC"
+ *   Output: ""
+ *   Explanation:
+ *     We have many possible scenarios, and all lead to the same answer.
+ *     For example:
+ *       "abBAcC" --> "aAcC" --> "cC" --> ""
+ *       "abBAcC" --> "abBA" --> "aA" --> ""
+ *
+ * Example 3:
+ *   Input: s = "s"
+ *   Output: "s"
+ *
+ * Constraints:
+ *   • 1 <= s.length <= 100
+ *   • s contains only lower and upper case English letters.
+ *
+ * Hint 1:
+ *   The order you choose 2 characters to remove doesn't matter.
+ *
+ * Hint 2:
+ *   Keep applying the mentioned step to s till the length of the string is not changed.
+ **
+ * https://leetcode.com/problems/make-the-string-great/
+***/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problems;
+
+public class MakeTheStringGreat
+{
+    public string MakeGood( string s )
+    {
+        if ( s.Length < 2 )
+        {
+            return s;
+        }
+
+        Stack<char> stack = [];
+
+        foreach ( char c in s )
+        {
+            if ( stack.TryPeek( out char stackTopVal ) && Math.Abs( stackTopVal - c ) == 32 )
+            {
+                stack.Pop();
+            }
+            else
+            {
+                stack.Push( c );
+            }
+        }
+
+        return String.Join( string.Empty, stack.ToArray().Reverse() );
+    }
+}
---
+++ c-sharp/Tests/stack/MakeTheStringGreatTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MakeTheStringGreatTests
+{
+    [TestCase( "leEeetcode", ExpectedResult = "leetcode" )]
+    [TestCase( "abBAcC", ExpectedResult = "" )]
+    [TestCase( "s", ExpectedResult = "s" )]
+    public string MakeGoodTest( string s )
+        => new MakeTheStringGreat().MakeGood( s );
+}
