a9eaaff3124aa44d24d1a653e4cb7a3758f423de
Vitaly Dushkin <vetalapo@gmail.com>
2024-04-07 08:40:14 +0200

    C-Sharp #678: Valid Parenthesis String solution

---
+++ c-sharp/Tests/greedy/ValidParenthesisStringTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class ValidParenthesisStringTests
+{
+    [TestCase( "()", ExpectedResult = true )]
+    [TestCase( "(*)", ExpectedResult = true )]
+    [TestCase( "(*))", ExpectedResult = true )]
+    public bool CheckValidStringTest( string s )
+        => new ValidParenthesisString().CheckValidString( s );
+}
