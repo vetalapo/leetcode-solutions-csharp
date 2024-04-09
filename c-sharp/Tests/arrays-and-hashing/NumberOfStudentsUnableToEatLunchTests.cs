280085ab9006e9832fc7b1954dc3368f30c9f0b1
Vitaly Dushkin <vetalapo@gmail.com>
2024-04-09 02:06:58 +0200

    C-Sharp #1700: Number of Students Unable to Eat Lunch

---
+++ c-sharp/Tests/arrays-and-hashing/NumberOfStudentsUnableToEatLunchTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class NumberOfStudentsUnableToEatLunchTests
+{
+    [TestCase( new int[] { 1, 1, 0, 0 }, new int[] { 0, 1, 0, 1 }, ExpectedResult = 0 )]
+    [TestCase( new int[] { 1, 1, 1, 0, 0, 1 }, new int[] { 1, 0, 0, 0, 1, 1 }, ExpectedResult = 3 )]
+    public int CountStudentsTest( int[] students, int[] sandwiches )
+        => new NumberOfStudentsUnableToEatLunch().CountStudents( students, sandwiches );
+}
