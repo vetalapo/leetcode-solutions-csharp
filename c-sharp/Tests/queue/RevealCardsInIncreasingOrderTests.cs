c265b108d53151a687f835f574ee4e9b721cb72e
Vitaly Dushkin <vetalapo@gmail.com>
2024-04-11 01:54:26 +0200

    C-Sharp #950: Reveal Cards In Incleasing Order

---
+++ c-sharp/Tests/queue/RevealCardsInIncreasingOrderTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class RevealCardsInIncreasingOrderTests
+{
+    [TestCase( new int[] { 17, 13, 11, 2, 3, 5, 7 }, ExpectedResult = new int[] { 2, 13, 3, 11, 5, 17, 7 } )]
+    [TestCase( new int[] { 1, 1000 }, ExpectedResult = new int[] { 1, 1000 } )]
+    public int[] DeckRevealedIncreasingTest( int[] deck )
+        => new RevealCardsInIncreasingOrder().DeckRevealedIncreasing( deck );
+}
