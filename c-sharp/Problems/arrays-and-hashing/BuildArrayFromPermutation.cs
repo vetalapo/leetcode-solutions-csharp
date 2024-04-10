bcee353e72c727f43effcb7fe801e51bc59685b4
Vitaly Dushkin <vetalapo@gmail.com>
2024-04-10 23:46:51 +0200

    White space changes

--- c-sharp/Problems/arrays-and-hashing/BuildArrayFromPermutation.cs
+++ c-sharp/Problems/arrays-and-hashing/BuildArrayFromPermutation.cs
@@ -15,7 +15,7 @@
  *   ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
  *       = [nums[0], nums[2], nums[1], nums[5], nums[3], nums[4]]
  *       = [0,1,2,4,5,3]
- *       
+ *
  * Example 2:
  *   Input: nums = [5,0,1,2,3,4]
  *   Output: [4,5,0,1,2,3]
@@ -24,8 +24,7 @@
  *       = [nums[5], nums[0], nums[1], nums[2], nums[3], nums[4]]
  *       = [4,5,0,1,2,3]
  **
- * https://leetcode.com/problems/build-array-from-permutation/description/
- **
+ * https://leetcode.com/problems/build-array-from-permutation/
 ***/
 
 namespace Problems;
