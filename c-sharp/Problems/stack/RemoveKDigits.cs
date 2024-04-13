e3aac6ad83e41754b3b716ff7b5ac1aaa0ab716e
Vitaly Dushkin <vetalapo@gmail.com>
2024-04-14 01:34:44 +0200

    C-Sharp #402: Remove K Digits

---
+++ c-sharp/Problems/stack/RemoveKDigits.cs
@@ -0,0 +1,79 @@
+﻿/**
+ * 402
+ * Remove K Digits
+ **
+ * Given string num representing a non-negative integer num,
+ * and an integer k,
+ * return the smallest possible integer after removing k digits from num.
+ *
+ * Example 1:
+ *   Input: num = "1432219", k = 3
+ *   Output: "1219"
+ *   Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+ *
+ * Example 2:
+ *   Input: num = "10200", k = 1
+ *   Output: "200"
+ *   Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+ *
+ * Example 3:
+ *   Input: num = "10", k = 2
+ *   Output: "0"
+ *   Explanation: Remove all the digits from the number and it is left with nothing which is 0.
+ *
+ * Constraints:
+ *   • 1 <= k <= num.length <= 10^5
+ *   • num consists of only digits.
+ *   • num does not have any leading zeros except for the zero itself.
+ **
+ * https://leetcode.com/problems/remove-k-digits/
+***/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problems;
+
+public class RemoveKDigitsSolution
+{
+    public string RemoveKDigits( string num, int k )
+    {
+        Stack<char> stack = [];
+
+        foreach ( char c in num )
+        {
+            while ( stack.TryPeek( out char stackValue ) && k > 0 && stackValue > c )
+            {
+                stack.Pop();
+                k--;
+            }
+
+            stack.Push( c );
+        }
+
+        while ( k > 0 )
+        {
+            stack.Pop();
+            k--;
+        }
+
+        string result = String.Join( "", stack.ToArray().Reverse() );
+
+        int left = 0;
+
+        while ( left < result.Length && result[left] == '0' )
+        {
+            left++;
+        }
+
+        result = result[left..];
+
+        if ( result == string.Empty )
+        {
+            return "0";
+        }
+
+        return result;
+    }
+}
---
+++ c-sharp/Tests/stack/RemoveKDigitsSolutionTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class RemoveKDigitsSolutionTests
+{
+    [TestCase( "1432219", 3, ExpectedResult = "1219" )]
+    [TestCase( "10200", 1, ExpectedResult = "200" )]
+    [TestCase( "10", 2, ExpectedResult = "0" )]
+    public string RemoveKDigitsTest( string num, int k )
+        => new RemoveKDigitsSolution().RemoveKDigits( num, k );
+}
