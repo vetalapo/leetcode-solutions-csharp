1d3077721fa5f5aa971e259d1af6787bfaca1091
Vitaly Dushkin <vetalapo@gmail.com>
2024-04-26 17:14:44 +0200

    #981 description update

--- c-sharp/Problems/binary-search/TimeBasedKeyValueStore.cs
+++ c-sharp/Problems/binary-search/TimeBasedKeyValueStore.cs
@@ -5,18 +5,18 @@
  * Design a time-based key-value data structure that
  * can store multiple values for the same key at different time stamps
  * and retrieve the key's value at a certain timestamp.
- * 
+ *
  * Implement the TimeMap class:
  *   TimeMap() Initializes the object of the data structure.
- *   
+ *
  *   void set(String key, String value, int timestamp) Stores the key key with the value value at the given time timestamp.
- *   
+ *
  *   String get(String key, int timestamp) Returns a value such that set was called previously, with timestamp_prev <= timestamp.
  *     If there are multiple such values, it returns the value associated with the largest timestamp_prev.
  *     If there are no values, it returns "".
- * 
+ **
  * https://leetcode.com/problems/time-based-key-value-store/
- */
+***/
 
 using System.Collections.Generic;
 using System.Linq;
@@ -29,14 +29,14 @@ public class TimeMap
 
     public TimeMap()
     {
-        this._store = new Dictionary<string, List<(int timestamp, string value)>>();
+        this._store = [];
     }
 
     public void Set( string key, string value, int timestamp )
     {
         if ( !this._store.ContainsKey( key ) )
         {
-            this._store[key] = new List<(int timestamp, string value)>();
+            this._store[key] = [];
         }
 
         this._store[key].Add( (timestamp, value) );
