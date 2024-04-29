c1705714b2b7311e0040bcd16baa95e464a7f85e
Vitaly Dushkin <vetalapo@gmail.com>
2024-04-29 19:52:39 +0200

    White space changes

--- c-sharp/Problems/linked-list/MergeInBetweenLinkedLists.cs
+++ c-sharp/Problems/linked-list/MergeInBetweenLinkedLists.cs
@@ -11,13 +11,12 @@
  *   Input: list1 = [10,1,13,6,9,5], a = 3, b = 4, list2 = [1000000,1000001,1000002]
  *   Output: [10,1,13,1000000,1000001,1000002,5]
  *   Explanation: We remove the nodes 3 and 4 and put the entire list2 in their place. The blue edges and nodes in the above figure indicate the result.
- * 
+ *
  * Example 2:
  *   Input: list1 = [0,1,2,3,4,5,6], a = 2, b = 5, list2 = [1000000,1000001,1000002,1000003,1000004]
  *   Output: [0,1,1000000,1000001,1000002,1000003,1000004,6]
  *   Explanation: The blue edges and nodes in the above figure indicate the result.
- *  
- * 
+ *
  * Constraints:
  *   • 3 <= list1.length <= 10^4
  *   • 1 <= a <= b < list1.length - 1
