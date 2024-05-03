8fb6eff1de3ebbb86c5d6c58611d683002459234
Vitaly Dushkin <vetalapo@gmail.com>
2024-05-03 02:45:03 +0200

    White space changes

--- c-sharp/Problems/linked-list/ReverseLinkedList.cs
+++ c-sharp/Problems/linked-list/ReverseLinkedList.cs
@@ -3,7 +3,7 @@
  * Reverse Linked List
  **
  * Given the head of a singly linked list, reverse the list, and return the reversed list.
- * 
+ *
  * Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
  **
  * https://leetcode.com/problems/reverse-linked-list/
