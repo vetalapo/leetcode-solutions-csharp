f4a83f1e5c273fda41eba256e0676b289fe6ad69
Vitaly Dushkin <vetalapo@gmail.com>
2024-05-05 11:32:11 +0200

    #7 description update

--- c-sharp/Problems/math/ReverseInteger.cs
+++ c-sharp/Problems/math/ReverseInteger.cs
@@ -18,7 +18,7 @@
  *   Input: x = 120
  *   Output: 21
  *
- * Constraints: -231 <= x <= 231 - 1
+ * Constraints: -2^31 <= x <= 2^31 - 1
  **
  * https://leetcode.com/problems/reverse-integer/
 ***/
