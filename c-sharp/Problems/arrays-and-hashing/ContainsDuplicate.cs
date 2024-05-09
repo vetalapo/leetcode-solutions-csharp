ac8cd53555495a63c181926d82a5ae68592caa10
Vitaly Dushkin <vetalapo@gmail.com>
2024-05-09 11:45:21 +0200

    #217: description update

--- c-sharp/Problems/arrays-and-hashing/ContainsDuplicate.cs
+++ c-sharp/Problems/arrays-and-hashing/ContainsDuplicate.cs
@@ -19,8 +19,8 @@
  *   Output: true
  *
  * Constraints:
- *   1 <= nums.length <= 105
- *   -109 <= nums[i] <= 109
+ *   1 <= nums.length <= 10^5
+ *   -10^9 <= nums[i] <= 10^9
  **
  * https://leetcode.com/problems/contains-duplicate/
 ***/
