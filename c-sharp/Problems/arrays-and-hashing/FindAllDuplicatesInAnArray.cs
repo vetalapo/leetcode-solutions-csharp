0f53fba2d488bfb225dc9ca26f882a18e422bb22
Vitaly Dushkin <vetalapo@gmail.com>
2024-05-11 13:31:43 +0200

    #442: description update

--- c-sharp/Problems/arrays-and-hashing/FindAllDuplicatesInAnArray.cs
+++ c-sharp/Problems/arrays-and-hashing/FindAllDuplicatesInAnArray.cs
@@ -8,20 +8,20 @@
  * You must write an algorithm that runs in O(n) time and uses only constant extra space.
  *
  * Example 1:
- * Input: nums = [4,3,2,7,8,2,3,1]
- * Output: [2,3]
+ *   Input: nums = [4,3,2,7,8,2,3,1]
+ *   Output: [2,3]
  *
  * Example 2:
- * Input: nums = [1,1,2]
- * Output: [1]
+ *   Input: nums = [1,1,2]
+ *   Output: [1]
  *
  * Example 3:
- * Input: nums = [1]
- * Output: []
+ *   Input: nums = [1]
+ *   Output: []
  *
  * Constraints:
  *   • n == nums.length
- *   • 1 <= n <= 105
+ *   • 1 <= n <= 10^5
  *   • 1 <= nums[i] <= n
  *   • Each element in nums appears once or twice.
  **
