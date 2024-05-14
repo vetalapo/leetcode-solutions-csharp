b85ca1ab2e594db891c4aa8e36e2e416500c3742
Vitaly Dushkin <vetalapo@gmail.com>
2024-05-14 12:09:17 +0200

    white space changes

--- c-sharp/Problems/arrays-and-hashing/ConcatenationOfArray.cs
+++ c-sharp/Problems/arrays-and-hashing/ConcatenationOfArray.cs
@@ -3,7 +3,7 @@
  * Concatenation of Array
  **
  * Given an integer array nums of length n,
- * you want to create an array ans of length 2n 
+ * you want to create an array ans of length 2n
  * where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
  * Specifically, ans is the concatenation of two nums arrays.
  *
@@ -25,7 +25,7 @@
  *     - ans = [nums[0],nums[1],nums[2],nums[3],nums[0],nums[1],nums[2],nums[3]]
  *     - ans = [1,3,2,1,1,3,2,1]
  *
- * Constraints: 
+ * Constraints:
  *   n == nums.length
  *   1 <= n <= 1000
  *   1 <= nums[i] <= 1000
