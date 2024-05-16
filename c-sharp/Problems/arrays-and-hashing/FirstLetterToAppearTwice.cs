a50de5f29f844fdf3d9ad35e7840d89660f34161
Vitaly Dushkin <vetalapo@gmail.com>
2024-05-16 11:55:38 +0200

    #2351: description update

--- c-sharp/Problems/arrays-and-hashing/FirstLetterToAppearTwice.cs
+++ c-sharp/Problems/arrays-and-hashing/FirstLetterToAppearTwice.cs
@@ -15,22 +15,22 @@
  *     • The letter 'b' appears on the indexes 1 and 4.
  *     • The letter 'c' appears on the indexes 2, 3 and 7.
  *     • The letter 'z' appears on the index 8.
- *     • The letter 'c' is the first letter to appear twice, because out of all the letters the index of its second occurrence is the smallest.
- * 
+ *     • The letter 'c' is the first letter to appear twice,
+ *       because out of all the letters the index of its second occurrence is the smallest.
+ *
  * Example 2:
  *   Input: s = "abcdd"
  *   Output: "d"
  *   Explanation: The only letter that appears twice is 'd' so we return 'd'.
- *  
- * 
+ *
  * Constraints:
  *   • 2 <= s.length <= 100
  *   • s consists of lowercase English letters.
  *   • s has at least one repeated letter.
- * 
+ *
  * Hint 1: Iterate through the string from left to right.
  *         Keep track of the elements you have already seen in a set.
- * 
+ *
  * Hint 2: If the current element is already in the set, return that element.
  **
  * https://leetcode.com/problems/first-letter-to-appear-twice/
@@ -44,7 +44,7 @@ public class FirstLetterToAppearTwice
 {
     public char RepeatedCharacter( string s )
     {
-        HashSet<char> charMap = new();
+        HashSet<char> charMap = [];
 
         foreach ( char c in s )
         {
