0b525b01e6163a443dcb7c6c8398cf9bdc6d4358
Vitaly Dushkin <vetalapo@gmail.com>
2024-05-29 11:14:10 +0200

    #657: description update

--- c-sharp/Problems/arrays-and-hashing/RobotReturnToOrigin.cs
+++ c-sharp/Problems/arrays-and-hashing/RobotReturnToOrigin.cs
@@ -6,9 +6,9 @@
  * Given a sequence of its moves, judge if this robot ends up at (0, 0) after it completes its moves.
  * You are given a string moves that represents the move sequence of the robot where moves[i] represents its ith move.
  * Valid moves are 'R' (right), 'L' (left), 'U' (up), and 'D' (down).
- * 
+ *
  * Return true if the robot returns to the origin after it finishes all of its moves, or false otherwise.
- * 
+ *
  * Note: The way that the robot is "facing" is irrelevant.
  * 'R' will always make the robot move to the right once,
  * 'L' will always make it move left, etc.
@@ -25,7 +25,7 @@
  *   Explanation: The robot moves left twice. It ends up two "moves" to the left of the origin. We return false because it is not at the origin at the end of its moves.
  *
  * Constraints:
- *   • 1 <= moves.length <= 2 * 104
+ *   • 1 <= moves.length <= 2 * 10^4
  *   • moves only contains the characters 'U', 'D', 'L' and 'R'.
  **
  * https://leetcode.com/problems/robot-return-to-origin/
