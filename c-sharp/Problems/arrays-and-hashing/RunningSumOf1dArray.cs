fa97cdeb81c72c20f49763451f80e5c6ce28a8cf
Vitaly Dushkin <vetalapo@gmail.com>
2024-05-30 12:45:48 +0200

    #1480: description update

--- c-sharp/Problems/arrays-and-hashing/RunningSumOf1dArray.cs
+++ c-sharp/Problems/arrays-and-hashing/RunningSumOf1dArray.cs
@@ -2,7 +2,9 @@
  * 1480
  * Running Sum of 1d Array
  **
- * Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+ * Given an array nums.
+ * We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+ *
  * Return the running sum of nums.
  *
  * Example 1:
