c263c6541e77a9e1eb9ef81e65cfdcb9888ad086
Vitaly Dushkin <vetalapo@gmail.com>
2024-06-01 10:29:53 +0200

    #2788: description update

--- c-sharp/Problems/arrays-and-hashing/SplitStringsBySeparator.cs
+++ c-sharp/Problems/arrays-and-hashing/SplitStringsBySeparator.cs
@@ -18,10 +18,10 @@
  *     In this example we split as follows:
  *       - "one.two.three" splits into "one", "two", "three"
  *       - "four.five" splits into "four", "five"
- *       - "six" splits into "six" 
- * 
+ *       - "six" splits into "six"
+ *
  *     Hence, the resulting array is ["one","two","three","four","five","six"].
- * 
+ *
  * Example 2:
  *   Input: words = ["$easy$","$problem$"], separator = "$"
  *   Output: ["easy","problem"]
@@ -29,24 +29,24 @@
  *     In this example we split as follows:
  *       - "$easy$" splits into "easy" (excluding empty strings)
  *       - "$problem$" splits into "problem" (excluding empty strings)
- *   
+ *
  *     Hence, the resulting array is ["easy","problem"].
- * 
+ *
  * Example 3:
  *   Input: words = ["|||"], separator = "|"
  *   Output: []
- *   Explanation: In this example the resulting split of "|||" will contain only empty strings, so we return an empty array []. 
- *  
- * 
+ *   Explanation: In this example the resulting split of "|||" will contain only empty strings, so we return an empty array [].
+ *
  * Constraints:
  *   • 1 <= words.length <= 100
  *   • 1 <= words[i].length <= 20
  *   • characters in words[i] are either lowercase English letters or characters from the string ".,|$#@" (excluding the quotes)
  *   • separator is a character from the string ".,|$#@" (excluding the quotes)
- * 
+ *
  * Hint 1:
- *   Iterate over each string in the given array using a loop and perform string splitting based on the provided separator character.
- *   
+ *   Iterate over each string in the given array using a loop
+ *   and perform string splitting based on the provided separator character.
+ *
  * Hint 2:
  *   Be sure not to return empty strings.
  **
