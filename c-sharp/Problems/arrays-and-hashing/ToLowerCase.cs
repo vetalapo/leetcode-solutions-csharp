b41a2d9bb1e761e496f435d991b1dcc99f16bb73
Vitaly Dushkin <vetalapo@gmail.com>
2024-06-02 18:18:05 +0200

    #709: description update

--- c-sharp/Problems/arrays-and-hashing/ToLowerCase.cs
+++ c-sharp/Problems/arrays-and-hashing/ToLowerCase.cs
@@ -4,30 +4,30 @@
  **
  * Given a string s,
  * return the string after replacing every uppercase letter with the same lowercase letter.
- * 
+ *
  * Example 1:
  *   Input: s = "Hello"
  *   Output: "hello"
- * 
+ *
  * Example 2:
  *   Input: s = "here"
  *   Output: "here"
- * 
+ *
  * Example 3:
  *   Input: s = "LOVELY"
  *   Output: "lovely"
- * 
+ *
  * Constraints:
  *   • 1 <= s.length <= 100
  *   • s consists of printable ASCII characters.
- * 
+ *
  * Hint 1:
  *   Most languages support lowercase conversion for a string data type.
  *   However, that is certainly not the purpose of the problem.
  *   Think about how the implementation of the lowercase function call can be done easily.
- * 
+ *
  * Hint 2: Think ASCII!
- * 
+ *
  * Hint 3:
  *   Think about the different capital letters and their ASCII codes
  *   and how that relates to their lowercase counterparts.
