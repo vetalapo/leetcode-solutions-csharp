dc17676b513e48b317186b401c585c4e0f510250
Vitaly Dushkin <vetalapo@gmail.com>
2024-06-06 12:40:43 +0200

    #1 description update

--- c-sharp/Problems/arrays-and-hashing/TwoSum.cs
+++ c-sharp/Problems/arrays-and-hashing/TwoSum.cs
@@ -4,7 +4,9 @@
  **
  * Given an array of integers nums and an integer target,
  * return indices of the two numbers such that they add up to target.
- * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * You may assume that each input would have exactly one solution,
+ * and you may not use the same element twice.
  *
  * You can return the answer in any order.
  *
@@ -22,9 +24,9 @@
  *   Output: [0,1]
  *
  * Constraints:
- *   • 2 <= nums.length <= 104
- *   • -109 <= nums[i] <= 109
- *   • -109 <= target <= 109
+ *   • 2 <= nums.length <= 10^4
+ *   • -10^9 <= nums[i] <= 10^9
+ *   • -10^9 <= target <= 10^9
  *   • Only one valid answer exists.
  *
  * Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
