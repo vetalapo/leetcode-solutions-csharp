012b472795b961eaf6228ae5293736a6d636c620
Vitaly Dushkin <vetalapo@gmail.com>
2024-06-07 11:43:42 +0200

    #242: description update

--- c-sharp/Problems/arrays-and-hashing/ValidAnagram.cs
+++ c-sharp/Problems/arrays-and-hashing/ValidAnagram.cs
@@ -15,10 +15,12 @@
  *   Output: false
  *
  * Constraints:
- *   • 1 <= s.length, t.length <= 5 * 104
+ *   • 1 <= s.length, t.length <= 5 * 10^4
  *   • s and t consist of lowercase English letters.
  *
- * Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+ * Follow up:
+ *   What if the inputs contain Unicode characters?
+ *   How would you adapt your solution to such a case?
  **
  * https://leetcode.com/problems/valid-anagram/
 ***/
