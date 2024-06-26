8e8f2327238f732ecb366ac87bde73ebbcaa55bd
Vitaly Dushkin <vetalapo@gmail.com>
2024-06-26 11:29:53 +0200

    #2469: description and solution update

--- c-sharp/Problems/math/ConvertTheTemperature.cs
+++ c-sharp/Problems/math/ConvertTheTemperature.cs
@@ -2,7 +2,8 @@
  * 2469
  * Convert the Temperature
  **
- * You are given a non-negative floating point number rounded to two decimal places celsius, that denotes the temperature in Celsius.
+ * You are given a non-negative floating point number rounded to two decimal places Celsius,
+ * that denotes the temperature in Celsius.
  * You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, fahrenheit].
  *
  * Return the array ans. Answers within 10-5 of the actual answer will be accepted.
@@ -20,6 +21,9 @@
  *   Input: celsius = 122.11
  *   Output: [395.26000,251.79800]
  *   Explanation: Temperature at 122.11 Celsius converted in Kelvin is 395.26 and converted in Fahrenheit is 251.798.
+ *
+ * Constraints:
+ *   • 0 <= celsius <= 1000
  **
  * https://leetcode.com/problems/convert-the-temperature/
 ***/
@@ -31,6 +35,6 @@ public class ConvertTheTemperature
 {
     public double[] ConvertTemperature( double celsius )
     {
-        return new double[] { celsius + 273.15, celsius * 1.8 + 32 };
+        return [celsius + 273.15, celsius * 1.8 + 32];
     }
 }
