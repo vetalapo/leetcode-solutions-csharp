423ec138b3d61f8e99f45427aff2ca87bbe9cf1d
Vitaly Dushkin <vetalapo@gmail.com>
2024-06-27 11:31:02 +0200

    2769: description update

--- c-sharp/Problems/math/FindTheMaximumAchievableNumber.cs
+++ c-sharp/Problems/math/FindTheMaximumAchievableNumber.cs
@@ -3,16 +3,20 @@
  * Find the Maximum Achievable Number
  **
  * You are given two integers, num and t.
- * An integer x is called achievable if it can become equal to num after applying the following operation no more than t times:
- * Increase or decrease x by 1, and simultaneously increase or decrease num by 1.
+ * An integer x is called achievable if it can become equal to num
+ * after applying the following operation no more than t times:
+ * Increase or decrease x by 1,
+ * and simultaneously increase or decrease num by 1.
  *
- * Return the maximum possible achievable number. It can be proven that there exists at least one achievable number.
+ * Return the maximum possible achievable number.
+ * It can be proven that there exists at least one achievable number.
  *
  * Example 1:
  *   Input: num = 4, t = 1
  *   Output: 6
  *   Explanation:
- *     The maximum achievable number is x = 6; it can become equal to num after performing this operation:
+ *     The maximum achievable number is x = 6;
+ *     it can become equal to num after performing this operation:
  *       1- Decrease x by 1, and increase num by 1. Now, x = 5 and num = 5. 
  *     It can be proven that there is no achievable number larger than 6.
  *
@@ -25,9 +29,12 @@
  *       2- Decrease x by 1, and increase num by 1. Now, x = 5 and num = 5.
  *     It can be proven that there is no achievable number larger than 7.
  *
- * Constraints: 1 <= num, t <= 50
+ * Constraints:
+ *   • 1 <= num, t <= 50
  *
- * Hint: Let x be the answer, it’s always optimal to decrease x in each operation and increase nums.
+ * Hint:
+ *   Let x be the answer,
+ *   it’s always optimal to decrease x in each operation and increase nums.
  **
  * https://leetcode.com/problems/find-the-maximum-achievable-number/
 ***/
