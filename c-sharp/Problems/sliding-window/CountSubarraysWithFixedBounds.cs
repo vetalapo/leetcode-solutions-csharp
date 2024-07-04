93df21f8b71d419cceae7317830915ddd2a25fe7
Vitaly Dushkin <vetalapo@gmail.com>
2024-07-04 12:50:14 +0200

    #2444: description update

--- c-sharp/Problems/sliding-window/CountSubarraysWithFixedBounds.cs
+++ c-sharp/Problems/sliding-window/CountSubarraysWithFixedBounds.cs
@@ -12,18 +12,18 @@
  * A subarray is a contiguous part of an array.
  *
  * Example 1:
- * Input: nums = [1,3,5,2,7,5], minK = 1, maxK = 5
- * Output: 2
- * Explanation: The fixed-bound subarrays are [1,3,5] and [1,3,5,2].
+ *   Input: nums = [1,3,5,2,7,5], minK = 1, maxK = 5
+ *   Output: 2
+ *   Explanation: The fixed-bound subarrays are [1,3,5] and [1,3,5,2].
  *
  * Example 2:
- * Input: nums = [1,1,1,1], minK = 1, maxK = 1
- * Output: 10
- * Explanation: Every subarray of nums is a fixed-bound subarray. There are 10 possible subarrays.
+ *   Input: nums = [1,1,1,1], minK = 1, maxK = 1
+ *   Output: 10
+ *   Explanation: Every subarray of nums is a fixed-bound subarray. There are 10 possible subarrays.
  *
  * Constraints:
- *   • 2 <= nums.length <= 105
- *   • 1 <= nums[i], minK, maxK <= 106
+ *   • 2 <= nums.length <= 10^5
+ *   • 1 <= nums[i], minK, maxK <= 10^6
  *
  * Hint 1
  *   Can you solve the problem
