100ad7af60f24ee9a5d738ca984b9854bd217f21
Vitaly Dushkin <vetalapo@gmail.com>
2024-07-08 13:00:18 +0200

    #567: description update

--- c-sharp/Problems/sliding-window/PermutationInString.cs
+++ c-sharp/Problems/sliding-window/PermutationInString.cs
@@ -15,8 +15,29 @@
  *   Output: false
  *
  * Constraints:
- *   • 1 <= s1.length, s2.length <= 104
+ *   • 1 <= s1.length, s2.length <= 10^4
  *   • s1 and s2 consist of lowercase English letters.
+ * 
+ * Hint 1:
+ *   Obviously, brute force will result in TLE. Think of something else.
+ * 
+ * Hint 2:
+ *   How will you check whether one string is a permutation of another string?
+ *
+ * Hint 3:
+ *   One way is to sort the string and then compare.
+ *   But, Is there a better way?
+ *
+ * Hint 4:
+ *   If one string is a permutation of another string then they must one common metric.
+ *   What is that?
+ *
+ * Hint 5:
+ *   Both strings must have same character frequencies, if one is permutation of another.
+ *   Which data structure should be used to store frequencies?
+ *
+ * Hint 6:
+ *   What about hash table? An array of size 26?
  **
  * https://leetcode.com/problems/permutation-in-string/
 ***/
