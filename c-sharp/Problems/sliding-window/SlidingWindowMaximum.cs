2c7e35ad964985ff79b59846a72c81aec3608f12
Vitaly Dushkin <vetalapo@gmail.com>
2024-07-09 12:33:05 +0200

    #239: description and solution update

--- c-sharp/Problems/sliding-window/SlidingWindowMaximum.cs
+++ c-sharp/Problems/sliding-window/SlidingWindowMaximum.cs
@@ -3,14 +3,46 @@
  * Sliding Window Maximum
  ** 
  * You are given an array of integers nums,
- * there is a sliding window of size k which is moving from the very left of the array to the very right.
+ * there is a sliding window of size k
+ * which is moving from the very left of the array to the very right.
  * You can only see the k numbers in the window.
  * Each time the sliding window moves right by one position.
- * 
+ *
  * Return the max sliding window.
+ *
+ * Example 1:
+ *   Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+ *   Output: [3,3,5,5,6,7]
+ *   Explanation: 
+ *     Window position                Max
+ *     ---------------               -----
+ *     [1  3  -1] -3  5  3  6  7       3
+ *      1 [3  -1  -3] 5  3  6  7       3
+ *      1  3 [-1  -3  5] 3  6  7       5
+ *      1  3  -1 [-3  5  3] 6  7       5
+ *      1  3  -1  -3 [5  3  6] 7       6
+ *      1  3  -1  -3  5 [3  6  7]      7
  * 
+ * Example 2:
+ *   Input: nums = [1], k = 1
+ *   Output: [1]
+ *
+ * Constraints:
+ *   • 1 <= nums.length <= 10^5
+ *   • -10^4 <= nums[i] <= 10^4
+ *   • 1 <= k <= nums.length
+ *
+ * Hint 1:
+ *   How about using a data structure such as deque (double-ended queue)?
+ *
+ * Hint 2:
+ *   The queue size need not be the same as the window’s size.
+ *
+ * Hint 3:
+ *   Remove redundant elements and the queue should store only elements that need to be considered.
+ **
  * https://leetcode.com/problems/sliding-window-maximum/
- */
+***/
 
 using System.Collections.Generic;
 
@@ -20,8 +52,8 @@ public class SlidingWindowMaximum
 {
     public int[] MaxSlidingWindow( int[] nums, int k )
     {
-        List<int> result = new();
-        LinkedList<int> queue = new();
+        List<int> result = [];
+        LinkedList<int> queue = [];
 
         int left = 0;
         int right = 0;
@@ -51,6 +83,6 @@ public class SlidingWindowMaximum
             right++;
         }
 
-        return result.ToArray();
+        return [.. result];
     }
 }
