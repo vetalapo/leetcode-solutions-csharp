5cdba25b8494cf86495e1588d8e1e8511c35250d
Vitaly Dushkin <vetalapo@gmail.com>
2024-07-13 15:45:05 +0200

    #84: update description

--- c-sharp/Problems/stack/LargestRectangleInHistogram.cs
+++ c-sharp/Problems/stack/LargestRectangleInHistogram.cs
@@ -2,7 +2,10 @@
  * 84
  * Largest Rectangle in Histogram
  **
- * Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
+ * Given an array of integers heights
+ * representing the histogram's bar height
+ * where the width of each bar is 1,
+ * return the area of the largest rectangle in the histogram.
  *
  * Example 1:
  *   Input: heights = [2,1,5,6,2,3]
@@ -16,8 +19,8 @@
  *   Output: 4
  *
  * Constraints:
- *   • 1 <= heights.length <= 105
- *   • 0 <= heights[i] <= 104
+ *   • 1 <= heights.length <= 10^5
+ *   • 0 <= heights[i] <= 10^4
  **
  * https://leetcode.com/problems/largest-rectangle-in-histogram/
 ***/
