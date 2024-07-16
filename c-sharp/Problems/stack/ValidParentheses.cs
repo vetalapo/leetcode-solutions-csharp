12f23cd1a46c8596fcd9d43037b8b86bfa023f51
Vitaly Dushkin <vetalapo@gmail.com>
2024-07-16 14:24:24 +0200

    #20: description update

--- c-sharp/Problems/stack/ValidParentheses.cs
+++ c-sharp/Problems/stack/ValidParentheses.cs
@@ -23,12 +23,14 @@
  *   Output: false
  *
  * Constraints:
- *   • 1 <= s.length <= 104
+ *   • 1 <= s.length <= 10^4
  *   • s consists of parentheses only '()[]{}'.
  *
- * Hint 1: Use a stack of characters.
+ * Hint 1:
+ *   Use a stack of characters.
  *
- * Hint 2: When you encounter an opening bracket, push it to the top of the stack.
+ * Hint 2:
+ *   When you encounter an opening bracket, push it to the top of the stack.
  *
  * Hint 3:
  *   When you encounter a closing bracket,
