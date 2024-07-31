7cba28bf527180e164343b12feb44772ecf081d4
Vitaly Dushkin <vetalapo@gmail.com>
2024-07-31 15:22:05 +0200

    #791: description update

--- c-sharp/Problems/arrays-and-hashing/CustomSortString.cs
+++ c-sharp/Problems/arrays-and-hashing/CustomSortString.cs
@@ -16,7 +16,6 @@
  *   Output:  "cbad"
  *   Explanation:
  *     "a", "b", "c" appear in order, so the order of "a", "b", "c" should be "c", "b", and "a".
- *
  *     Since "d" does not appear in order,
  *     it can be at any position in the returned string.
  *     "dcba", "cdba", "cbda" are also valid outputs.
@@ -27,7 +26,6 @@
  *   Explanation:
  *     The characters "b", "c", and "a" from order dictate the order for the characters in s.
  *     The character "d" in s does not appear in order, so its position is flexible.
- *
  *     Following the order of appearance in order, "b", "c", and "a"
  *     from s should be arranged as "b", "c", "a". "d" can be placed at any position since it's not in order.
  *     The output "bcad" correctly follows this rule.
