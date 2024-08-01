2f528c9363baa901d7a6dc70a9ddbca4df30cde9
Vitaly Dushkin <vetalapo@gmail.com>
2024-08-01 18:33:35 +0200

    white space changes

--- c-sharp/Tests/arrays-and-hashing/BuildArrayFromPermutationTests.cs
+++ c-sharp/Tests/arrays-and-hashing/BuildArrayFromPermutationTests.cs
@@ -6,5 +6,6 @@ public class BuildArrayFromPermutationTests
 {
     [TestCase( new int[] { 0, 2, 1, 5, 3, 4 }, ExpectedResult = new int[] { 0, 1, 2, 4, 5, 3 } )]
     [TestCase( new int[] { 5, 0, 1, 2, 3, 4 }, ExpectedResult = new int[] { 4, 5, 0, 1, 2, 3 } )]
-    public int[] BuildArrayTest( int[] nums ) => new BuildArrayFromPermutation().BuildArray( nums );
+    public int[] BuildArrayTest( int[] nums ) =>
+        new BuildArrayFromPermutation().BuildArray( nums );
 }
