2a2d68ba26c5d7fbefda09a5165f0c6c9e64294f
Vitaly Dushkin <vetalapo@gmail.com>
2024-08-03 22:12:19 +0200

    #525: tests formatting

--- c-sharp/Tests/arrays-and-hashing/ContiguousArrayTests.cs
+++ c-sharp/Tests/arrays-and-hashing/ContiguousArrayTests.cs
@@ -6,5 +6,6 @@ public class ContiguousArrayTests
 {
     [TestCase( new int[] { 0, 1 }, ExpectedResult = 2 )]
     [TestCase( new int[] { 0, 1, 0 }, ExpectedResult = 2 )]
-    public int FindMaxLengthTest( int[] nums ) => new ContiguousArray().FindMaxLength( nums );
+    public int FindMaxLengthTest( int[] nums ) =>
+        new ContiguousArray().FindMaxLength( nums );
 }
