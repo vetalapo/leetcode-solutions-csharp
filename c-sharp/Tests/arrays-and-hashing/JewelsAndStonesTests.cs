661dedc8e11c2ac63fac6b8d01473034762fb84f
Vitaly Dushkin <vetalapo@gmail.com>
2024-08-11 14:22:26 +0200

    #771: improved tests formatting

--- c-sharp/Tests/arrays-and-hashing/JewelsAndStonesTests.cs
+++ c-sharp/Tests/arrays-and-hashing/JewelsAndStonesTests.cs
@@ -6,5 +6,6 @@ public class JewelsAndStonesTests
 {
     [TestCase( "aA", "aAAbbbb", ExpectedResult = 3 )]
     [TestCase( "z", "ZZ", ExpectedResult = 0 )]
-    public int NumJewelsInStonesTest( string jewels, string stones ) => new JewelsAndStones().NumJewelsInStones( jewels, stones );
+    public int NumJewelsInStonesTest( string jewels, string stones ) =>
+        new JewelsAndStones().NumJewelsInStones( jewels, stones );
 }
