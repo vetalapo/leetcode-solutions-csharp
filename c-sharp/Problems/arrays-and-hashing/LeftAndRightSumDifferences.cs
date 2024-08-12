7e5a3d00d8fa69a6836fb2b7ca0f3f80753db77d
Vitaly Dushkin <vetalapo@gmail.com>
2024-08-12 12:46:02 +0200

    #2574: improved description and tests formatting

--- c-sharp/Problems/arrays-and-hashing/LeftAndRightSumDifferences.cs
+++ c-sharp/Problems/arrays-and-hashing/LeftAndRightSumDifferences.cs
@@ -17,20 +17,23 @@
  * Example 1:
  *   Input: nums = [10,4,8,3]
  *   Output: [15,1,11,22]
- *   Explanation: The array leftSum is [0,10,14,22] and the array rightSum is [15,11,3,0].
- *                The array answer is [|0 - 15|,|10 - 11|,|14 - 3|,|22 - 0|] = [15,1,11,22].
+ *   Explanation:
+ *     The array leftSum is [0,10,14,22] and the array rightSum is [15,11,3,0].
+ *     The array answer is [|0 - 15|,|10 - 11|,|14 - 3|,|22 - 0|] = [15,1,11,22].
  *
  * Example 2:
  *   Input: nums = [1]
  *   Output: [0]
- *   Explanation: The array leftSum is [0] and the array rightSum is [0].
- *                The array answer is [|0 - 0|] = [0].
+ *   Explanation:
+ *     The array leftSum is [0] and the array rightSum is [0].
+ *     The array answer is [|0 - 0|] = [0].
  *
  * Constraints:
  *   • 1 <= nums.length <= 1000
  *   • 1 <= nums[i] <= 10^5
  *
- * Hint 1: For each index i, maintain two variables leftSum and rightSum.
+ * Hint 1:
+ *   For each index i, maintain two variables leftSum and rightSum.
  *
  * Hint 2:
  *   Iterate on the range j: [0 … i - 1] and
