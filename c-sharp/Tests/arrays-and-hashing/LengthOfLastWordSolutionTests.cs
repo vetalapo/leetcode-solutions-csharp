8e83151b1448ad57e80b2e49a871da66daaec30c
Vitaly Dushkin <vetalapo@gmail.com>
2024-08-13 11:23:17 +0200

    #58: improved tests formatting

--- c-sharp/Tests/arrays-and-hashing/LengthOfLastWordSolutionTests.cs
+++ c-sharp/Tests/arrays-and-hashing/LengthOfLastWordSolutionTests.cs
@@ -7,6 +7,6 @@ public class LengthOfLastWordSolutionTests
     [TestCase( "Hello World", ExpectedResult = 5 )]
     [TestCase( "   fly me   to   the moon  ", ExpectedResult = 4 )]
     [TestCase( "luffy is still joyboy", ExpectedResult = 6 )]
-    public int LengthOfLastWordTest( string s )
-        => new LengthOfLastWordSolution().LengthOfLastWord( s );
+    public int LengthOfLastWordTest( string s ) =>
+        new LengthOfLastWordSolution().LengthOfLastWord( s );
 }
