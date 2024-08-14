b650073000609af5a8dc89e6da3cc848640c465f
Vitaly Dushkin <vetalapo@gmail.com>
2024-08-14 12:37:57 +0200

    #128: improved formatting for tests

--- c-sharp/Tests/arrays-and-hashing/LongestConsecutiveSequenceTests.cs
+++ c-sharp/Tests/arrays-and-hashing/LongestConsecutiveSequenceTests.cs
@@ -6,5 +6,6 @@ public class LongestConsecutiveSequenceTests
 {
     [TestCase(new object[] { new int[] { 100, 4, 200, 1, 3, 2 } }, ExpectedResult = 4 )]
     [TestCase(new object[] { new int[] { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 } }, ExpectedResult = 9 )]
-    public int LongestConsecutiveTest( int[] nums ) => new LongestConsecutiveSequence().LongestConsecutive( nums );
+    public int LongestConsecutiveTest( int[] nums ) =>
+        new LongestConsecutiveSequence().LongestConsecutive( nums );
 }
