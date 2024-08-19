260fe3a7466ad0fee96d6ec1768377628f529079
Vitaly Dushkin <vetalapo@gmail.com>
2024-08-19 14:23:26 +0200

    #1700: improved tests formatting

--- c-sharp/Tests/arrays-and-hashing/NumberOfStudentsUnableToEatLunchTests.cs
+++ c-sharp/Tests/arrays-and-hashing/NumberOfStudentsUnableToEatLunchTests.cs
@@ -6,6 +6,6 @@ public class NumberOfStudentsUnableToEatLunchTests
 {
     [TestCase( new int[] { 1, 1, 0, 0 }, new int[] { 0, 1, 0, 1 }, ExpectedResult = 0 )]
     [TestCase( new int[] { 1, 1, 1, 0, 0, 1 }, new int[] { 1, 0, 0, 0, 1, 1 }, ExpectedResult = 3 )]
-    public int CountStudentsTest( int[] students, int[] sandwiches )
-        => new NumberOfStudentsUnableToEatLunch().CountStudents( students, sandwiches );
+    public int CountStudentsTest( int[] students, int[] sandwiches ) =>
+        new NumberOfStudentsUnableToEatLunch().CountStudents( students, sandwiches );
 }
