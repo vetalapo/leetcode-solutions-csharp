0f82262b8bf0570c9b3ccc227b06d2d4e46d03b0
Vitaly Dushkin <vetalapo@gmail.com>
2024-08-20 13:39:43 +0200

    #66: improved tests formatting

--- c-sharp/Tests/arrays-and-hashing/PlusOneSolutionTests.cs
+++ c-sharp/Tests/arrays-and-hashing/PlusOneSolutionTests.cs
@@ -8,5 +8,6 @@ public class PlusOneSolutionTests
     [TestCase( new int[] { 4, 3, 2, 1 }, ExpectedResult = new int[] { 4, 3, 2, 2 } )]
     [TestCase( new int[] { 9 }, ExpectedResult = new int[] { 1, 0 } )]
     [TestCase( new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }, ExpectedResult = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 1 } )]
-    public int[] PlusOneTest( int[] digits ) => new PlusOneSolution().PlusOne( digits );
+    public int[] PlusOneTest( int[] digits ) =>
+        new PlusOneSolution().PlusOne( digits );
 }
