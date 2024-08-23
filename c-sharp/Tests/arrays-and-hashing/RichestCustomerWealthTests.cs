43ad6807b50f7d09108d6a64bf082982954fcab4
Vitaly Dushkin <vetalapo@gmail.com>
2024-08-23 13:59:59 +0200

    #1672: improve tests formatting

--- c-sharp/Tests/arrays-and-hashing/RichestCustomerWealthTests.cs
+++ c-sharp/Tests/arrays-and-hashing/RichestCustomerWealthTests.cs
@@ -7,15 +7,27 @@ using Problems;
 public class RichestCustomerWealthTests
 {
     [TestCaseSource( nameof( TestCases ) )]
-    public int MaximumWealthTest( int[][] accounts ) => new RichestCustomerWealth().MaximumWealth( accounts );
+    public int MaximumWealthTest( int[][] accounts ) =>
+        new RichestCustomerWealth().MaximumWealth( accounts );
 
     private static IEnumerable TestCases
     {
         get
         {
-            yield return new TestCaseData( new object[] { new int[][] { new int[] { 1, 2, 3 }, new int[] { 3, 2, 1 } } } ).Returns( 6 );
-            yield return new TestCaseData( new object[] { new int[][] { new int[] { 1, 5 }, new int[] { 7, 3 }, new int[] { 3, 5 } } } ).Returns( 10 );
-            yield return new TestCaseData( new object[] { new int[][] { new int[] { 2, 8, 7 }, new int[] { 7, 1, 3 }, new int[] { 1, 9, 5 } } } ).Returns( 17 );
+            yield return new TestCaseData(
+                [
+                    new int[][] { new int[] { 1, 2, 3 }, new int[] { 3, 2, 1 } }
+                ] ).Returns( 6 );
+
+            yield return new TestCaseData(
+                [
+                    new int[][] { new int[] { 1, 5 }, new int[] { 7, 3 }, new int[] { 3, 5 } }
+                ] ).Returns( 10 );
+
+            yield return new TestCaseData(
+                [
+                    new int[][] { new int[] { 2, 8, 7 }, new int[] { 7, 1, 3 }, new int[] { 1, 9, 5 } }
+                ] ).Returns( 17 );
         }
     }
 }
