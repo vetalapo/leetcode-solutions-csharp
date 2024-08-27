3a05872a170a4673a5893faca3908ec9eebbd2ed
Vitaly Dushkin <vetalapo@gmail.com>
2024-08-27 19:39:03 +0200

    #2073: improved tests formatting

--- c-sharp/Tests/arrays-and-hashing/TimeNeededToBuyTicketsTests.cs
+++ c-sharp/Tests/arrays-and-hashing/TimeNeededToBuyTicketsTests.cs
@@ -6,6 +6,6 @@ public class TimeNeededToBuyTicketsTests
 {
     [TestCase( new int[] { 2, 3, 2 }, 2, ExpectedResult = 6 )]
     [TestCase( new int[] { 5, 1, 1, 1 }, 0, ExpectedResult = 8 )]
-    public int TimeRequiredToBuyTest( int[] tickets, int k )
-        => new TimeNeededToBuyTickets().TimeRequiredToBuy( tickets, k );
+    public int TimeRequiredToBuyTest( int[] tickets, int k ) =>
+        new TimeNeededToBuyTickets().TimeRequiredToBuy( tickets, k );
 }
