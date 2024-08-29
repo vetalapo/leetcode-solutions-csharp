ecac267b3e200fd964c4d7d83729b0bd8eb37588
Vitaly Dushkin <vetalapo@gmail.com>
2024-08-29 13:10:18 +0200

    #1816: improved tests formatting

--- c-sharp/Tests/arrays-and-hashing/TruncateSentenceSolutionTests.cs
+++ c-sharp/Tests/arrays-and-hashing/TruncateSentenceSolutionTests.cs
@@ -7,5 +7,6 @@ public class TruncateSentenceSolutionTests
     [TestCase( "Hello how are you Contestant", 4, ExpectedResult = "Hello how are you" )]
     [TestCase( "What is the solution to this problem", 4, ExpectedResult = "What is the solution" )]
     [TestCase( "chopper is not a tanuki", 5, ExpectedResult = "chopper is not a tanuki" )]
-    public string TruncateSentenceTest( string s, int k ) => new TruncateSentenceSolution().TruncateSentence( s, k );
+    public string TruncateSentenceTest( string s, int k ) =>
+        new TruncateSentenceSolution().TruncateSentence( s, k );
 }
