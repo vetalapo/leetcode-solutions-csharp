4307513948a6880c708883e66a464ae5fdb215ec
Vitaly Dushkin <vetalapo@gmail.com>
2024-08-30 15:47:31 +0200

    #1: improved tests formatting

--- c-sharp/Tests/arrays-and-hashing/TwoSumSolutionTests.cs
+++ c-sharp/Tests/arrays-and-hashing/TwoSumSolutionTests.cs
@@ -8,5 +8,6 @@ public class TwoSumSolutionTests
     [TestCase( new int[] { 3, 2, 4 }, 6, ExpectedResult = new int[] { 1, 2 } )]
     [TestCase( new int[] { 3, 3 }, 6, ExpectedResult = new int[] { 0, 1 } )]
     [TestCase( new int[] { 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1 }, 11, ExpectedResult = new int[] { 5, 11 } )]
-    public int[] TwoSumTest( int[] nums, int target ) => new TwoSumSolution().TwoSum( nums, target );
+    public int[] TwoSumTest( int[] nums, int target ) =>
+        new TwoSumSolution().TwoSum( nums, target );
 }
