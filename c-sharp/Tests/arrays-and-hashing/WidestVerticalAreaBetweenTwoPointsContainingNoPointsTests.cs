452c3f7637894078222c8647964166c74d97bf81
Vitaly Dushkin <vetalapo@gmail.com>
2024-09-03 13:59:30 +0200

    1637: improved tests formatting

--- c-sharp/Tests/arrays-and-hashing/WidestVerticalAreaBetweenTwoPointsContainingNoPointsTests.cs
+++ c-sharp/Tests/arrays-and-hashing/WidestVerticalAreaBetweenTwoPointsContainingNoPointsTests.cs
@@ -23,10 +23,10 @@ public class WidestVerticalAreaBetweenTwoPointsContainingNoPointsTests
     {
         get
         {
-            yield return new TestCaseData( new object[] { new int[][] { [8, 7], [9, 9], [7, 4], [9, 7] } } )
+            yield return new TestCaseData( [new int[][] { [8, 7], [9, 9], [7, 4], [9, 7] }] )
                 .Returns( 1 );
 
-            yield return new TestCaseData( new object[] { new int[][] { [3, 1], [9, 0], [1, 0], [1, 4], [5, 3], [8, 8] } } )
+            yield return new TestCaseData( [new int[][] { [3, 1], [9, 0], [1, 0], [1, 4], [5, 3], [8, 8] }] )
                 .Returns( 3 );
         }
     }
