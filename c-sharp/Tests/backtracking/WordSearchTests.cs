2f25edc2c66c57f393963ef39288a44082874f4c
Vitaly Dushkin <vetalapo@gmail.com>
2024-09-04 11:00:24 +0200

    #79: improved tests formatting

--- c-sharp/Tests/backtracking/WordSearchTests.cs
+++ c-sharp/Tests/backtracking/WordSearchTests.cs
@@ -7,21 +7,42 @@ using Problems;
 public class WordSearchTests
 {
     [TestCaseSource( nameof( TestCases ) )]
-    public bool ExistTest( char[][] board, string word )
-        => new WordSearch().Exist( board, word );
+    public bool ExistTest( char[][] board, string word ) =>
+        new WordSearch().Exist( board, word );
 
     private static IEnumerable TestCases
     {
         get
         {
-            yield return new TestCaseData( new char[][] { ['A', 'B', 'C', 'E'], ['S', 'F', 'C', 'S'], ['A', 'D', 'E', 'E'] }, "ABCCED" )
-                .Returns( true );
+            yield return new TestCaseData(
+                new char[][]
+                {
+                    ['A', 'B', 'C', 'E'],
+                    ['S', 'F', 'C', 'S'],
+                    ['A', 'D', 'E', 'E']
+                },
+                "ABCCED"
+            ).Returns( true );
 
-            yield return new TestCaseData( new char[][] { ['A', 'B', 'C', 'E'], ['S', 'F', 'C', 'S'], ['A', 'D', 'E', 'E'] }, "SEE" )
-                .Returns( true );
+            yield return new TestCaseData(
+                new char[][]
+                {
+                    ['A', 'B', 'C', 'E'],
+                    ['S', 'F', 'C', 'S'],
+                    ['A', 'D', 'E', 'E']
+                },
+                "SEE"
+            ).Returns( true );
 
-            yield return new TestCaseData( new char[][] { ['A', 'B', 'C', 'E'], ['S', 'F', 'C', 'S'], ['A', 'D', 'E', 'E'] }, "ABCB" )
-                .Returns( false );
+            yield return new TestCaseData(
+                new char[][]
+                {
+                    ['A', 'B', 'C', 'E'],
+                    ['S', 'F', 'C', 'S'],
+                    ['A', 'D', 'E', 'E']
+                },
+                "ABCB"
+            ).Returns( false );
         }
     }
 }
