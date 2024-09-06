95e929ea5dd57869526efe917d8c634b0f66ccc7
Vitaly Dushkin <vetalapo@gmail.com>
2024-09-06 09:50:53 +0200

    #153: improved tests formatting

--- c-sharp/Tests/binary-search/FindMinimumInRotatedSortedArrayTests.cs
+++ c-sharp/Tests/binary-search/FindMinimumInRotatedSortedArrayTests.cs
@@ -8,5 +8,6 @@ public class FindMinimumInRotatedSortedArrayTests
     [TestCase( new int[] { 4, 5, 6, 7, 0, 1, 2 }, ExpectedResult = 0 )]
     [TestCase( new int[] { 11, 13, 15, 17 }, ExpectedResult = 11 )]
     [TestCase( new int[] { 3, 1, 2 }, ExpectedResult = 1 )]
-    public int FindMinTest( int[] nums ) => new FindMinimumInRotatedSortedArray().FindMin( nums );
+    public int FindMinTest( int[] nums ) =>
+        new FindMinimumInRotatedSortedArray().FindMin( nums );
 }
