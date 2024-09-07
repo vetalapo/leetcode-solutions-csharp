cbdb7df56c028fb0568c7a389286e3c9aa817414
Vitaly Dushkin <vetalapo@gmail.com>
2024-09-07 11:06:11 +0200

    #875: improve tests formatting

--- c-sharp/Tests/binary-search/KokoEatingBananasTests.cs
+++ c-sharp/Tests/binary-search/KokoEatingBananasTests.cs
@@ -8,5 +8,6 @@ public class KokoEatingBananasTests
     [TestCase( new int[] { 30, 11, 23, 4, 20 }, 5, ExpectedResult = 30 )]
     [TestCase( new int[] { 30, 11, 23, 4, 20 }, 6, ExpectedResult = 23 )]
     [TestCase( new int[] { 805306368, 805306368, 805306368 }, 1000000000, ExpectedResult = 3 )]
-    public int MinEatingSpeedTest( int[] piles, int h ) => new KokoEatingBananas().MinEatingSpeed( piles, h );
+    public int MinEatingSpeedTest( int[] piles, int h ) =>
+        new KokoEatingBananas().MinEatingSpeed( piles, h );
 }
