5e76bb7dabf00d8446fa47c3a72a4240ac6f8685
Vitaly Dushkin <vetalapo@gmail.com>
2024-09-13 13:10:39 +0200

    #948: improve tests formatting

--- c-sharp/Tests/greedy/BagOfTokensTests.cs
+++ c-sharp/Tests/greedy/BagOfTokensTests.cs
@@ -7,6 +7,6 @@ public class BagOfTokensTests
     [TestCase( new int[] { 100 }, 50, ExpectedResult = 0 )]
     [TestCase( new int[] { 200, 100 }, 150, ExpectedResult = 1 )]
     [TestCase( new int[] { 100, 200, 300, 400 }, 200, ExpectedResult = 2 )]
-    public int BagOfTokensScoreTest( int[] tokens, int power )
-        => new BagOfTokens().BagOfTokensScore( tokens, power );
+    public int BagOfTokensScoreTest( int[] tokens, int power ) =>
+        new BagOfTokens().BagOfTokensScore( tokens, power );
 }
