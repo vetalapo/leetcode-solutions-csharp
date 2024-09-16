a92daffcafe5d4b61d3256b429ecd5a4354789e5
Vitaly Dushkin <vetalapo@gmail.com>
2024-09-16 15:40:58 +0200

    #138: improve tests formatting

--- c-sharp/Tests/linked-list/CopyListWithRandomPointerTests.cs
+++ c-sharp/Tests/linked-list/CopyListWithRandomPointerTests.cs
@@ -7,26 +7,27 @@ namespace Problems.Tests;
 public class CopyListWithRandomPointerTests
 {
     [TestCaseSource( nameof( TestCases ) )]
-    public Node CopyRandomListTest( Node node ) => new CopyListWithRandomPointer().CopyRandomList( node );
+    public Node CopyRandomListTest( Node node ) =>
+        new CopyListWithRandomPointer().CopyRandomList( node );
 
     private static IEnumerable TestCases
     {
         get
         {
-            yield return new TestCaseData( Node.Create( new int?[][]
-            {
-                new int?[] { 7, null }, new int?[] { 13, 0 }, new int?[] { 11, 4 }, new int?[] { 10, 2 }, new int?[] { 1,0 }
-            } ) )
-            .Returns( Node.Create( new int?[][]
-            {
-                new int?[] { 7, null }, new int?[] { 13, 0 }, new int?[] { 11, 4 }, new int?[] { 10, 2 }, new int?[] { 1, 0 }
-            } ) );
+            yield return new TestCaseData(
+                Node
+                    .Create( [[7, null], [13, 0], [11, 4], [10, 2], [1, 0]] ) )
+                    .Returns( Node.Create( [[7, null], [13, 0], [11, 4], [10, 2], [1, 0]] ) );
 
-            yield return new TestCaseData( Node.Create( new int?[][] { new int?[] { 1, 1 }, new int?[] { 2, 1 } } ) )
-                .Returns( Node.Create( new int?[][] { new int?[] { 1, 1 }, new int?[] { 2, 1 } } ) );
+            yield return new TestCaseData(
+                Node
+                    .Create( [[1, 1], [2, 1]] ) )
+                    .Returns( Node.Create( [[1, 1], [2, 1]] ) );
 
-            yield return new TestCaseData( Node.Create( new int?[][] { new int?[] { 3, null }, new int?[] { 3, 0 }, new int?[] { 3, null } } ) )
-                .Returns( Node.Create( new int?[][] { new int?[] { 3, null }, new int?[] { 3, 0 }, new int?[] { 3, null } } ) );
+            yield return new TestCaseData(
+                Node
+                    .Create( [[3, null], [3, 0], [3, null]] ) )
+                    .Returns( Node.Create( [[3, null], [3, 0], [3, null]] ) );
         }
     }
 }
