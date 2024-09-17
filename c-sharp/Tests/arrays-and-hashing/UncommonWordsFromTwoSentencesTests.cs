e3fe867c0da82519c7cdf412ff67c21b63c14ff6
Vitaly Dushkin <vetalapo@gmail.com>
2024-09-17 13:10:04 +0200

    #884: add solution and tests

---
+++ c-sharp/Tests/arrays-and-hashing/UncommonWordsFromTwoSentencesTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Problems.Tests;
+
+public class UncommonWordsFromTwoSentencesTests
+{
+    [TestCase( "this apple is sweet", "this apple is sour", ExpectedResult = new string[] { "sweet", "sour" } )]
+    [TestCase( "apple apple", "banana", ExpectedResult = new string[] { "banana" } )]
+    [TestCase( "this apple is sweet sweet sweet", "this apple is sour", ExpectedResult = new string[] { "sour" } )]
+    [TestCase( "this apple is sweet sweet sweet and good", "this apple is sour", ExpectedResult = new string[] { "and", "good", "sour" } )]
+    [TestCase( "s z z z s", "s z ejt", ExpectedResult = new string[] { "ejt" } )]
+    [TestCase( "", "", ExpectedResult = new string[] { } )]
+    public string[] UncommonFromSentencesTest( string s1, string s2 ) =>
+        new UncommonWordsFromTwoSentences().UncommonFromSentences( s1, s2 );
+}
