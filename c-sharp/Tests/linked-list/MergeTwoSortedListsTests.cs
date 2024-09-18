a468241fa4c3686dfc199f799194c4d4396a02e8
Vitaly Dushkin <vetalapo@gmail.com>
2024-09-18 16:23:05 +0200

    #21: improve tests formatting

--- c-sharp/Tests/linked-list/MergeTwoSortedListsTests.cs
+++ c-sharp/Tests/linked-list/MergeTwoSortedListsTests.cs
@@ -8,37 +8,34 @@ using Problems;
 public class MergeTwoSortedListsTests
 {
     [TestCaseSource( nameof( TestCases ) )]
-    public ListNode MergeTwoListsTest( ListNode list1, ListNode list2 ) => new MergeTwoSortedLists().MergeTwoLists( list1, list2 );
+    public ListNode MergeTwoListsTest( ListNode list1, ListNode list2 ) =>
+        new MergeTwoSortedLists().MergeTwoLists( list1, list2 );
 
     [TestCaseSource( nameof( TestCases ) )]
-    public ListNode MergeTwoListsNaiveTest( ListNode list1, ListNode list2 ) => new MergeTwoSortedLists().MergeTwoListsNaive( list1, list2 );
+    public ListNode MergeTwoListsNaiveTest( ListNode list1, ListNode list2 ) =>
+        new MergeTwoSortedLists().MergeTwoListsNaive( list1, list2 );
 
     private static IEnumerable TestCases
     {
         get
         {
-            yield return new TestCaseData(
-                    ListNode.Create( new int[] { 1, 2, 4 } ), ListNode.Create( new int[] { 1, 3, 4 } )
-                )
-                .Returns( ListNode.Create( new int[] { 1, 1, 2, 3, 4, 4 } ) );
+            yield return new TestCaseData( ListNode.Create( [1, 2, 4] ), ListNode.Create( [1, 3, 4] ) )
+                .Returns( ListNode.Create( [1, 1, 2, 3, 4, 4] ) );
 
-            yield return new TestCaseData(
-                    ListNode.Create( new int[] { 2, 4, 6, 8 } ), ListNode.Create( new int[] { 1, 3, 5, 7 } )
-                )
-                .Returns( ListNode.Create( new int[] { 1, 2, 3, 4, 5, 6, 7, 8 } ) );
+            yield return new TestCaseData( ListNode.Create( [2, 4, 6, 8] ), ListNode.Create( [1, 3, 5, 7] ) )
+                .Returns( ListNode.Create( [1, 2, 3, 4, 5, 6, 7, 8] ) );
 
-            yield return new TestCaseData( null, null ).Returns( null );
+            yield return new TestCaseData( null, null )
+                .Returns( null );
 
-            yield return new TestCaseData( null, ListNode.Create( new int[] { 0 } ) )
-                .Returns( ListNode.Create( new int[] { 0 } ) );
+            yield return new TestCaseData( null, ListNode.Create( [0] ) )
+                .Returns( ListNode.Create( [0] ) );
 
-            yield return new TestCaseData( ListNode.Create( new int[] { 5, 6 } ), null )
-                .Returns( ListNode.Create( new int[] { 5, 6 } ) );
+            yield return new TestCaseData( ListNode.Create( [5, 6] ), null )
+                .Returns( ListNode.Create( [5, 6] ) );
 
-            yield return new TestCaseData(
-                    ListNode.Create( new int[] { 5 } ), ListNode.Create( new int[] { 1, 2, 4 } )
-                )
-                .Returns( ListNode.Create( new int[] { 1, 2, 4, 5 } ) );
+            yield return new TestCaseData( ListNode.Create( [5] ), ListNode.Create( [1, 2, 4] ) )
+                .Returns( ListNode.Create( [1, 2, 4, 5] ) );
         }
     }
 }
