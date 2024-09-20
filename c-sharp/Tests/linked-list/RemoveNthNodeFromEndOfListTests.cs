b36b443ead39b280c07c98449bad8c6ecfd7a5ee
Vitaly Dushkin <vetalapo@gmail.com>
2024-09-20 12:10:38 +0200

    #19: improved tests formatting

--- c-sharp/Tests/linked-list/RemoveNthNodeFromEndOfListTests.cs
+++ c-sharp/Tests/linked-list/RemoveNthNodeFromEndOfListTests.cs
@@ -7,26 +7,28 @@ using Problems;
 public class RemoveNthNodeFromEndOfListTests
 {
     [TestCaseSource( nameof( TestCases ) )]
-    public ListNode RemoveNthFromEndTest( ListNode head, int n ) => new RemoveNthNodeFromEndOfList().RemoveNthFromEnd( head, n );
+    public ListNode RemoveNthFromEndTest( ListNode head, int n ) =>
+        new RemoveNthNodeFromEndOfList().RemoveNthFromEnd( head, n );
 
     [TestCaseSource( nameof( TestCases ) )]
-    public ListNode RemoveNthFromEndProperTest( ListNode head, int n ) => new RemoveNthNodeFromEndOfList().RemoveNthFromEndProper( head, n );
+    public ListNode RemoveNthFromEndProperTest( ListNode head, int n ) =>
+        new RemoveNthNodeFromEndOfList().RemoveNthFromEndProper( head, n );
 
     private static IEnumerable TestCases
     {
         get
         {
-            yield return new TestCaseData( ListNode.Create( new int[] { 1, 2, 3, 4, 5 } ), 2 )
-                .Returns( ListNode.Create( new int[] { 1, 2, 3, 5 } ) );
+            yield return new TestCaseData( ListNode.Create( [1, 2, 3, 4, 5] ), 2 )
+                .Returns( ListNode.Create( [1, 2, 3, 5] ) );
 
-            yield return new TestCaseData( ListNode.Create( new int[] { 1 } ), 1 )
-                .Returns( ListNode.Create( new int[] { } ) );
+            yield return new TestCaseData( ListNode.Create( [1] ), 1 )
+                .Returns( ListNode.Create( [] ) );
 
-            yield return new TestCaseData( ListNode.Create( new int[] { 1, 2 } ), 1 )
-                .Returns( ListNode.Create( new int[] { 1 } ) );
+            yield return new TestCaseData( ListNode.Create( [1, 2] ), 1 )
+                .Returns( ListNode.Create( [1] ) );
 
-            yield return new TestCaseData( ListNode.Create( new int[] { 1, 2 } ), 2 )
-                .Returns( ListNode.Create( new int[] { 2 } ) );
+            yield return new TestCaseData( ListNode.Create( [1, 2] ), 2 )
+                .Returns( ListNode.Create( [2] ) );
         }
     }
 }
