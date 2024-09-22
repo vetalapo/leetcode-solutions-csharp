47801d3cd3ab68a2feed83c5a6babeae5f467404
Vitaly Dushkin <vetalapo@gmail.com>
2024-09-22 21:19:46 +0200

    #206: improved tests formatting

--- c-sharp/Tests/linked-list/ReverseLinkedListTests.cs
+++ c-sharp/Tests/linked-list/ReverseLinkedListTests.cs
@@ -7,23 +7,26 @@ using Problems;
 public class ReverseLinkedListTests
 {
     [TestCaseSource( nameof( TestCases ) )]
-    public ListNode ReverseListIterativelyTest( ListNode head ) => new ReverseLinkedList().ReverseListIteratively( head );
+    public ListNode ReverseListIterativelyTest( ListNode head ) =>
+        new ReverseLinkedList().ReverseListIteratively( head );
 
     [TestCaseSource( nameof( TestCases ) )]
-    public ListNode ReverseListTest( ListNode head ) => new ReverseLinkedList().ReverseList( head );
+    public ListNode ReverseListTest( ListNode head ) =>
+        new ReverseLinkedList().ReverseList( head );
 
     [TestCaseSource( nameof( TestCases ) )]
-    public ListNode ReverseListRightWayTest( ListNode head ) => new ReverseLinkedList().ReverseListRightWay( head );
+    public ListNode ReverseListRightWayTest( ListNode head ) =>
+        new ReverseLinkedList().ReverseListRightWay( head );
 
     private static IEnumerable TestCases
     {
         get
         {
-            yield return new TestCaseData( ListNode.Create( new int[] { 1, 2, 3, 4, 5 } ) )
-                .Returns( ListNode.Create( new int[] { 5, 4, 3, 2, 1 } ) );
+            yield return new TestCaseData( ListNode.Create( [1, 2, 3, 4, 5] ) )
+                .Returns( ListNode.Create( [5, 4, 3, 2, 1] ) );
 
-            yield return new TestCaseData( ListNode.Create( new int[] { 1, 2 } ) )
-                .Returns( ListNode.Create( new int[] { 2, 1 } ) );
+            yield return new TestCaseData( ListNode.Create( [1, 2] ) )
+                .Returns( ListNode.Create( [2, 1] ) );
 
             yield return new TestCaseData( null ).Returns( null );
         }
