8f643e01931f79d2b9f497d18808e9c7c0497671
Vitaly Dushkin <vetalapo@gmail.com>
2024-09-26 11:17:43 +0200

    #2769: improve tests formatting

--- c-sharp/Tests/math/FindTheMaximumAchievableNumberTests.cs
+++ c-sharp/Tests/math/FindTheMaximumAchievableNumberTests.cs
@@ -6,5 +6,6 @@ public class FindTheMaximumAchievableNumberTests
 {
     [TestCase( 4, 1, ExpectedResult = 6 )]
     [TestCase( 3, 2, ExpectedResult = 7 )]
-    public int TheMaximumAchievableXTest( int num, int t ) => new FindTheMaximumAchievableNumber().TheMaximumAchievableX( num, t );
+    public int TheMaximumAchievableXTest( int num, int t ) =>
+        new FindTheMaximumAchievableNumber().TheMaximumAchievableX( num, t );
 }
