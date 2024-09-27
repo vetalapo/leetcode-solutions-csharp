7ef6e02300a4cbf0ff988433bfb4033a3afff03d
Vitaly Dushkin <vetalapo@gmail.com>
2024-09-27 13:36:05 +0200

    #2485: improve tests formatting

--- c-sharp/Tests/math/FindThePivotIntegerTests.cs
+++ c-sharp/Tests/math/FindThePivotIntegerTests.cs
@@ -7,5 +7,6 @@ public class FindThePivotIntegerTests
     [TestCase( 8, ExpectedResult = 6 )]
     [TestCase( 1, ExpectedResult = 1 )]
     [TestCase( 4, ExpectedResult = -1 )]
-    public int PivotIntegerTest( int n ) => new FindThePivotInteger().PivotInteger( n );
+    public int PivotIntegerTest( int n ) =>
+        new FindThePivotInteger().PivotInteger( n );
 }
