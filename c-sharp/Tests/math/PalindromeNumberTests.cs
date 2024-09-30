746b27800f358b17f3e1c44b7fac07868c446c16
Vitaly Dushkin <vetalapo@gmail.com>
2024-09-30 12:55:31 +0200

    #9: improve tests formatting

--- c-sharp/Tests/math/PalindromeNumberTests.cs
+++ c-sharp/Tests/math/PalindromeNumberTests.cs
@@ -7,5 +7,6 @@ public class PalindromeNumberTests
     [TestCase( 121, ExpectedResult = true )]
     [TestCase( -121, ExpectedResult = false )]
     [TestCase( 10, ExpectedResult = false )]
-    public bool IsPalindromeTest( int x ) => new PalindromeNumber().IsPalindrome( x );
+    public bool IsPalindromeTest( int x ) =>
+        new PalindromeNumber().IsPalindrome( x );
 }
