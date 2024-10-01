4a2b360d8243667220cbd3d9b14da39f5bb905e2
Vitaly Dushkin <vetalapo@gmail.com>
2024-10-01 12:36:04 +0200

    #231: improve tests formatting

--- c-sharp/Tests/math/PowerOfTwoTests.cs
+++ c-sharp/Tests/math/PowerOfTwoTests.cs
@@ -8,5 +8,6 @@ public class PowerOfTwoTests
     [TestCase( 3, ExpectedResult = false )]
     [TestCase( 8, ExpectedResult = true )]
     [TestCase( 16, ExpectedResult = true )]
-    public bool IsPowerOfTwoTest( int n ) => new PowerOfTwo().IsPowerOfTwo( n );
+    public bool IsPowerOfTwoTest( int n ) =>
+        new PowerOfTwo().IsPowerOfTwo( n );
 }
