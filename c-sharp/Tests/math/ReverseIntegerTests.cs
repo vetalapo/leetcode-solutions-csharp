c604a6f632df34c4ed018f57400ee0734eaa34cc
Vitaly Dushkin <vetalapo@gmail.com>
2024-10-02 17:38:05 +0200

    #7: improve tests formatting

--- c-sharp/Tests/math/ReverseIntegerTests.cs
+++ c-sharp/Tests/math/ReverseIntegerTests.cs
@@ -8,5 +8,6 @@ public class ReverseIntegerTests
     [TestCase( -123, ExpectedResult = -321 )]
     [TestCase( 120, ExpectedResult = 21 )]
     [TestCase( 1534236469, ExpectedResult = 0 )]
-    public int ReverseTest( int x ) => new ReverseInteger().Reverse( x );
+    public int ReverseTest( int x )
+        => new ReverseInteger().Reverse( x );
 }
