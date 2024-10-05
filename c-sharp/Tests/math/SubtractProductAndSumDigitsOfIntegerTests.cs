239ab5889ce959655ca76a85282031ce0f439b85
Vitaly Dushkin <vetalapo@gmail.com>
2024-10-05 15:36:52 +0200

    #1281: improve tests formatting

--- c-sharp/Tests/math/SubtractProductAndSumDigitsOfIntegerTests.cs
+++ c-sharp/Tests/math/SubtractProductAndSumDigitsOfIntegerTests.cs
@@ -6,5 +6,6 @@ public class SubtractProductAndSumDigitsOfIntegerTests
 {
     [TestCase( 234, ExpectedResult = 15 )]
     [TestCase( 4421, ExpectedResult = 21 )]
-    public int SubtractProductAndSumTest( int n ) => new SubtractProductAndSumDigitsOfInteger().SubtractProductAndSum( n );
+    public int SubtractProductAndSumTest( int n ) =>
+        new SubtractProductAndSumDigitsOfInteger().SubtractProductAndSum( n );
 }
