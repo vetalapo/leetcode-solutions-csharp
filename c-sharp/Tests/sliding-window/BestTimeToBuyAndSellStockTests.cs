1e0900bb1785bb0d49fdbf198b66a5e1d95fab51
Vitaly Dushkin <vetalapo@gmail.com>
2024-10-07 13:36:08 +0200

    #121: improve tests formatting

--- c-sharp/Tests/sliding-window/BestTimeToBuyAndSellStockTests.cs
+++ c-sharp/Tests/sliding-window/BestTimeToBuyAndSellStockTests.cs
@@ -6,5 +6,6 @@ public class BestTimeToBuyAndSellStockTests
 {
     [TestCase( new int[] { 7, 1, 5, 3, 6, 4 }, ExpectedResult = 5 )]
     [TestCase( new int[] { 7, 6, 4, 3, 1 }, ExpectedResult = 0 )]
-    public int MaxProfitTest( int[] prices ) => new BestTimeToBuyAndSellStock().MaxProfit( prices );
+    public int MaxProfitTest( int[] prices ) =>
+        new BestTimeToBuyAndSellStock().MaxProfit( prices );
 }
