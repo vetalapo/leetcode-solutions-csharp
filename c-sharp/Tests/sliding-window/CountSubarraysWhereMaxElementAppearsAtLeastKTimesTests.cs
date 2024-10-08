4d39bf40b38f351a0270c1f9765026f713f3f37d
Vitaly Dushkin <vetalapo@gmail.com>
2024-10-08 16:17:45 +0200

    #2962: improve tests formatting

--- c-sharp/Tests/sliding-window/CountSubarraysWhereMaxElementAppearsAtLeastKTimesTests.cs
+++ c-sharp/Tests/sliding-window/CountSubarraysWhereMaxElementAppearsAtLeastKTimesTests.cs
@@ -6,6 +6,6 @@ public class CountSubarraysWhereMaxElementAppearsAtLeastKTimesTests
 {
     [TestCase( new int[] { 1, 3, 2, 3, 3 }, 2, ExpectedResult = 6 )]
     [TestCase( new int[] { 1, 4, 2, 1 }, 3, ExpectedResult = 0 )]
-    public long CountSubarraysTest( int[] nums, int k )
-        => new CountSubarraysWhereMaxElementAppearsAtLeastKTimes().CountSubarrays( nums, k );
+    public long CountSubarraysTest( int[] nums, int k ) =>
+        new CountSubarraysWhereMaxElementAppearsAtLeastKTimes().CountSubarrays( nums, k );
 }
