98aea3ba4730a13e5e2a5e40e5557a323634d389
Vitaly Dushkin <vetalapo@gmail.com>
2024-10-09 13:13:10 +0200

    #2444: improve tests formatting

--- c-sharp/Tests/sliding-window/CountSubarraysWithFixedBoundsTests.cs
+++ c-sharp/Tests/sliding-window/CountSubarraysWithFixedBoundsTests.cs
@@ -6,6 +6,6 @@ public class CountSubarraysWithFixedBoundsTests
 {
     [TestCase( new int[] { 1, 3, 5, 2, 7, 5 }, 1, 5, ExpectedResult = 2 )]
     [TestCase( new int[] { 1, 1, 1, 1 }, 1, 1, ExpectedResult = 10 )]
-    public long CountSubarraysTest( int[] nums, int minK, int maxK )
-        => new CountSubarraysWithFixedBounds().CountSubarrays( nums, minK, maxK );
+    public long CountSubarraysTest( int[] nums, int minK, int maxK ) =>
+        new CountSubarraysWithFixedBounds().CountSubarrays( nums, minK, maxK );
 }
