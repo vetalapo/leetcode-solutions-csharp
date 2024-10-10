aa2bda3c04e212bf5763bb25101efadef7eacacc
Vitaly Dushkin <vetalapo@gmail.com>
2024-10-10 17:05:09 +0200

    #2958: improve tests formatting

--- c-sharp/Tests/sliding-window/LengthOfLongestSubarrayWithAtMostKFrequencyTests.cs
+++ c-sharp/Tests/sliding-window/LengthOfLongestSubarrayWithAtMostKFrequencyTests.cs
@@ -7,6 +7,6 @@ public class LengthOfLongestSubarrayWithAtMostKFrequencyTests
     [TestCase( new int[] { 1, 2, 3, 1, 2, 3, 1, 2 }, 2, ExpectedResult = 6 )]
     [TestCase( new int[] { 1, 2, 1, 2, 1, 2, 1, 2 }, 1, ExpectedResult = 2 )]
     [TestCase( new int[] { 5, 5, 5, 5, 5, 5, 5 }, 4, ExpectedResult = 4 )]
-    public int MaxSubarrayLengthTest( int[] nums, int k )
-        => new LengthOfLongestSubarrayWithAtMostKFrequency().MaxSubarrayLength( nums, k );
+    public int MaxSubarrayLengthTest( int[] nums, int k ) =>
+        new LengthOfLongestSubarrayWithAtMostKFrequency().MaxSubarrayLength( nums, k );
 }
