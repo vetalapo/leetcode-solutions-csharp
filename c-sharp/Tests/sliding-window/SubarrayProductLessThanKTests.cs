833e49e9fca302053cbc953c429175786a4c0a54
Vitaly Dushkin <vetalapo@gmail.com>
2024-10-16 13:42:49 +0200

    #713: improve tests formatting

--- c-sharp/Tests/sliding-window/SubarrayProductLessThanKTests.cs
+++ c-sharp/Tests/sliding-window/SubarrayProductLessThanKTests.cs
@@ -6,6 +6,6 @@ public class SubarrayProductLessThanKTests
 {
     [TestCase( new int[] { 10, 5, 2, 6 }, 100, ExpectedResult = 8 )]
     [TestCase( new int[] { 1, 2, 3 }, 0, ExpectedResult = 0 )]
-    public int NumSubarrayProductLessThanKTest( int[] nums, int k )
-        => new SubarrayProductLessThanK().NumSubarrayProductLessThanK( nums, k );
+    public int NumSubarrayProductLessThanKTest( int[] nums, int k ) =>
+        new SubarrayProductLessThanK().NumSubarrayProductLessThanK( nums, k );
 }
