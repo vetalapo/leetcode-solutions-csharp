43795451166c29175f0e24bf9d5aea87e6f787d9
Vitaly Dushkin <vetalapo@gmail.com>
2024-10-17 16:54:34 +0200

    #992: improve tests formatting

--- c-sharp/Tests/sliding-window/SubarraysWithKDifferentIntegersTests.cs
+++ c-sharp/Tests/sliding-window/SubarraysWithKDifferentIntegersTests.cs
@@ -6,6 +6,6 @@ public class SubarraysWithKDifferentIntegersTests
 {
     [TestCase( new int[] { 1, 2, 1, 2, 3 }, 2, ExpectedResult = 7 )]
     [TestCase( new int[] { 1, 2, 1, 3, 4 }, 3, ExpectedResult = 3 )]
-    public int SubarraysWithKDistinctTest( int[] nums, int k )
-        => new SubarraysWithKDifferentIntegers().SubarraysWithKDistinct( nums, k );
+    public int SubarraysWithKDistinctTest( int[] nums, int k ) =>
+        new SubarraysWithKDifferentIntegers().SubarraysWithKDistinct( nums, k );
 }
