96553f8a1a491fa513e2c871d09be278869a388a
Vitaly Dushkin <vetalapo@gmail.com>
2024-10-19 15:12:15 +0200

    #739: improve tests formatting

--- c-sharp/Tests/stack/DailyTemperaturesSolutionTests.cs
+++ c-sharp/Tests/stack/DailyTemperaturesSolutionTests.cs
@@ -4,8 +4,9 @@ using Problems;
 
 public class DailyTemperaturesSolutionTests
 {
-    [TestCase( new object[] { new int[] { 73, 74, 75, 71, 69, 72, 76, 73 } }, ExpectedResult = new int[] { 1, 1, 4, 2, 1, 1, 0, 0 } )]
-    [TestCase( new object[] { new int[] { 30, 40, 50, 60 } }, ExpectedResult = new int[] { 1, 1, 1, 0 } )]
-    [TestCase( new object[] { new int[] { 30, 60, 90 } }, ExpectedResult = new int[] { 1, 1, 0 } )]
-    public int[] DailyTemperaturesTest( int[] temperatures ) => new DailyTemperaturesSolution().DailyTemperatures( temperatures );
+    [TestCase( [new int[] { 73, 74, 75, 71, 69, 72, 76, 73 }], ExpectedResult = new int[] { 1, 1, 4, 2, 1, 1, 0, 0 } )]
+    [TestCase( [new int[] { 30, 40, 50, 60 }], ExpectedResult = new int[] { 1, 1, 1, 0 } )]
+    [TestCase( [new int[] { 30, 60, 90 }], ExpectedResult = new int[] { 1, 1, 0 } )]
+    public int[] DailyTemperaturesTest( int[] temperatures ) =>
+        new DailyTemperaturesSolution().DailyTemperatures( temperatures );
 }
