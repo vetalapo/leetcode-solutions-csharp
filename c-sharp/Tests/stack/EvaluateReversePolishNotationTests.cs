77d2f42d75ac8fb063537c3856b1056e1d69dbe9
Vitaly Dushkin <vetalapo@gmail.com>
2024-10-21 10:43:13 +0200

    #150: improve tests formatting

--- c-sharp/Tests/stack/EvaluateReversePolishNotationTests.cs
+++ c-sharp/Tests/stack/EvaluateReversePolishNotationTests.cs
@@ -7,6 +7,6 @@ public class EvaluateReversePolishNotationTests
     [TestCase( [new string[] { "2", "1", "+", "3", "*" }], ExpectedResult = 9 )]
     [TestCase( [new string[] { "4", "13", "5", "/", "+" }], ExpectedResult = 6 )]
     [TestCase( [new string[] { "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+" }], ExpectedResult = 22 )]
-    public int EvalRPNTest( string[] tokens )
-        => new EvaluateReversePolishNotation().EvalRPN( tokens );
+    public int EvalRPNTest( string[] tokens ) =>
+        new EvaluateReversePolishNotation().EvalRPN( tokens );
 }
