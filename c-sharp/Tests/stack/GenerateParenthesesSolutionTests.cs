2b2ea383b6bb07f92a6179c9b53a818419502bf5
Vitaly Dushkin <vetalapo@gmail.com>
2024-10-22 13:43:57 +0200

    #22: improve tests formatting

--- c-sharp/Tests/stack/GenerateParenthesesSolutionTests.cs
+++ c-sharp/Tests/stack/GenerateParenthesesSolutionTests.cs
@@ -9,5 +9,6 @@ public class GenerateParenthesesSolutionTests
     [TestCase( 3, ExpectedResult = new string[] { "((()))", "(()())", "(())()", "()(())", "()()()" } )]
     [TestCase( 2, ExpectedResult = new string[] { "(())", "()()" } )]
     [TestCase( 1, ExpectedResult = new string[] { "()" } )]
-    public IList<string> GenerateParenthesisTest( int n ) => new GenerateParenthesesSolution().GenerateParenthesis( n );
+    public IList<string> GenerateParenthesisTest( int n ) =>
+        new GenerateParenthesesSolution().GenerateParenthesis( n );
 }
