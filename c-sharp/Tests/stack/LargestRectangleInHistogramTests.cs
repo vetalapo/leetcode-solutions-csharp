8fc55b0c31e313d67d93b6fbe4d76c048a1a47eb
Vitaly Dushkin <vetalapo@gmail.com>
2024-10-23 08:40:25 +0200

    #84: improve tests formatting

--- c-sharp/Tests/stack/LargestRectangleInHistogramTests.cs
+++ c-sharp/Tests/stack/LargestRectangleInHistogramTests.cs
@@ -7,17 +7,21 @@ using Problems;
 public class LargestRectangleInHistogramTests
 {
     [TestCaseSource( nameof( TestCases ) )]
-    public int LargestRectangleAreaTest( int[] heights ) => new LargestRectangleInHistogram().LargestRectangleArea( heights );
+    public int LargestRectangleAreaTest( int[] heights ) =>
+        new LargestRectangleInHistogram().LargestRectangleArea( heights );
 
     [TestCaseSource( nameof( TestCases ) )]
-    public int LargestRectangleExplicitTest( int[] heights ) => new LargestRectangleInHistogram().LargestRectangleExplicit( heights );
+    public int LargestRectangleExplicitTest( int[] heights ) =>
+        new LargestRectangleInHistogram().LargestRectangleExplicit( heights );
 
     private static IEnumerable TestCases
     {
         get
         {
             yield return new TestCaseData( new int[] { 2, 1, 5, 6, 2, 3 } ).Returns( 10 );
+
             yield return new TestCaseData( new int[] { 2, 4 } ).Returns( 4 );
+
             yield return new TestCaseData( new int[] { 2, 1, 2 } ).Returns( 3 );
         }
     }
