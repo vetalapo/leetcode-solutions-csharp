afe8841b81efcf4575bd78bb21ed5d01fdbd0018
Vitaly Dushkin <vetalapo@gmail.com>
2024-10-25 12:17:38 +0200

    #1614: improve tests formatting

--- c-sharp/Tests/stack/MaximumNestingDepthOfTheParenthesesTests.cs
+++ c-sharp/Tests/stack/MaximumNestingDepthOfTheParenthesesTests.cs
@@ -6,6 +6,6 @@ public class MaximumNestingDepthOfTheParenthesesTests
 {
     [TestCase( "(1+(2*3)+((8)/4))+1", ExpectedResult = 3 )]
     [TestCase( "(1)+((2))+(((3)))", ExpectedResult = 3 )]
-    public int MaxDepthTest( string s )
-        => new MaximumNestingDepthOfTheParentheses().MaxDepth( s );
+    public int MaxDepthTest( string s ) =>
+        new MaximumNestingDepthOfTheParentheses().MaxDepth( s );
 }
