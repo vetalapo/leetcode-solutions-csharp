e627e781b57528c1ad3247abfb1a5ef630702868
Vitaly Dushkin <vetalapo@gmail.com>
2024-10-26 16:29:51 +0200

    #1249: improve tests formatting

--- c-sharp/Tests/stack/MinimumRemoveToMakeValidParenthesesTests.cs
+++ c-sharp/Tests/stack/MinimumRemoveToMakeValidParenthesesTests.cs
@@ -7,6 +7,6 @@ public class MinimumRemoveToMakeValidParenthesesTests
     [TestCase( "lee(t(c)o)de)", ExpectedResult = "lee(t(c)o)de" )]
     [TestCase( "a)b(c)d", ExpectedResult = "ab(c)d" )]
     [TestCase( "))((", ExpectedResult = "" )]
-    public string MinRemoveToMakeValidTest( string s )
-        => new MinimumRemoveToMakeValidParentheses().MinRemoveToMakeValid( s );
+    public string MinRemoveToMakeValidTest( string s ) =>
+        new MinimumRemoveToMakeValidParentheses().MinRemoveToMakeValid( s );
 }
