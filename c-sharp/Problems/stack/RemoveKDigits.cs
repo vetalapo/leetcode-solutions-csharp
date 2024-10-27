47cee746ffee765a2960bbc5fbc09729175a2b70
Vitaly Dushkin <vetalapo@gmail.com>
2024-10-27 15:59:25 +0100

    #402: improve tests formatting

--- c-sharp/Problems/stack/RemoveKDigits.cs
+++ c-sharp/Problems/stack/RemoveKDigits.cs
@@ -2,8 +2,7 @@
  * 402
  * Remove K Digits
  **
- * Given string num representing a non-negative integer num,
- * and an integer k,
+ * Given string num representing a non-negative integer num, and an integer k,
  * return the smallest possible integer after removing k digits from num.
  *
  * Example 1:
--- c-sharp/Tests/stack/RemoveKDigitsSolutionTests.cs
+++ c-sharp/Tests/stack/RemoveKDigitsSolutionTests.cs
@@ -7,6 +7,6 @@ public class RemoveKDigitsSolutionTests
     [TestCase( "1432219", 3, ExpectedResult = "1219" )]
     [TestCase( "10200", 1, ExpectedResult = "200" )]
     [TestCase( "10", 2, ExpectedResult = "0" )]
-    public string RemoveKDigitsTest( string num, int k )
-        => new RemoveKDigitsSolution().RemoveKDigits( num, k );
+    public string RemoveKDigitsTest( string num, int k ) =>
+        new RemoveKDigitsSolution().RemoveKDigits( num, k );
 }
