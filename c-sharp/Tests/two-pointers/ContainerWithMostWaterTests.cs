a070c7796221160d8b464d8f03611dd78fb3cc39
Vitaly Dushkin <vetalapo@gmail.com>
2024-10-30 13:05:28 +0100

    #11: improve tests formatting

--- c-sharp/Tests/two-pointers/ContainerWithMostWaterTests.cs
+++ c-sharp/Tests/two-pointers/ContainerWithMostWaterTests.cs
@@ -6,5 +6,6 @@ public class ContainerWithMostWaterTests
 {
     [TestCase( new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }, ExpectedResult = 49 )]
     [TestCase( new int[] { 1, 1 }, ExpectedResult = 1 )]
-    public int MaxAreaTest( int[] height ) => new ContainerWithMostWater().MaxArea( height );
+    public int MaxAreaTest( int[] height ) =>
+        new ContainerWithMostWater().MaxArea( height );
 }
