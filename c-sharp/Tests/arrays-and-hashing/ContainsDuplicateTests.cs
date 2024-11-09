2a0e0adfb20bba36d0b61ffec2c3ae6317c1fa0b
Vitaly Dushkin <vetalapo@gmail.com>
2024-11-09 17:12:29 +0100

    #217: improve tests formatting

--- c-sharp/Tests/arrays-and-hashing/ContainsDuplicateTests.cs
+++ c-sharp/Tests/arrays-and-hashing/ContainsDuplicateTests.cs
@@ -19,7 +19,9 @@ public class ContainsDuplicateTests
         get
         {
             yield return new TestCaseData( new int[] { 1, 2, 3, 1 } ).Returns( true );
+            
             yield return new TestCaseData( new int[] { 1, 2, 3, 4 } ).Returns( false );
+            
             yield return new TestCaseData( new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 } ).Returns( true );
         }
     }
