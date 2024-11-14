e30ae46422e89c07a0f86fd3a6a6bf33af8f5935
Vitaly Dushkin <vetalapo@gmail.com>
2024-11-14 16:15:27 +0100

    #41: improve tests formatting

--- c-sharp/Tests/arrays-and-hashing/FirstMissingPositiveSolutionTests.cs
+++ c-sharp/Tests/arrays-and-hashing/FirstMissingPositiveSolutionTests.cs
@@ -19,7 +19,9 @@ public class FirstMissingPositiveSolutionTests
         get
         {
             yield return new TestCaseData( [new int[] { 1, 2, 0 }] ).Returns( 3 );
+
             yield return new TestCaseData( [new int[] { 3, 4, -1, 1 }] ).Returns( 2 );
+
             yield return new TestCaseData( [new int[] { 7, 8, 9, 11, 12 }] ).Returns( 1 );
         }
     }
