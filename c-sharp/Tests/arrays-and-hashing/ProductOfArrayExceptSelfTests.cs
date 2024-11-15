d69965ed9bffb61a4e911d8c0689c63fb7378212
Vitaly Dushkin <vetalapo@gmail.com>
2024-11-15 15:56:15 +0100

    #238: improve tests formatting

--- c-sharp/Tests/arrays-and-hashing/ProductOfArrayExceptSelfTests.cs
+++ c-sharp/Tests/arrays-and-hashing/ProductOfArrayExceptSelfTests.cs
@@ -19,6 +19,7 @@ public class ProductOfArrayExceptSelfTests
         get
         {
             yield return new TestCaseData( new int[] { 1, 2, 3, 4 } ).Returns( new int[] { 24, 12, 8, 6 } );
+
             yield return new TestCaseData( new int[] { -1, 1, 0, -3, 3 } ).Returns( new int[] { 0, 0, 9, 0, 0 } );
         }
     }
