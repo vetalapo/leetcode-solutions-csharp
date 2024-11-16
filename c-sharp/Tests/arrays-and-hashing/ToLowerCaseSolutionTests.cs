c3192c33dfa529dad8aa1564a77a36e91c6c4f2b
Vitaly Dushkin <vetalapo@gmail.com>
2024-11-16 13:27:41 +0100

    #709: improve tests formatting

--- c-sharp/Tests/arrays-and-hashing/ToLowerCaseSolutionTests.cs
+++ c-sharp/Tests/arrays-and-hashing/ToLowerCaseSolutionTests.cs
@@ -19,9 +19,13 @@ public class ToLowerCaseSolutionTests
         get
         {
             yield return new TestCaseData( string.Empty ).Returns( string.Empty );
+
             yield return new TestCaseData( "Hello" ).Returns( "hello" );
+
             yield return new TestCaseData( "here" ).Returns( "here" );
+
             yield return new TestCaseData( "LOVELY" ).Returns( "lovely" );
+
             yield return new TestCaseData( "al&phaBET" ).Returns( "al&phabet" );
         }
     }
