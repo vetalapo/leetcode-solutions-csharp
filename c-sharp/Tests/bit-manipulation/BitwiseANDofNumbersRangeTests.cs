0c513be07d1bdd20f177f712bbc8a7637b4b1181
Vitaly Dushkin <vetalapo@gmail.com>
2024-11-20 11:54:46 +0100

    #201: improve tests formatting

--- c-sharp/Tests/bit-manipulation/BitwiseANDofNumbersRangeTests.cs
+++ c-sharp/Tests/bit-manipulation/BitwiseANDofNumbersRangeTests.cs
@@ -15,10 +15,15 @@ public class BitwiseANDofNumbersRangeTests
         get
         {
             yield return new TestCaseData( 0, 0 ).Returns( 0 );
+            
             yield return new TestCaseData( 0, 1 ).Returns( 0 );
+            
             yield return new TestCaseData( 1, 1 ).Returns( 1 );
+            
             yield return new TestCaseData( 2, 5 ).Returns( 0 );
+            
             yield return new TestCaseData( 5, 7 ).Returns( 4 );
+            
             yield return new TestCaseData( 1, 2147483647 ).Returns( 0 );
         }
     }
