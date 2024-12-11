12da03a5a442a2b11535d670fdaa40038583fdee
Vitaly Dushkin <vetalapo@gmail.com>
2024-12-11 13:13:18 +0100

    #3110: add solution and tests

---
+++ c-sharp/Tests/arrays-and-hashing/ScoreOfAStringTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class ScoreOfAStringTests
+{
+    [TestCase( "hello", ExpectedResult = 13 )]
+    [TestCase( "zaz", ExpectedResult = 50 )]
+    public int ScoreOfStringTest( string s ) =>
+        new ScoreOfAString().ScoreOfString( s );
+}
