cdc22ef74a80ae415252488c919d97fcb955d8b0
Vitaly Dushkin <vetalapo@gmail.com>
2024-12-26 17:48:59 +0100

    #1281: improve formatting

--- c-sharp/Problems/math/SubtractProductAndSumDigitsOfInteger.cs
+++ c-sharp/Problems/math/SubtractProductAndSumDigitsOfInteger.cs
@@ -2,7 +2,9 @@
  * 1281
  * Subtract the Product and Sum of Digits of an Integer
  **
- * Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+ * Given an integer number n,
+ * return the difference between the product of its digits
+ * and the sum of its digits.
  *
  * Example 1:
  *   Input: n = 234
