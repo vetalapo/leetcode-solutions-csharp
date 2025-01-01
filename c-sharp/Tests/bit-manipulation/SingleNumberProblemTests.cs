169e114689d9cd86ae6cd9993969c68bb6222345
Vitaly Dushkin <vetalapo@gmail.com>
2025-01-01 17:42:43 +0100

    #136: solution and tests

---
+++ c-sharp/Tests/bit-manipulation/SingleNumberProblemTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class SingleNumberProblemTests
+{
+    [TestCase( new int[] { 2, 2, 1 }, ExpectedResult = 1 )]
+    [TestCase( new int[] { 4, 1, 2, 1, 2 }, ExpectedResult = 4 )]
+    [TestCase( new int[] { 1 }, ExpectedResult = 1 )]
+    public int SingleNumberTest( int[] nums ) =>
+        new SingleNumberProblem().SingleNumber( nums );
+}
