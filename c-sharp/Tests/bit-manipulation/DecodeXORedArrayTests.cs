d721bdce1353bf84be9630dc3bcc9864f4039207
Vitaly Dushkin <vetalapo@gmail.com>
2025-01-03 16:34:48 +0100

    #1720: Decode XORed Array; solution and tests

---
+++ c-sharp/Tests/bit-manipulation/DecodeXORedArrayTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class DecodeXORedArrayTests
+{
+    [TestCase( new int[] { 1, 2, 3 }, 1, ExpectedResult = new int[] { 1, 0, 2, 1 } )]
+    [TestCase( new int[] { 6, 2, 7, 3 }, 4, ExpectedResult = new int[] { 4, 2, 0, 7, 4 } )]
+    public int[] DecodeTest( int[] encoded, int first ) =>
+        new DecodeXORedArray().Decode( encoded, first );
+}
