62f59446d1f561af31c142a7bac75058c62e4574
Vitaly Dushkin <vetalapo@gmail.com>
2025-01-05 18:09:36 +0100

    #3280: Convert Date to Binary; solution and tests

---
+++ c-sharp/Tests/math/ConvertDateToBinarySolutionTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class ConvertDateToBinarySolutionTests
+{
+    [TestCase( "2080-02-29", ExpectedResult = "100000100000-10-11101" )]
+    [TestCase( "1900-01-01", ExpectedResult = "11101101100-1-1" )]
+    public string ConvertDateToBinaryTest( string date ) =>
+        new ConvertDateToBinarySolution().ConvertDateToBinary( date );
+}
