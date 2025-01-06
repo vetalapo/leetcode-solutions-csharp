bdf4fd8fb235d023d9e5ff75b33a1e1e7cbd861b
Vitaly Dushkin <vetalapo@gmail.com>
2025-01-06 12:48:53 +0100

    #1684: Count the Number of Consistent Strings; solution and tests

---
+++ c-sharp/Tests/arrays-and-hashing/CountTheNumberOfConsistentStringsTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CountTheNumberOfConsistentStringsTests
+{
+    [TestCase( "ab", new string[] { "ad", "bd", "aaab", "baa", "badab" }, ExpectedResult = 2 )]
+    [TestCase( "abc", new string[] { "a", "b", "c", "ab", "ac", "bc", "abc" }, ExpectedResult = 7 )]
+    [TestCase( "cad", new string[] { "cc", "acd", "b", "ba", "bac", "bad", "ac", "d" }, ExpectedResult = 4 )]
+    [TestCase( "fstqyienx", new string[] { "n", "eeitfns", "eqqqsfs", "i", "feniqis", "lhoa", "yqyitei", "sqtn", "kug", "z", "neqqis" }, ExpectedResult = 8 )]
+    public int CountConsistentStringsTest( string allowed, string[] words ) =>
+        new CountTheNumberOfConsistentStrings().CountConsistentStrings( allowed, words );
+}
