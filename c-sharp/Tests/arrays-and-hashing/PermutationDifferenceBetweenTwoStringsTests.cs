da2c62b01eb7ab09a0790412eb9a4c7d50109b72
Vitaly Dushkin <vetalapo@gmail.com>
2025-01-07 13:12:54 +0100

    #3146: Permutation Difference between Two Strings; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/PermutationDifferenceBetweenTwoStringsTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class PermutationDifferenceBetweenTwoStringsTests
+{
+    [TestCase( "abc", "bac", ExpectedResult = 2 )]
+    [TestCase( "abcde", "edbac", ExpectedResult = 12 )]
+    public int FindPermutationDifferenceTest( string s, string t ) =>
+        new PermutationDifferenceBetweenTwoStrings().FindPermutationDifference( s, t );
+}
