851af5a3ebe2b16003b0bb08a2f8c8bf98affd58
Vitaly Dushkin <vetalapo@gmail.com>
2025-01-08 16:16:09 +0100

    #1431: Kids With the Greatest Number of Candies; solution and tests

---
+++ c-sharp/Tests/arrays-and-hashing/KidsWithTheGreatestNumberOfCandiesTests.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class KidsWithTheGreatestNumberOfCandiesTests
+{
+    [TestCase( new int[] { 2, 3, 5, 1, 3 }, 3, ExpectedResult = new bool[] { true, true, true, false, true } )]
+    [TestCase( new int[] { 4, 2, 1, 1, 2 }, 1, ExpectedResult = new bool[] { true, false, false, false, false } )]
+    [TestCase( new int[] { 12, 1, 12 }, 10, ExpectedResult = new bool[] { true, false, true } )]
+    public IList<bool> KidsWithCandiesTest( int[] candies, int extraCandies ) =>
+        new KidsWithTheGreatestNumberOfCandies().KidsWithCandies( candies, extraCandies );
+}
