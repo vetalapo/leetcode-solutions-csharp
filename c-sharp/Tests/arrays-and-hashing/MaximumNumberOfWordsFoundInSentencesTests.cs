52e60bedbfb9a5d20f9d66a39c766f51834be78d
Vitaly Dushkin <vetalapo@gmail.com>
2025-01-10 16:18:52 +0100

    #2114: Maximum Number of Words Found in Sentences; solution and tests

---
+++ c-sharp/Tests/arrays-and-hashing/MaximumNumberOfWordsFoundInSentencesTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MaximumNumberOfWordsFoundInSentencesTests
+{
+    [TestCase( [new string[] { "alice and bob love leetcode", "i think so too", "this is great thanks very much" }], ExpectedResult = 6 )]
+    [TestCase( [new string[] { "please wait", "continue to fight", "continue to win" }], ExpectedResult = 3 )]
+    public int MostWordsFoundTest( string[] sentences ) =>
+        new MaximumNumberOfWordsFoundInSentences().MostWordsFound( sentences );
+}
