97c7a046e469b7c10b7f60a047f9c621859f2e7c
Vitaly Dushkin <vetalapo@gmail.com>
2025-01-11 17:59:53 +0100

    #2535: Difference Between Element Sum and Digit Sum of an Array; solution and tests

---
+++ c-sharp/Tests/arrays-and-hashing/DifferenceBetweenElementSumAndDigitSumOfAnArrayTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class DifferenceBetweenElementSumAndDigitSumOfAnArrayTests
+{
+    [TestCase( new int[] { 1, 15, 6, 3 }, ExpectedResult = 9 )]
+    [TestCase( new int[] { 1, 2, 3, 4 }, ExpectedResult = 0 )]
+    public int DifferenceOfSumTest( int[] nums ) =>
+        new DifferenceBetweenElementSumAndDigitSumOfAnArray().DifferenceOfSum( nums );
+}
