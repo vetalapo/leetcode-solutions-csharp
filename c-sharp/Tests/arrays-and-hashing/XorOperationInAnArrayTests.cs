4bd5ea1344747c30d1860390fea941b9cf0ec7ba
Vitaly Dushkin <vetalapo@gmail.com>
2025-01-12 23:44:27 +0100

    #1486: XOR Operation in an Array; solution and tests

---
+++ c-sharp/Tests/arrays-and-hashing/XorOperationInAnArrayTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class XorOperationInAnArrayTests
+{
+    [TestCase( 5, 0, ExpectedResult = 8 )]
+    [TestCase( 4, 3, ExpectedResult = 8 )]
+    public int XorOperationTest( int n, int start ) =>
+        new XorOperationInAnArray().XorOperation( n, start );
+}
