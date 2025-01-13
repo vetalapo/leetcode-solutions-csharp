f2347727fd599147875727002ada4dc393090ef5
Vitaly Dushkin <vetalapo@gmail.com>
2025-01-13 17:46:56 +0100

    #1748: Sum of Unique Elements; solution and tests

---
+++ c-sharp/Tests/arrays-and-hashing/SumOfUniqueElementsTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class SumOfUniqueElementsTests
+{
+    [TestCase( new int[] { 1, 2, 3, 2 }, ExpectedResult = 4 )]
+    [TestCase( new int[] { 1, 1, 1, 1, 1 }, ExpectedResult = 0 )]
+    [TestCase( new int[] { 1, 2, 3, 4, 5 }, ExpectedResult = 15 )]
+    public int SumOfUniqueTest( int[] nums ) =>
+        new SumOfUniqueElements().SumOfUnique( nums );
+}
