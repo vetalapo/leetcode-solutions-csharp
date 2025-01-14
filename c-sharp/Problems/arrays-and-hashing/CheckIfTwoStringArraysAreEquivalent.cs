9cd973fc5cac24ce2d63e8ba7d3c3569ff46ff46
Vitaly Dushkin <vetalapo@gmail.com>
2025-01-14 17:42:09 +0100

    #1662: Check If Two String Arrays are Equivalent; solution and tests

---
+++ c-sharp/Problems/arrays-and-hashing/CheckIfTwoStringArraysAreEquivalent.cs
@@ -0,0 +1,69 @@
+﻿/**
+ * 1662
+ * Check If Two String Arrays are Equivalent
+ **
+ * Given two string arrays word1 and word2,
+ * return true if the two arrays represent the same string,
+ * and false otherwise.
+ *
+ * A string is represented by an array if the array elements concatenated in order forms the string.
+ *
+ * Example 1:
+ *   Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
+ *   Output: true
+ *   Explanation:
+ *     - word1 represents string "ab" + "c" -> "abc"
+ *     - word2 represents string "a" + "bc" -> "abc"
+ *     - The strings are the same, so return true.
+ *
+ * Example 2:
+ *   Input: word1 = ["a", "cb"], word2 = ["ab", "c"]
+ *   Output: false
+ *
+ * Example 3:
+ *   Input: word1  = ["abc", "d", "defg"], word2 = ["abcddefg"]
+ *   Output: true
+ *
+ * Constraints:
+ *   • 1 <= word1.length, word2.length <= 10^3
+ *   • 1 <= word1[i].length, word2[i].length <= 10^3
+ *   • 1 <= sum(word1[i].length), sum(word2[i].length) <= 10^3
+ *   • word1[i] and word2[i] consist of lowercase letters.
+ *
+ * Hint 1:
+ *   Concatenate all strings in the first array into a single string in the given order,
+ *   the same for the second array.
+ *
+ * Hint 2:
+ *   Both arrays represent the same string if and only if the generated strings are the same.
+ **
+ * https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+***/
+
+using System;
+
+namespace Problems;
+
+public class CheckIfTwoStringArraysAreEquivalent
+{
+    public bool ArrayStringsAreEqual( string[] word1, string[] word2 )
+    {
+        string str1 = String.Join( "", word1 );
+        string str2 = String.Join( "", word2 );
+
+        if ( str1.Length != str2.Length )
+        {
+            return false;
+        }
+
+        for ( int i = 0; i < str1.Length; i++ )
+        {
+            if ( str1[i] != str2[i] )
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/CheckIfTwoStringArraysAreEquivalentTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CheckIfTwoStringArraysAreEquivalentTests
+{
+    [TestCase( new string[] { "ab", "c" }, new string[] { "a", "bc" }, ExpectedResult = true )]
+    [TestCase( new string[] { "a", "cb" }, new string[] { "ab", "c" }, ExpectedResult = false )]
+    [TestCase( new string[] { "abc", "d", "defg" }, new string[] { "abcddefg" }, ExpectedResult = true )]
+    public bool ArrayStringsAreEqualTest( string[] word1, string[] word2 ) =>
+        new CheckIfTwoStringArraysAreEquivalent().ArrayStringsAreEqual( word1, word2 );
+}
