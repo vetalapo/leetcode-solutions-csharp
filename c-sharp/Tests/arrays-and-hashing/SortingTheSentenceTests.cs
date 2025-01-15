4c29e74f2ef89e8645911864dda5225003e5fb55
Vitaly Dushkin <vetalapo@gmail.com>
2025-01-15 14:36:47 +0100

    #1859: Sorting the Sentence; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/SortingTheSentenceTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class SortingTheSentenceTests
+{
+    [TestCase( "is2 sentence4 This1 a3", ExpectedResult = "This is a sentence" )]
+    [TestCase( "Myself2 Me1 I4 and3", ExpectedResult = "Me Myself and I" )]
+    public string SortSentenceTest( string s ) =>
+        new SortingTheSentence().SortSentence( s );
+}
