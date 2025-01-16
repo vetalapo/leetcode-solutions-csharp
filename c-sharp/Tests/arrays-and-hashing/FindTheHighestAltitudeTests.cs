7e9bba7650a2942558e12b36bdcfea45ad5fd1fb
Vitaly Dushkin <vetalapo@gmail.com>
2025-01-16 13:09:18 +0100

    #1732: Find the Highest Altitude; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/FindTheHighestAltitudeTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class FindTheHighestAltitudeTests
+{
+    [TestCase( new int[] { -5, 1, 5, 0, -7 }, ExpectedResult = 1 )]
+    [TestCase( new int[] { -4, -3, -2, -1, 4, 3, 2 }, ExpectedResult = 0 )]
+    public int LargestAltitudeTest( int[] gain ) =>
+        new FindTheHighestAltitude().LargestAltitude( gain );
+}
