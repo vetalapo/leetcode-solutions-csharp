b47a1e6bd0e6b3557668d1bbc9db1cb9dbaeec14
Vitaly Dushkin <vetalapo@gmail.com>
2025-01-18 18:20:25 +0100

    #1768: Merge Strings Alternately; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/MergeStringsAlternatelyTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MergeStringsAlternatelyTests
+{
+    [TestCase( "abc", "pqr", ExpectedResult = "apbqcr" )]
+    [TestCase( "ab", "pqrs", ExpectedResult = "apbqrs" )]
+    [TestCase( "abcd", "pq", ExpectedResult = "apbqcd" )]
+    public string MergeAlternatelyTest( string word1, string word2 ) =>
+        new MergeStringsAlternately().MergeAlternately( word1, word2 );
+}
