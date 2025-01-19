59435b6a73b0f90a325570ccedd3cad16f494484
Vitaly Dushkin <vetalapo@gmail.com>
2025-01-19 19:07:45 +0100

    #700: Search in a Binary Search Tree; solution

---
+++ c-sharp/Problems/trees/SearchInABinarySearchTree.cs
@@ -0,0 +1,56 @@
+﻿/**
+ * 700
+ * Search in a Binary Search Tree
+ **
+ * You are given the root of a binary search tree (BST) and an integer val.
+ *
+ * Find the node in the BST that the node's value equals val and
+ * return the subtree rooted with that node.
+ *
+ * If such a node does not exist, return null.
+ *
+ * Example 1:
+ *   Input: root = [4,2,7,1,3], val = 2
+ *   Output: [2,1,3]
+ *
+ * Example 2:
+ *   Input: root = [4,2,7,1,3], val = 5
+ *   Output: []
+ *
+ * Constraints:
+ *   • The number of nodes in the tree is in the range [1, 5000].
+ *   • 1 <= Node.val <= 10^7
+ *   • root is a binary search tree.
+ *   • 1 <= val <= 10^7
+ **
+ * https://leetcode.com/problems/search-in-a-binary-search-tree/
+***/
+
+using Problems.Common;
+
+namespace Problems;
+
+public class SearchInABinarySearchTree
+{
+    public TreeNode SearchBST( TreeNode root, int val )
+    {
+        if ( root is null )
+        {
+            return null;
+        }
+
+        if ( root.val == val )
+        {
+            return root;
+        }
+
+        if ( val > root.val )
+        {
+            return SearchBST( root.right, val );
+        }
+        else
+        {
+            return SearchBST( root.left, val );
+        }
+    }
+}
