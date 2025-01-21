1fafcbea78485d006977c532eb5b7dc04cddf89d
Vitaly Dushkin <vetalapo@gmail.com>
2025-01-21 13:46:36 +0100

    #2000: Reverse Prefix of Word; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/ReversePrefixOfWordTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace Problems.Tests;
+
+public class ReversePrefixOfWordTests
+{
+    [TestCase( "abcdefd", 'd', ExpectedResult = "dcbaefd" )]
+    [TestCase( "xyxzxe", 'z', ExpectedResult = "zxyxxe" )]
+    [TestCase( "abcd", 'z', ExpectedResult = "abcd" )]
+    public string ReversePrefixTest( string word, char ch ) =>
+        new ReversePrefixOfWord().ReversePrefix( word, ch );
+}
