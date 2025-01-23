1aa50dac9acad0fa9606b46b734288ded1040c36
Vitaly Dushkin <vetalapo@gmail.com>
2025-01-23 12:56:16 +0100

    #1688: Count of Matches in Tournament: solution & tests

---
+++ c-sharp/Tests/math/CountOfMatchesInTournamentTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CountOfMatchesInTournamentTests
+{
+    [TestCase( 7, ExpectedResult = 6 )]
+    [TestCase( 14, ExpectedResult = 13 )]
+    public int NumberOfMatchesTest( int n ) =>
+        new CountOfMatchesInTournament().NumberOfMatches( n );
+}
