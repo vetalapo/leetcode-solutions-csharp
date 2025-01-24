98ee502b36bfe108c4ada0b4830ed0d0c424f3f6
Vitaly Dushkin <vetalapo@gmail.com>
2025-01-24 13:35:07 +0100

    #1791: Find Center of Star Graph; solution & tests

---
+++ c-sharp/Tests/graph/FindCenterOfStarGraphTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class FindCenterOfStarGraphTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int FindCenterTest( int[][] edges ) =>
+        new FindCenterOfStarGraph().FindCenter( edges );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( [new int[][] { [1, 2], [2, 3], [4, 2] }] ).Returns( 2 );
+
+            yield return new TestCaseData( [new int[][] { [1, 2], [5, 1], [1, 3], [1, 4] }] ).Returns( 1 );
+        }
+    }
+}
