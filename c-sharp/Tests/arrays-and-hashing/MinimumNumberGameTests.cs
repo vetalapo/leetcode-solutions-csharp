2a44fd60542df7edff660eb093d67f10d5ed7234
Vitaly Dushkin <vetalapo@gmail.com>
2025-01-25 21:14:43 +0100

    #2974: Minimum Number Game; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/MinimumNumberGameTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MinimumNumberGameTests
+{
+    [TestCase( new int[] { 5, 4, 2, 3 }, ExpectedResult = new int[] { 3, 2, 5, 4 } )]
+    [TestCase( new int[] { 2, 5 }, ExpectedResult = new int[] { 5, 2 } )]
+    public int[] NumberGameTest( int[] nums ) =>
+        new MinimumNumberGame().NumberGame( nums );
+}
