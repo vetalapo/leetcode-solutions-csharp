a1d1ac7a84cd56ec48e538bb1e4098061d53eef9
Vitaly Dushkin <vetalapo@gmail.com>
2025-01-26 13:08:36 +0100

    #1528: Shuffle String; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/ShuffleStringTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class ShuffleStringTests
+{
+    [TestCase( "codeleet", new int[] { 4, 5, 6, 7, 0, 2, 1, 3 }, ExpectedResult = "leetcode" )]
+    [TestCase( "abc", new int[] { 0, 1, 2 }, ExpectedResult = "abc" )]
+    public string RestoreStringTest( string s, int[] indices ) =>
+        new ShuffleString().RestoreString( s, indices );
+}
