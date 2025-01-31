3517e3ad1e0388f0ef7a4a8e2046385cb529e01f
Vitaly Dushkin <vetalapo@gmail.com>
2025-01-31 12:40:15 +0100

    #2824: Count Pairs Whose Sum is Less than Target; solution & tests

---
+++ c-sharp/Tests/two-pointers/CountPairsWhoseSumIsLessThanTargetTests.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class CountPairsWhoseSumIsLessThanTargetTests
+{
+    [TestCase( new int[] { -1, 1, 2, 3, 1 }, 2, ExpectedResult = 3 )]
+    [TestCase( new int[] { -6, 2, 5, -2, -7, -1, 3 }, -2, ExpectedResult = 10 )]
+    public int CountPairsTest( IList<int> nums, int target ) =>
+        new CountPairsWhoseSumIsLessThanTarget().CountPairs( nums, target );
+}
