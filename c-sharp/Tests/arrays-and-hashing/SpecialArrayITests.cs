b251c62e272625bfb467b38e9bed505254fe8ffd
Vitaly Dushkin <vetalapo@gmail.com>
2025-02-01 16:13:48 +0100

    #3151: Special Array I; solution and tests

---
+++ c-sharp/Tests/arrays-and-hashing/SpecialArrayITests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class SpecialArrayITests
+{
+    [TestCase( new int[] { 1 }, ExpectedResult = true )]
+    [TestCase( new int[] { 2, 1, 4 }, ExpectedResult = true )]
+    [TestCase( new int[] { 4, 3, 1, 6 }, ExpectedResult = false )]
+    public bool IsArraySpecialTest( int[] nums ) =>
+        new SpecialArrayI().IsArraySpecial( nums );
+}
