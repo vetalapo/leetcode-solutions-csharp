6c0232dd7ea71342cdbe16ecc8d83cd5ba4822a6
Vitaly Dushkin <vetalapo@gmail.com>
2025-02-02 22:33:01 +0100

    #2418: Sort the People; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/SortThePeopleTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class SortThePeopleTests
+{
+    [TestCase( new string[] { "Mary", "John", "Emma" }, new int[] { 180, 165, 170 }, ExpectedResult = new string[] { "Mary", "Emma", "John" } )]
+    [TestCase( new string[] { "Alice", "Bob", "Bob" }, new int[] { 155, 185, 150 }, ExpectedResult = new string[] { "Bob", "Alice", "Bob" } )]
+    public string[] SortPeopleTest( string[] names, int[] heights ) =>
+        new SortThePeople().SortPeople( names, heights );
+}
