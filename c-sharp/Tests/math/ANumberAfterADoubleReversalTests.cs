5a1f599bebe4712d088db0b90f8f7a185c376d56
Vitaly Dushkin <vetalapo@gmail.com>
2025-02-06 12:36:39 +0100

    #2119: A Number After a Double Reversal; solution & tests

---
+++ c-sharp/Tests/math/ANumberAfterADoubleReversalTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class ANumberAfterADoubleReversalTests
+{
+    [TestCase( 526, ExpectedResult = true )]
+    [TestCase( 1800, ExpectedResult = false )]
+    [TestCase( 0, ExpectedResult = true )]
+    public bool IsSameAfterReversalsTest( int num ) =>
+        new ANumberAfterADoubleReversal().IsSameAfterReversals( num );
+}
