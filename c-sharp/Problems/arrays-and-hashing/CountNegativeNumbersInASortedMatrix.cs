acbc596c2be142a5b80c93455692a610d37b2081
Vitaly Dushkin <vetalapo@gmail.com>
2025-02-08 22:57:19 +0100

    #1351: Count Negative Numbers in a Sorted Matrix; solution & tests

---
+++ c-sharp/Problems/arrays-and-hashing/CountNegativeNumbersInASortedMatrix.cs
@@ -0,0 +1,54 @@
+﻿/**
+ * 1351
+ * Count Negative Numbers in a Sorted Matrix
+ **
+ * Given a m x n matrix grid
+ * which is sorted in non-increasing order
+ * both row-wise and column-wise,
+ * return the number of negative numbers in grid.
+ *
+ * Example 1:
+ *   Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+ *   Output: 8
+ *   Explanation: There are 8 negatives number in the matrix.
+ *
+ * Example 2:
+ *   Input: grid = [[3,2],[1,0]]
+ *   Output: 0
+ *
+ * Constraints:
+ *   • m == grid.length
+ *   • n == grid[i].length
+ *   • 1 <= m, n <= 100
+ *   • -100 <= grid[i][j] <= 100
+ *
+ * Follow up: Could you find an O(n + m) solution?
+ *
+ * Hint 1:
+ *   Use binary search for optimization or simply brute force.
+ **
+ * https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+***/
+
+namespace Problems;
+
+public class CountNegativeNumbersInASortedMatrix
+{
+    public int CountNegatives( int[][] grid )
+    {
+        int negativeNumbersCount = 0;
+
+        for ( int row = 0; row < grid.Length; row++ )
+        {
+            int col = grid[row].Length - 1;
+
+            while ( col >= 0 && grid[row][col] < 0 )
+            {
+                negativeNumbersCount++;
+                col--;
+            }
+        }
+
+        return negativeNumbersCount;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/CountNegativeNumbersInASortedMatrixTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class CountNegativeNumbersInASortedMatrixTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int CountNegativesTest( int[][] grid ) =>
+        new CountNegativeNumbersInASortedMatrix().CountNegatives( grid );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( [new int[][] { [4, 3, 2, -1], [3, 2, 1, -1], [1, 1, -1, -2], [-1, -1, -2, -3] }] ).Returns( 8 );
+
+            yield return new TestCaseData( [new int[][] { [3, 2], [1, 0] }] ).Returns( 0 );
+        }
+    }
+}
