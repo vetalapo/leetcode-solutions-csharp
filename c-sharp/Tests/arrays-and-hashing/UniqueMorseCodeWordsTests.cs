9cd62ee237613aec442bddbf9d24392920a7ca1c
Vitaly Dushkin <vetalapo@gmail.com>
2025-02-09 10:38:29 +0100

    #804: Unique Morse Code Words; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/UniqueMorseCodeWordsTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class UniqueMorseCodeWordsTests
+{
+    [TestCase( [new string[] { "gin", "zen", "gig", "msg" }], ExpectedResult = 2 )]
+    [TestCase( [new string[] { "a" }], ExpectedResult = 1 )]
+    public int UniqueMorseRepresentationsTest( string[] words )
+        => new UniqueMorseCodeWords().UniqueMorseRepresentations( words );
+}
