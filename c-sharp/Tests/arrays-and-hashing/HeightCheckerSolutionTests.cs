9486098302aae8841823b00db83566bb94dcf490
Vitaly Dushkin <vetalapo@gmail.com>
2025-02-10 13:53:07 +0100

    #1051: Height Checker; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/HeightCheckerSolutionTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class HeightCheckerSolutionTests
+{
+    [TestCase( new int[] { 1, 1, 4, 2, 1, 3 }, ExpectedResult = 3 )]
+    [TestCase( new int[] { 5, 1, 2, 3, 4 }, ExpectedResult = 5 )]
+    [TestCase( new int[] { 1, 2, 3, 4, 5 }, ExpectedResult = 0 )]
+    public int HeightCheckerTest( int[] heights ) =>
+        new HeightCheckerSolution().HeightChecker( heights );
+}
